7409b674265b4985432de6318cfb8fd56f2e7e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 14:03:31 +0000

    Add package heygptcli/0.3.5/heygptcli-0.3.5.tar.gz

---
+++ packages/heygptcli/heygptcli-0.3.5.tar.gz/heygptcli-0.3.5/heygpt/constant.py
@@ -0,0 +1,39 @@
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+prompt_items_url = os.environ.get("GPT_PROMPT_URL", "")
+
+genrtare_prompt_url = os.environ.get(
+    "PROMPT_GENERATE_URL",
+    "https://api-inference.huggingface.co/models/merve/chatgpt-prompt-generator-v12",
+)
+
+
+def get_config_path(app: str = "heygpt"):
+    if os.name == "nt":  # Windows
+        return os.path.expanduser(f"~\\AppData\\Roaming\\{app}\\config.json")
+    elif os.name == "posix":  # Linux, macOS, etc.
+        return os.path.expanduser(f"~/.config/{app}/config.json")
+    else:
+        raise OSError("Unsupported operating system")
+
+
+config_path = get_config_path()
+
+try:
+    with open(config_path, "r") as f:
+        configs = json.loads(f.read())
+        if "openai_key" in configs:
+            os.environ["OPENAI_API_KEY"] = configs["openai_key"]
+        if "openai_endpoint" in configs:
+            os.environ["OPENAI_BASE_URL"] = configs.get(
+                "openai_endpoint", "https://api.openai.com/v1"
+            )
+            if "https://openrouter.ai/api/v1" in configs.get("openai_endpoint", ""):
+                os.environ["OPENROUTER_API_BASE"] = configs["openai_endpoint"]
+                os.environ["OPENROUTER_API_KEY"] = configs["openai_key"]
+except FileNotFoundError:
+    configs = {}
