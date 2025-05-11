3de7a4cc5f7c2aea8d4568a70ff921dcfce6347a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 14:03:19 +0000

    Add package tempconverter2025-user/0.1/tempconverter2025_user-0.1.tar.gz

---
+++ packages/tempconverter2025-user/tempconverter2025_user-0.1.tar.gz/tempconverter2025_user-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tempconverter2025_user',
+    version='0.1',
+    install_requires=[
+        'tempconverter2025'
+    ],
+    py_modules=['main'],
+    author='Boss',
+    author_email='Vladimirtlv@gmail.com',
+    description='Simple project depending on tempconverter2025',
+    url='https://github.com/Cryptonations/tempconverter2025_user',
+)
