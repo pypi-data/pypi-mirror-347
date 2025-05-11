57fa8fd06a989fb998abc7043cda0dded4923c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:03:55 +0000

    Add package docutranslate/0.1.8/docutranslate-0.1.8-py3-none-any.whl

---
+++ packages/docutranslate/docutranslate-0.1.8-py3-none-any.whl/docutranslate/utils/convert.py
@@ -0,0 +1,31 @@
+import os
+from huggingface_hub.errors import LocalEntryNotFoundError
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling_core.types.doc import ImageRefMode
+from pathlib import Path
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.document import DocumentStream
+from docutranslate.logger import translater_logger
+IMAGE_RESOLUTION_SCALE = 4
+
+
+def file2markdown_embed_images(file_path: Path | str|DocumentStream, formula=False, code=False,artifacts_path:Path|str|None=None) -> str:
+    pipeline_options = PdfPipelineOptions(artifacts_path=artifacts_path)
+    # pipeline_options.do_ocr=False
+    pipeline_options.images_scale = IMAGE_RESOLUTION_SCALE
+    pipeline_options.generate_picture_images = True
+    if formula:
+        pipeline_options.do_formula_enrichment=True
+    if code:
+        pipeline_options.do_code_enrichment=True
+    converter = DocumentConverter(format_options={
+        InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+    })
+    try:
+        result = converter.convert(file_path).document.export_to_markdown(image_mode=ImageRefMode.EMBEDDED)
+    except LocalEntryNotFoundError:
+        translater_logger.info(f"无法连接huggingface，正在尝试换源")
+        os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+        result = converter.convert(file_path).document.export_to_markdown(image_mode=ImageRefMode.EMBEDDED)
+    return result
