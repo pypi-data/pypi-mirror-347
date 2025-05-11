14d34cc41b2f76444b646b654a0591b6740bb19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 14:03:28 +0000

    Add package gds-fdtd/0.3.7/gds_fdtd-0.3.7-py3-none-any.whl

---
+++ packages/gds-fdtd/gds_fdtd-0.3.7-py3-none-any.whl/gds_fdtd/__init__.py
@@ -0,0 +1,6 @@
+"""gds_fdtd Top-level package imports."""
+from . import core, lyprocessor, simprocessor
+
+__author__ = """Mustafa Hammood"""
+__email__ = "mustafa@siepic.com"
+__version__ = "0.3.7"
