4990febfb0b0f818d735bc7a1cdacf3b92301998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:04:00 +0000

    Add package database-mysql-local/0.1.1/database_mysql_local-0.1.1.tar.gz

---
+++ packages/database-mysql-local/database_mysql_local-0.1.1.tar.gz/database_mysql_local-0.1.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+PACKAGE_NAME = "database-mysql-local"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+setuptools.setup(
+    name=PACKAGE_NAME,  # https://pypi.org/project/database-mysql-local
+    version='0.1.1',
+    author="Circles",
+    author_email="info@circlez.ai",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f'{package_dir}/src'},
+    package_data={package_dir: ['*.py']},
+    long_description="Database MySQL Local",
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license
+        # TODO Fix all repos especially the python templates
+        # 'License :: OSI Approved :: MIT License',
+        # 'License :: OSI Approved :: Apache Software License',
+        # 'License :: OSI Approved :: BSD License'
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "mysql-connector-python>=9.0.0",  # https://pypi.org/project/mysql-connector-python/
+        "url-remote>=0.0.108",  # https://pypi.org/project/url-remote/
+        "logger-local>=0.0.166",  # https://pypi.org/project/logger-local/
+        # database-infrastructure-local 0.0.35 generic_crud_abstract.py GenericCrudAbstract
+        "database-infrastructure-local>=0.0.38",  # https://pypi.org/project/database-infrastructure-local/
+        "language-remote>=0.0.24",  # https://pypi.org/project/language-remote/
+        "sql-to-code-local>=0.0.18",  # https://pypi.org/project/sql-to-code-local/
+        "python-sdk-remote>=0.0.131",
+        # Commented because of a problem with serverless, TODO We should add a test to spot such case
+        # "sshtunnel>=0.4.0", # https://pypi.org/project/sshtunnel/
+    ]
+)
