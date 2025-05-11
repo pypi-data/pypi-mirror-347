0d73b31d667c9751e3caa0ddac9a2d50a2d80cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:04:09 +0000

    Add package MetaRagTool/0.2.8/metaragtool-0.2.8.tar.gz

---
+++ packages/MetaRagTool/metaragtool-0.2.8.tar.gz/metaragtool-0.2.8/MetaRagTool/Encoders/MyReranker.py
@@ -0,0 +1,42 @@
+﻿from abc import ABC, abstractmethod
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+
+
+class MyReranker(ABC):
+
+    def __init__(self, model_name: str, verbose=False):
+        self.verbose = verbose
+        self.model_name = model_name
+
+    @abstractmethod
+    def rerank(self, query:str, documents: list):
+        pass
+
+
+class JinaRerankerV2(MyReranker):
+    def __init__(self, model_name: str = "jinaai/jina-reranker-v2-base-multilingual", verbose=False):
+        super().__init__(model_name, verbose)
+
+        # Load the Jina Reranker v2 model and tokenizer
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            torch_dtype="auto",
+            trust_remote_code=True
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def rerank(self, query: str, documents: list):
+        # Create sentence pairs for the model
+        sentence_pairs = [[query, doc] for doc in documents]
+
+        # Tokenize the input
+        inputs = self.tokenizer(sentence_pairs, padding=True, truncation=True, return_tensors="pt", max_length=1024)
+
+        # Get the model's predictions
+        with torch.no_grad():
+            scores = self.model(**inputs).logits
+
+        # Return the scores for each document
+        return scores.float().numpy().tolist()
+
---
+++ packages/MetaRagTool/metaragtool-0.2.8.tar.gz/metaragtool-0.2.8/MetaRagTool/RAG/DocumentStructs.py
@@ -0,0 +1,85 @@
+﻿from enum import Enum
+
+class ChunkingMethod(Enum):
+    SENTENCE_MERGER_CROSS_PARAGRAPH = 'sentence_merger_cross_paragraph'
+    SENTENCE = 'sent'
+    PARAGRAPH = 'paragraph'
+    DOCUMENT = 'document'
+    RECURSIVE = 'recursive'
+    SENTENCE_MERGER = 'sentence_merger'
+
+class MyDocument:
+    def __init__(self,text=""):
+        self.MyParagraphs = []
+        self.MySentences = []
+        self.Text = text
+        self.isChunked = False
+
+    def AddParagraph(self, paragraph):
+        self.MyParagraphs.append(paragraph)
+
+    def AddSentence(self, sentence):
+        self.MySentences.append(sentence)
+
+
+
+class MyParagraph:
+    def __init__(self,document,text=""):
+        self.MyDocument = document
+        self.MySentences = []
+        self.Prev = None
+        self.Next = None
+        self.Text = text
+        self.Embeddings = None
+
+    def AddSentence(self, sentence):
+        self.MySentences.append(sentence)
+
+    def SetPrev(self, prev):
+        self.Prev = prev
+
+    def SetNext(self, nextParagraph):
+        self.Next = nextParagraph
+
+
+class MySentence:
+    def __init__(self ,document,paragraph,text=""):
+        self.MyDocument = document
+        self.MyParagraph = paragraph
+        self.Prev = None
+        self.Next = None
+        self.Text = text
+
+    def SetPrev(self, prev):
+        self.Prev = prev
+
+    def SetNext(self, nextSentence):
+        self.Next = nextSentence
+
+class MyChunk:
+    def __init__(self,document, text=""):
+        self.Text = text
+        self.Paragraphs = []
+        self.Sentences = []
+        self.Document = document
+        self.PrevRelated = None
+        self.NextRelated = None
+        self.Embeddings = None
+        self.Length = -1
+
+    def AddParagraph(self, paragraph):
+        if paragraph not in self.Paragraphs:
+            self.Paragraphs.append(paragraph)
+
+    def AddSentence(self, sentence):
+        self.Sentences.append(sentence)
+
+
+
+    def __str__(self):
+        return self.Text
+
+    def __repr__(self):
+        return self.Text
+
+
