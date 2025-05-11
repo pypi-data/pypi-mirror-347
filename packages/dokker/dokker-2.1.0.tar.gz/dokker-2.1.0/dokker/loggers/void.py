35e768428e1fef1d135acaeb659f0f0f0c1ebac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:04:05 +0000

    Add package dokker/2.1.0/dokker-2.1.0.tar.gz

---
+++ packages/dokker/dokker-2.1.0.tar.gz/dokker-2.1.0/dokker/loggers/void.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel
+
+
+class VoidLogger(BaseModel):
+    """A logger that omits all logs"""
+
+    def on_pull(self, log: str) -> None:
+        """A method for logs
+
+        Parameters
+        ----------
+        log : str
+            The log to print
+        """
+
+        pass
+
+    def on_up(self, log: str) -> None:
+        """A method for logs
+
+        Parameters
+        ----------
+        log : str
+            The log to print
+        """
+        pass
+
+    def on_stop(self, log: str) -> None:
+        """A method for logs
+
+        Parameters
+        ----------
+        log : str
+            The log to print
+        """
+        pass
+
+    def on_logs(self, log: str) -> None:
+        """A method for logs
+
+        Parameters
+        ----------
+        log : str
+            The log to print
+        """
+        pass
+
+    def on_down(self, log: str) -> None:
+        """A method for logs
+
+        Parameters
+        ----------
+        log : str
+            The log to print
+        """
+        pass
