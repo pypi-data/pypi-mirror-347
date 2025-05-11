7e29a6288dbf7bd521a1c61639380d5fac16e101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:03:55 +0000

    Add package pagesmith/0.0.2/pagesmith-0.0.2-py3-none-any.whl

---
+++ packages/pagesmith/pagesmith-0.0.2-py3-none-any.whl/pagesmith/__init__.py
@@ -0,0 +1,9 @@
+"""Splitting HTML into pages, preserving HTML tags
+while respecting the original document structure and text integrity.
+
+The file is mandatory for build system to find the package.
+"""
+
+from pagesmith.__about__ import __version__
+
+__all__ = ["__version__"]
