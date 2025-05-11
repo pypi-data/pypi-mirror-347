2e6513d74c892b5744559a0caa9cd001e5d08687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:04:05 +0000

    Add package pdfsp/0.1.8/pdfsp-0.1.8-py3-none-any.whl

---
+++ packages/pdfsp/pdfsp-0.1.8-py3-none-any.whl/pdfsp/__init__.py
@@ -0,0 +1,24 @@
+
+# This file is part of the pdfsp project
+# Copyright (C) 2025 Sermet Pekin 
+#
+# This source code is free software; you can redistribute it and/or
+# modify it under the terms of the European Union Public License
+# (EUPL), Version 1.2, as published by the European Commission.
+#
+# You should have received a copy of the EUPL version 1.2 along with this
+# program. If not, you can obtain it at:
+# <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
+#
+# This source code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# European Union Public License for more details.
+#
+# Alternatively, if agreed upon, you may use this code under any later
+# version of the EUPL published by the European Commission.
+from .core import extract_tables , extract_tables_from_pdf
+__all__ = [
+    "extract_tables",
+    "extract_tables_from_pdf",
+]
