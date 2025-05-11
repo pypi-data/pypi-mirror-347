a22633f75b7196e4ec23217d1fada1e4cb7e5caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 14:04:07 +0000

    Add package duckdb/1.3.0.dev3309/duckdb-1.3.0.dev3309.tar.gz

---
+++ packages/duckdb/duckdb-1.3.0.dev3309.tar.gz/duckdb-1.3.0.dev3309/duckdb_build/ub_src_function_scalar_system.cpp
@@ -0,0 +1,10 @@
+#include "src/function/scalar/system/aggregate_export.cpp"
+
+#include "src/function/scalar/system/write_log.cpp"
+
+#include "src/function/scalar/system/current_transaction_id.cpp"
+
+#include "src/function/scalar/system/current_connection_id.cpp"
+
+#include "src/function/scalar/system/current_query_id.cpp"
+
