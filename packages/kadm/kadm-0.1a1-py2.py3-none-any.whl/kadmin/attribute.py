1d23382d3c8cc598a411a436060f266ea804caee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 14:03:21 +0000

    Add package kadm/0.1a1/kadm-0.1a1-py2.py3-none-any.whl

---
+++ packages/kadm/kadm-0.1a1-py2.py3-none-any.whl/kadmin/attribute.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "kadm"
+__version__ = "0.1.alpha.1"
+__urlhome__ = "https://github.com/podboy/kadmin/"
+__description__ = "Manage SSH keys easily, securely, and efficiently"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
