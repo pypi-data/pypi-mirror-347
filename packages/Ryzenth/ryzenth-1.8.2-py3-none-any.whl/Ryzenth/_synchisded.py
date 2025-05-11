e8c4212afc14bc9eb39f9b8bc612042e6f51532f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:03:48 +0000

    Add package Ryzenth/1.8.2/ryzenth-1.8.2-py3-none-any.whl

---
+++ packages/Ryzenth/ryzenth-1.8.2-py3-none-any.whl/Ryzenth/_synchisded.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2019-2025 (c) Randy W @xtdevs, @xtsea
+#
+# from : https://github.com/TeamKillerX
+# Channel : @RendyProjects
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+
+import httpx
+
+from Ryzenth.types import QueryParameter
+
+LOGS = logging.getLogger("[Ryzenth] sync")
+
+class RyzenthXSync:
+    def __init__(self, api_key: str, base_url: str = "https://randydev-ryu-js.hf.space/api"):
+        self.api_key = api_key
+        self.base_url = base_url.rstrip("/")
+        self.headers = {"x-api-key": self.api_key}
+
+    def send_message(self, model: str, params: QueryParameter):
+        model_dict = {
+            "hybrid": "AkenoX-1.9-Hybrid",
+            "melayu": "lu-melayu",
+            "nocodefou": "nocodefou",
+            "mia": "mia-khalifah",
+            "curlmcode": "curl-command-code",
+            "quotessad": "quotes-sad",
+            "quoteslucu": "quotes-lucu",
+            "lirikend": "lirik-end",
+            "alsholawat": "al-sholawat"
+        }
+        model_param = model_dict.get(model)
+        if not model_param:
+            raise ValueError(f"Invalid model name: {model}")
+
+        try:
+            response = httpx.get(
+                f"{self.base_url}/v1/ai/akenox/{model_param}",
+                params=params.dict(),
+                headers=self.headers,
+                timeout=10
+            )
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPError as e:
+            LOGS.error(f"[SYNC] Error fetching from model '{model_param}': {e}")
+            return None
