1be268c2b7cf485ac4521197681ef5af3dd848c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-11 15:04:16 +0000

    Add package bbdd-utils/0.3.15/bbdd_utils-0.3.15.tar.gz

---
+++ packages/bbdd-utils/bbdd_utils-0.3.15.tar.gz/bbdd_utils-0.3.15/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bbdd_utils',
+    version='0.3.15',
+    packages=find_packages(),
+    install_requires=[],
+    author='Luis_J_Domingo',
+    description='insertar datos en una base de datos SQL o NoSQL',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/tuusuario/milibreria',  # si usas GitHub
+)
