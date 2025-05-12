a41c968c0d17817edce38f293a76ef5216d0e534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:36 +0000

    Add package twinshare/0.1.10/twinshare-0.1.10.tar.gz

---
+++ packages/twinshare/twinshare-0.1.10.tar.gz/twinshare-0.1.10/setup.py
@@ -0,0 +1,39 @@
+# Skrypt instalacyjny
+"""
+setup.py
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="twinshare",
+    version="0.1.10",
+    description="Twin Share - Environment Manager - narzędzie do dzielenia się środowiskami Embedded/AI/VM",
+    author="Tom Sapletta",
+    packages=find_packages(),
+    install_requires=[
+        "libvirt-python",
+        "aiohttp",
+        "asyncio",
+        "tabulate",
+        "pyyaml",
+        "cryptography",
+        "python-daemon",
+        "netifaces"
+    ],
+    entry_points={
+        "console_scripts": [
+            "safetytwin=src.cli.main:main",
+        ],
+    },
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
