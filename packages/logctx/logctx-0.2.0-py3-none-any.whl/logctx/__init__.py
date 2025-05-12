878f4147ee74ab7c7e8d22c6b712ee90d374ba50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:36 +0000

    Add package logctx/0.2.0/logctx-0.2.0-py3-none-any.whl

---
+++ packages/logctx/logctx-0.2.0-py3-none-any.whl/logctx/__init__.py
@@ -0,0 +1,32 @@
+"""logctx package
+
+This package provides a convenient way to manage logging contexts in Python.
+
+It allows you to manage key-value pairs for log-contexts which can be automatically
+added to log messages within their respective context.
+"""
+
+__author__ = "Alexander Schulte"
+__maintainer__ = "Alexander Schulte"
+
+__version__ = "0.2.0"
+
+from logctx import decorators
+from logctx._core import (
+    ContextInjectingLoggingFilter,
+    LogContext,
+    clear,
+    get_current,
+    new_context,
+    update,
+)
+
+__all__ = [
+    "ContextInjectingLoggingFilter",
+    "LogContext",
+    "clear",
+    "get_current",
+    "new_context",
+    "update",
+    "decorators",
+]
