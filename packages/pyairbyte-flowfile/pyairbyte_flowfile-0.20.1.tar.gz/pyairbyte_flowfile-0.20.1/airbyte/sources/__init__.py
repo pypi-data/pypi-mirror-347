62e7f170b1874ece5474f168dda6fb4e2d90eca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:04:31 +0000

    Add package pyairbyte-flowfile/0.20.1/pyairbyte_flowfile-0.20.1.tar.gz

---
+++ packages/pyairbyte-flowfile/pyairbyte_flowfile-0.20.1.tar.gz/pyairbyte_flowfile-0.20.1/airbyte/sources/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+"""Sources connectors module for PyAirbyte."""
+
+from __future__ import annotations
+
+from airbyte.sources import base, util
+from airbyte.sources.base import Source
+from airbyte.sources.registry import (
+    ConnectorMetadata,
+    get_available_connectors,
+    get_connector_metadata,
+)
+from airbyte.sources.util import (
+    get_benchmark_source,
+    get_source,
+)
+
+
+__all__ = [
+    # Submodules
+    "base",
+    "util",
+    # Factories
+    "get_source",
+    "get_benchmark_source",
+    # Helper Functions
+    "get_available_connectors",
+    "get_connector_metadata",
+    # Classes
+    "Source",
+    "ConnectorMetadata",
+]
