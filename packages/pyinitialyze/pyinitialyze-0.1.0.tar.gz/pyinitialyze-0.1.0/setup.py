4e2968db4df60f3b3a89d3cab2b0c3ea01cbebc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:22 +0000

    Add package pyinitialyze/0.1.0/pyinitialyze-0.1.0.tar.gz

---
+++ packages/pyinitialyze/pyinitialyze-0.1.0.tar.gz/pyinitialyze-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyinitialyze',
+    version='0.1.0',
+    description='A Python lib you can use to initialize your Python scripts (Console based / Windowed)',
+    author='Guillaume Plagier',
+    packages=find_packages(),
+    install_requires=["requests"], 
+)
