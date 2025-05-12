9e65c11697c7b92aaa7f861be92831f64ed4ba3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:06:06 +0000

    Add package random-nick-generator/0.2/random_nick_generator-0.2.tar.gz

---
+++ packages/random-nick-generator/random_nick_generator-0.2.tar.gz/random_nick_generator-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='random-nick-generator',
+    version='0.2',
+    packages=find_packages(),
+    description='A simple library to generate random nicknames.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/dsadasdasdsaas/random-nick-generator',  # Укажи свой репозиторий
+)
---
+++ packages/random-nick-generator/random_nick_generator-0.2.tar.gz/random_nick_generator-0.2/tests/test_generator.py
@@ -0,0 +1,5 @@
+from random_nick_generator import generate
+
+# Генерируем и выводим несколько ников
+for _ in range(5):
+    print("Generated nick:", generate())
