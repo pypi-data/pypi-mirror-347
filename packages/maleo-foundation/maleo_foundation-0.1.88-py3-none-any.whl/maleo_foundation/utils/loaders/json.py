602327bf63f11b5d60d07b5224f10694c6998325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 16:04:40 +0000

    Add package maleo-foundation/0.1.88/maleo_foundation-0.1.88-py3-none-any.whl

---
+++ packages/maleo-foundation/maleo_foundation-0.1.88-py3-none-any.whl/maleo_foundation/utils/loaders/json.py
@@ -0,0 +1,14 @@
+import json
+from typing import Dict, List, Union, Any
+from pathlib import Path
+
+class JSONLoader:
+    @staticmethod
+    def load(path:Union[Path, str]) -> Union[Dict[str, Any], List[Any]]:
+        file_path = Path(path)
+
+        if not file_path.is_file():
+            raise FileNotFoundError(f"File not found: {file_path}")
+
+        with open(file_path, 'r') as f:
+            return json.load(f)
