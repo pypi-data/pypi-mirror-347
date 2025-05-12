1d20231aa16af2813129df21816220a9010bb178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 16:04:42 +0000

    Add package uipath/2.0.51/uipath-2.0.51.tar.gz

---
+++ packages/uipath/uipath-2.0.51.tar.gz/uipath-2.0.51/src/uipath/_utils/__init__.py
@@ -0,0 +1,17 @@
+from ._endpoint import Endpoint
+from ._infer_bindings import get_inferred_bindings_names, infer_bindings
+from ._logs import setup_logging
+from ._request_override import header_folder
+from ._request_spec import RequestSpec
+from ._user_agent import header_user_agent, user_agent_value
+
+__all__ = [
+    "Endpoint",
+    "setup_logging",
+    "RequestSpec",
+    "header_folder",
+    "get_inferred_bindings_names",
+    "infer_bindings",
+    "header_user_agent",
+    "user_agent_value",
+]
---
+++ packages/uipath/uipath-2.0.51.tar.gz/uipath-2.0.51/src/uipath/models/__init__.py
@@ -0,0 +1,49 @@
+from .action_schema import ActionSchema
+from .actions import Action
+from .assets import UserAsset
+from .buckets import Bucket
+from .connections import Connection, ConnectionToken
+from .context_grounding import ContextGroundingQueryResponse
+from .context_grounding_index import ContextGroundingIndex
+from .errors import BaseUrlMissingError, SecretMissingError
+from .exceptions import IngestionInProgressException
+from .interrupt_models import (
+    CreateAction,
+    InvokeProcess,
+    WaitAction,
+    WaitJob,
+)
+from .job import Job
+from .processes import Process
+from .queues import (
+    CommitType,
+    QueueItem,
+    QueueItemPriority,
+    TransactionItem,
+    TransactionItemResult,
+)
+
+__all__ = [
+    "Action",
+    "UserAsset",
+    "ContextGroundingQueryResponse",
+    "ContextGroundingIndex",
+    "Process",
+    "QueueItem",
+    "CommitType",
+    "TransactionItem",
+    "QueueItemPriority",
+    "TransactionItemResult",
+    "Connection",
+    "ConnectionToken",
+    "Job",
+    "InvokeProcess",
+    "ActionSchema",
+    "WaitJob",
+    "WaitAction",
+    "CreateAction",
+    "IngestionInProgressException",
+    "BaseUrlMissingError",
+    "SecretMissingError",
+    "Bucket",
+]
