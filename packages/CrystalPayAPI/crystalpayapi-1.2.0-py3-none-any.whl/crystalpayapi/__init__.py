81d6f4e143bc0f29b0e602faf97ec9a3d635994d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 03:22:38 +0000

    Add package CrystalPayAPI/1.2.0/crystalpayapi-1.2.0-py3-none-any.whl

---
+++ packages/CrystalPayAPI/crystalpayapi-1.2.0-py3-none-any.whl/crystalpayapi/__init__.py
@@ -0,0 +1,4 @@
+from .api import CrystalPayAPI, InvoiceType, PayoffSubtractFrom, CrystalPayAPIError
+
+__version__ = '1.2.0'
+__all__ = ['CrystalPayAPI', 'InvoiceType', 'PayoffSubtractFrom', 'CrystalPayAPIError']
