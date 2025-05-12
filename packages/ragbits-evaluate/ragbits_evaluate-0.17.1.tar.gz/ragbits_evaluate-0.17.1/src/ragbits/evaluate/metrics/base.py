bf31bbcc26e02da018d8d5a04824329fdbf65776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 16:04:49 +0000

    Add package ragbits-evaluate/0.17.1/ragbits_evaluate-0.17.1.tar.gz

---
+++ packages/ragbits-evaluate/ragbits_evaluate-0.17.1.tar.gz/ragbits_evaluate-0.17.1/src/ragbits/evaluate/metrics/base.py
@@ -0,0 +1,85 @@
+from abc import ABC, abstractmethod
+from types import ModuleType
+from typing import ClassVar, Generic
+
+from typing_extensions import Self
+
+from ragbits.core.utils.config_handling import WithConstructionConfig
+from ragbits.evaluate import metrics
+from ragbits.evaluate.pipelines.base import EvaluationResultT
+
+
+class Metric(WithConstructionConfig, Generic[EvaluationResultT], ABC):
+    """
+    Base class for metrics.
+    """
+
+    default_module: ClassVar[ModuleType | None] = metrics
+    configuration_key: ClassVar[str] = "metric"
+
+    def __init__(self, weight: float = 1.0) -> None:
+        """
+        Initializes the metric.
+
+        Args:
+            weight: Metric value weight in the final score, used during optimization.
+        """
+        super().__init__()
+        self.weight = weight
+
+    @abstractmethod
+    def compute(self, results: list[EvaluationResultT]) -> dict:
+        """
+        Compute the metric.
+
+        Args:
+            results: The evaluation results.
+
+        Returns:
+            The computed metric.
+        """
+
+
+class MetricSet(WithConstructionConfig, Generic[EvaluationResultT]):
+    """
+    Represents a set of metrics.
+    """
+
+    configuration_key: ClassVar[str] = "metrics"
+    default_module: ClassVar[ModuleType | None] = metrics
+
+    def __init__(self, *metrics: Metric[EvaluationResultT]) -> None:
+        """
+        Initialize the metric set.
+
+        Args:
+            metrics: The metrics.
+        """
+        self.metrics = metrics
+
+    @classmethod
+    def from_config(cls, config: dict) -> Self:
+        """
+        Create an instance of `MetricSet` from a configuration dictionary.
+
+        Args:
+            config: A dictionary containing configuration settings for the metric set.
+
+        Returns:
+            An instance of the metric set class initialized with the provided configuration.
+        """
+        return cls(*[Metric.subclass_from_config(metric_config) for metric_config in config.values()])
+
+    def compute(self, results: list[EvaluationResultT]) -> dict:
+        """
+        Compute the metrics.
+
+        Args:
+            results: The evaluation results.
+
+        Returns:
+            The computed metrics.
+        """
+        return {
+            name: metric.weight * value for metric in self.metrics for name, value in metric.compute(results).items()
+        }
---
+++ packages/ragbits-evaluate/ragbits_evaluate-0.17.1.tar.gz/ragbits_evaluate-0.17.1/src/ragbits/evaluate/pipelines/document_search.py
@@ -0,0 +1,96 @@
+from dataclasses import dataclass
+from uuid import uuid4
+
+from typing_extensions import Self
+
+from ragbits.core.sources.hf import HuggingFaceSource
+from ragbits.document_search import DocumentSearch
+from ragbits.evaluate.pipelines.base import EvaluationData, EvaluationPipeline, EvaluationResult
+
+
+class DocumentSearchData(EvaluationData):
+    """
+    Represents the evaluation data for document search.
+    """
+
+    question: str
+    reference_passages: list[str]
+
+
+@dataclass
+class DocumentSearchResult(EvaluationResult):
+    """
+    Represents the result of a single evaluation.
+    """
+
+    question: str
+    reference_passages: list[str]
+    predicted_passages: list[str]
+
+
+class DocumentSearchPipeline(EvaluationPipeline[DocumentSearch, DocumentSearchData, DocumentSearchResult]):
+    """
+    Document search evaluation pipeline.
+    """
+
+    def __init__(self, evaluation_target: DocumentSearch, source: dict | None = None) -> None:
+        """
+        Initialize the document search evaluation pipeline.
+
+        Args:
+            evaluation_target: Document Search instance.
+            source: Source data config for ingest.
+        """
+        super().__init__(evaluation_target=evaluation_target)
+        self.source = source or {}
+
+    @classmethod
+    def from_config(cls, config: dict) -> Self:
+        """
+        Create an instance of `DocumentSearchPipeline` from a configuration dictionary.
+
+        Args:
+            config: A dictionary containing configuration settings for the pipeline.
+
+        Returns:
+            An instance of the pipeline class initialized with the provided configuration.
+        """
+        # At this point, we assume that if the source is set, the pipeline is run in experimental mode
+        # and create random indexes for testing
+        # TODO: optimize this for cases with duplicated document search configs between runs
+        if config.get("source"):
+            config["vector_store"]["config"]["index_name"] = str(uuid4())
+        evaluation_target = DocumentSearch.from_config(config)
+        return cls(evaluation_target=evaluation_target, source=config.get("source"))
+
+    async def prepare(self) -> None:
+        """
+        Ingest corpus data for evaluation.
+        """
+        if self.source:
+            # For now we only support HF sources for pre-evaluation ingest
+            # TODO: Make it generic to any data source
+            sources = await HuggingFaceSource.list_sources(
+                path=self.source["config"]["path"],
+                split=self.source["config"]["split"],
+            )
+            await self.evaluation_target.ingest(sources)
+
+    async def __call__(self, data: DocumentSearchData) -> DocumentSearchResult:
+        """
+        Run the document search evaluation pipeline.
+
+        Args:
+            data: The evaluation data.
+
+        Returns:
+            The evaluation result.
+        """
+        elements = await self.evaluation_target.search(data.question)
+        predicted_passages = [element.text_representation for element in elements if element.text_representation]
+
+        return DocumentSearchResult(
+            question=data.question,
+            reference_passages=data.reference_passages,
+            predicted_passages=predicted_passages,
+        )
