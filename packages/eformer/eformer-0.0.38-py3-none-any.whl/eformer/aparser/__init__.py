750ba6fc36cd485ac014ed3b0fa293d98f329e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 05:03:31 +0000

    Add package eformer/0.0.38/eformer-0.0.38-py3-none-any.whl

---
+++ packages/eformer/eformer-0.0.38-py3-none-any.whl/eformer/aparser/__init__.py
@@ -0,0 +1,13 @@
+from ._aparser import (
+	ArgumentDefaultsHelpFormatter,
+	DataClass,
+	DataClassArgumentParser,
+	DataClassType,
+)
+
+__all__ = (
+	"ArgumentDefaultsHelpFormatter",
+	"DataClass",
+	"DataClassArgumentParser",
+	"DataClassType",
+)
