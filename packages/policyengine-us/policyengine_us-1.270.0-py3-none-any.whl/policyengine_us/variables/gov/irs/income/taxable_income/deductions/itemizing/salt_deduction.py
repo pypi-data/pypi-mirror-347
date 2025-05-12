e171c38d8b125236c0bb016b597b14a7aaf7375e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 05:04:28 +0000

    Add package policyengine-us/1.270.0/policyengine_us-1.270.0-py3-none-any.whl

---
+++ packages/policyengine-us/policyengine_us-1.270.0-py3-none-any.whl/policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/salt_deduction.py
@@ -0,0 +1,17 @@
+from policyengine_us.model_api import *
+
+
+class salt_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "SALT deduction"
+    unit = USD
+    documentation = "State and local taxes plus real estate tax deduction from taxable income."
+    definition_period = YEAR
+    reference = "https://www.law.cornell.edu/uscode/text/26/164"
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.irs.deductions.itemized.salt_and_real_estate
+        reported_salt = tax_unit("reported_salt", period)
+        cap = p.cap[tax_unit("filing_status", period)]
+        return min_(cap, reported_salt)
