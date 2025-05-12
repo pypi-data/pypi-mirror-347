6f61f8e81f49f3c57510b5b150d6f371f6dd7434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 03:22:37 +0000

    Add package pypasswizard/0.3.1/pypasswizard-0.3.1.tar.gz

---
+++ packages/pypasswizard/pypasswizard-0.3.1.tar.gz/pypasswizard-0.3.1/src/utility.py
@@ -0,0 +1,75 @@
+"""
+This module provides a database class for handling password storage and retrieval.
+"""
+
+import sqlite3
+import os
+import logging
+import pandas as pd  # type: ignore
+from pandera.typing import DataFrame
+
+
+class Database:
+    """
+    Database class for managing password storage and retrieval.
+    """
+
+    def __init__(self, path: str) -> None:
+        self.db_path: str = os.path.abspath(path)
+        self.create_database_path()
+        self.creating_table()
+
+    def create_database_path(self) -> None:
+        data_dir = os.path.dirname(self.db_path)
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+            logging.info(f"Created directory: {data_dir}")
+
+    def creating_table(self) -> None:
+        query = """
+        CREATE TABLE IF NOT EXISTS password (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE,
+            password TEXT
+        )
+        """
+        self.execute_query(query)
+
+    def execute_query(self, query: str, params: tuple = ()) -> list:
+        try:
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(query, params)
+                result = cursor.fetchall()
+                conn.commit()
+                return result
+        except sqlite3.Error as e:
+            logging.error(f"Database query failed: {e}")
+            raise ConnectionError(f"{e}")
+
+    def inserting_password(self, name: str, password: str) -> None:
+        if not password:
+            raise ValueError("Password cannot be empty.")
+        self.execute_query(
+            "INSERT INTO password(name, password) VALUES(?, ?)", (name, password)
+        )
+        logging.info(f"Password for name '{name}' inserted successfully.")
+
+    def retrieve_password_with_name(self, name: str) -> str:
+        result = self.execute_query(
+            "SELECT password FROM password WHERE name = ?", (name,)
+        )
+        if result:
+            return result[0][0]
+        raise ValueError(f"No password associated with name: {name}")
+
+    def delete_password_with_name(self, name: str) -> None:
+        self.execute_query("DELETE FROM password WHERE name = ?", (name,))
+        logging.info(f"Password for name '{name}' deleted successfully.")
+
+    def show_all_passwords(self) -> DataFrame:
+        result = self.execute_query("SELECT name, password FROM password")
+        df = pd.DataFrame(result, columns=["name", "password"])
+        if not df.empty:
+            return df
+        raise ValueError("No data found in the 'password' table.")
