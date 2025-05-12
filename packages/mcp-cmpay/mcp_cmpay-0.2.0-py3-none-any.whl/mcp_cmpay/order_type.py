ce2fbdf62f837fe348f2db40701820bb128050f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 15:05:00 +0000

    Add package mcp-cmpay/0.2.0/mcp_cmpay-0.2.0-py3-none-any.whl

---
+++ packages/mcp-cmpay/mcp_cmpay-0.2.0-py3-none-any.whl/mcp_cmpay/order_type.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class OrderType(Enum):
+    WAP_DIRECT_PAY = "WAPDirectPayConfirm"
+    ORDER_QUERY = "OrderQuery"
+    ORDER_REFUND = "OrderRefund"
+    MERCHANT_REFUND_QUERY = "merchantOrderRefundQuery"
