ce805bbe4efda0f307864a334c667e6fec842f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:05:24 +0000

    Add package safentic/1.0.1/safentic-1.0.1.tar.gz

---
+++ packages/safentic/safentic-1.0.1.tar.gz/safentic-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="safentic",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    license="Proprietary :: Safentic Commercial License",  # Custom classifier
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",  # Indicates not open source
+        "Operating System :: OS Independent",
+    ],
+    author="Safentic",
+    author_email="contact@safentic.com",
+    description="Safentic SDK for behavior analysis",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://safentic.com",
+)
