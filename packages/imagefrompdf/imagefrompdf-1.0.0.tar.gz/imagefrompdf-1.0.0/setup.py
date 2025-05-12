519967f21d7728056ac2ad5052d3060c969a63c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 05:04:13 +0000

    Add package imagefrompdf/1.0.0/imagefrompdf-1.0.0.tar.gz

---
+++ packages/imagefrompdf/imagefrompdf-1.0.0.tar.gz/imagefrompdf-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="imagefrompdf",
+    version="1.0.0",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jedahee/imagefrompdf",
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'imagefrompdf=imagefrompdf.__main__',  # Comando para la CLI
+        ],
+    },
+    install_requires=[
+        'pdf2image',
+        'Pillow',
+        'click',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'tox',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
