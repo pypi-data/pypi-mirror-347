2ecbd939b6ec1f99742431b33384a16364bdd39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:26 +0000

    Add package nonebot-plugin-resolver2/1.8.9/nonebot_plugin_resolver2-1.8.9-py3-none-any.whl

---
+++ packages/nonebot-plugin-resolver2/nonebot_plugin_resolver2-1.8.9-py3-none-any.whl/nonebot_plugin_resolver2/parsers/__init__.py
@@ -0,0 +1,19 @@
+from .acfun import AcfunParser as AcfunParser
+from .bilibili import BilibiliParser as BilibiliParser
+from .douyin import DouyinParser as DouyinParser
+from .kugou import KuGouParser as KuGouParser
+from .ncm import NCMParser as NCMParser
+from .utils import get_redirect_url as get_redirect_url
+from .weibo import WeiBoParser as WeiBoParser
+from .xiaohongshu import XiaoHongShuParser as XiaoHongShuParser
+
+__all__ = [
+    "AcfunParser",
+    "BilibiliParser",
+    "DouyinParser",
+    "KuGouParser",
+    "NCMParser",
+    "WeiBoParser",
+    "XiaoHongShuParser",
+    "get_redirect_url",
+]
