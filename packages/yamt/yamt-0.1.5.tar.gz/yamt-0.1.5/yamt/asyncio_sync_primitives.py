a068a602c1647642704a94365abfac89c2c335ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:20 +0000

    Add package yamt/0.1.5/yamt-0.1.5.tar.gz

---
+++ packages/yamt/yamt-0.1.5.tar.gz/yamt-0.1.5/yamt/asyncio_sync_primitives.py
@@ -0,0 +1,125 @@
+from typing import Literal
+import asyncio
+import inspect
+import traceback
+import types
+import logging
+
+logger = logging.getLogger("yamt")
+
+
+class ContextSkip(Exception):
+    ...
+
+
+class Grab:
+    is_grabbed: bool = False
+
+    async def __aenter__(self) -> None:
+        if self.is_grabbed:
+            raise ContextSkip()
+
+        self.is_grabbed = True
+        return None
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        tb: types.TracebackType | None
+    ) -> Literal[True] | None:
+        if isinstance(exc, ContextSkip):
+            return True
+        self.is_grabbed = False
+        return None
+
+
+# TODO: fix stack inspection in recursive call (rework locked_stack)
+class StackLimitedLock(asyncio.Lock):
+    stack_limit: int | None
+    locked_stack: list[int]
+
+    def __init__(self, stack_limit: int | None = None) -> None:
+        logger.warning("unstable synchronization primitive used")
+        self.stack_limit = stack_limit
+        self.locked_stack = list()
+        super().__init__()
+
+    async def acquire(self):
+        cur_frame = inspect.currentframe().f_back
+        cur_hash = hash(cur_frame.f_code)
+
+        if self.locked():
+            for i, (frame, _) in enumerate(traceback.walk_stack(cur_frame)):
+                if i == self.stack_limit:
+                    break
+                if hash(frame.f_code) in self.locked_stack:
+                    self.locked_stack.append(cur_hash)
+                    return None
+
+        await super().acquire()
+        self.locked_stack.append(cur_hash)
+
+    def release(self):
+        cur_frame = inspect.currentframe().f_back
+        self.locked_stack.remove(hash(cur_frame.f_code))
+        if self.locked():
+            super().release()
+
+
+class PerSecondSemaphore(asyncio.Semaphore):
+    deffer_time: float
+    _lock: asyncio.Lock
+
+    def __init__(self, value: int = 1) -> None:
+        super().__init__(value)
+        self.deffer_time = 1 / value
+        self._lock = asyncio.Lock()
+
+    def release(self):
+        asyncio.create_task(self._deffered_exit())
+
+    async def _deffered_exit(self):
+        async with self._lock:
+            await asyncio.sleep(self.deffer_time)
+            super().release()
+
+
+# deprecated
+SemaphorePerSecond = PerSecondSemaphore
+
+
+class LockOverflowError(Exception):
+    ...
+
+
+class OverflowLock(asyncio.Lock):
+    limit: int
+    counter: int = 0
+
+    def __init__(self, limit: int) -> None:
+        self.limit = limit
+        super().__init__()
+
+    async def acquire(self):
+        if self.limit <= self.counter:
+            raise LockOverflowError()
+
+        await super().acquire()
+        self.counter += 1
+
+    def release(self):
+        super().release()
+        self.counter -= 1
+
+
+class SkippedOverflowLock(OverflowLock):
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        tb: types.TracebackType | None
+    ) -> bool | None:
+        if isinstance(exc, LockOverflowError):
+            return True
+        return await super().__aexit__(exc_type, exc, tb)
