55c5955f49e1830d5e40a1b971bed37d584bd84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:04:32 +0000

    Add package async-tls-client/1.1/async_tls_client-1.1-py3-none-any.whl

---
+++ packages/async-tls-client/async_tls_client-1.1-py3-none-any.whl/async_tls_client/__version__.py
@@ -0,0 +1,11 @@
+#  _____  __  __         ___ _ _            _
+# /__   \/ / / _\       / __\ (_) ___ _ __ | |_
+#   / /\/ /  \ \ _____ / /  | | |/ _ \ '_ \| __|
+#  / / / /____\ \_____/ /___| | |  __/ | | | |_
+#  \/  \____/\__/     \____/|_|_|\___|_| |_|\__|
+
+__title__ = "async_tls_client"
+__description__ = "Asyncio fork of Python-TLS-Client with updated dependencies"
+__version__ = "1.1"
+__author__ = "Diprog (fork), Florian Zager (original)"
+__license__ = "MIT"
