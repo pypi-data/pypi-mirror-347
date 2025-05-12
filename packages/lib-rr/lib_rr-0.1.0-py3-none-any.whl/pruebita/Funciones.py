35d9dc0a91efe525b8ad14da403904074eaac5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:04:52 +0000

    Add package lib-rr/0.1.0/lib_rr-0.1.0-py3-none-any.whl

---
+++ packages/lib-rr/lib_rr-0.1.0-py3-none-any.whl/pruebita/Funciones.py
@@ -0,0 +1,19 @@
+#Funciones.py
+import random   # Importar random para generar numeros aleatorios
+
+
+# Funcion objetivo a minimizar
+def funcion_objetivo(x):
+    # """ calcula el cuadrado del valor de x. """
+    return x ** 2
+
+# Crear un individuo aleatorio
+def crear_individuo(valor_min, valor_max):
+    # """ Genera un numero aleatorio dentro de un rango especifico """
+    return random.unifor(valor_min, valor_max)
+print (crear_individuo)         
+
+# crear una poblacion inicial
+def crear_poblacion(tamano, valor_min, valor_max):
+    # Genera una lista de individuos aleatorios
+    return [crear_individuo(valor_min, valor_max) for _ in range(tamano)]
