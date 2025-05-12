8473b8bfc7474b871099cf811c3117ebb1916849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:23 +0000

    Add package obsidiana/2025.5.4/obsidiana-2025.5.4-py3-none-any.whl

---
+++ packages/obsidiana/obsidiana-2025.5.4-py3-none-any.whl/obsidiana/vault.py
@@ -0,0 +1,76 @@
+"""
+Python API for Obsidian vaults.
+"""
+
+from datetime import date
+from functools import cached_property
+from pathlib import Path
+
+from attrs import frozen
+import frontmatter
+
+
+@frozen
+class Vault:
+    """
+    An Obsidian vault.
+    """
+
+    path: Path
+
+    def child(self, *segments: str) -> Path:
+        """
+        Return a path within this vault.
+        """
+        return self.path.joinpath(*segments)
+
+    def notes(self):
+        """
+        All notes within the vault.
+        """
+        return (
+            Note(path=path, vault=self) for path in self.path.rglob("*.md")
+        )
+
+
+@frozen
+class Note:
+    """
+    An Obsidian note.
+    """
+
+    path: Path
+    _vault: Vault
+
+    @cached_property
+    def frontmatter(self):
+        """
+        Frontmatter from all notes (along with the notes themselves).
+        """
+        return frontmatter.loads(self.path.read_text()).metadata
+
+    @cached_property
+    def tags(self):
+        """
+        The note's topical tags.
+        """
+        return frozenset(self.frontmatter.get("tags", ()))
+
+    def subpath(self) -> str:
+        """
+        The subpath of this note inside of the fault, without extension.
+        """
+        path = self.path.relative_to(self._vault.path)
+        return str(path).removesuffix(".md")
+
+    def awaiting_triage(self):
+        """
+        A note in the vault which is awaiting being refiled into another spot.
+
+        For me these are daily notes in the root of the vault.
+        """
+        try:
+            date.fromisoformat(self.path.stem)
+        except ValueError:
+            return False
+        return self.path.parent == self._vault.path
