cc7c222757ecff0a265a1b1fbea72ad0ec1e3339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 05:03:59 +0000

    Add package spryx-iam/0.1.2/spryx_iam-0.1.2.tar.gz

---
+++ packages/spryx-iam/spryx_iam-0.1.2.tar.gz/spryx_iam-0.1.2/spryx_iam/client.py
@@ -0,0 +1,21 @@
+from spryx_http import SpryxAsyncClient
+
+from spryx_iam.resources.organizations import Organizations
+from spryx_iam.resources.plans import Plans
+
+
+class SpryxIAM(SpryxAsyncClient):
+    def __init__(
+        self,
+        application_id: str,
+        application_secret: str,
+        base_url: str = "https://api.spryx.ai",
+    ):
+        super().__init__(
+            base_url=base_url,
+            application_id=application_id,
+            application_secret=application_secret,
+        )
+
+        self.organizations = Organizations(self)
+        self.plans = Plans(self)
