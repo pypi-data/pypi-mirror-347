8b4a468d301943c5ae1817f43c10de178b1b245f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:06:11 +0000

    Add package twinshare/0.1.22/twinshare-0.1.22-py3-none-any.whl

---
+++ packages/twinshare/twinshare-0.1.22-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Twin Share - Environment Manager
+"""
+
+# Version information
+__version__ = "0.1.19"
+
+# Note: We avoid importing modules here to prevent circular imports
+# Modules will be imported as needed by the application
