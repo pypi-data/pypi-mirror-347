d6e92b24081a5155fcc7c5e6b58076b7d0f36562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 03:22:21 +0000

    Add package tadqeeq/1.4.6/tadqeeq-1.4.6-py3-none-any.whl

---
+++ packages/tadqeeq/tadqeeq-1.4.6-py3-none-any.whl/tadqeeq/__init__.py
@@ -0,0 +1,8 @@
+""" 
+Tadqeeq - Image Annotator Tool
+An interactive image annotation tool for efficient labeling.
+Developed by Mohamed Behery @ RTR Software Development (2025-04-27).
+Licensed under the MIT License.
+"""
+
+from .widgets import ImageAnnotator
