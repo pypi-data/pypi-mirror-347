a8b3552bef0c5da4267cbd202c79dcfb4336bc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:05:30 +0000

    Add package finsim/1.0.6/finsim-1.0.6-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/finsim/finsim-1.0.6-cp311-cp311-macosx_11_0_arm64.whl/finsim/estimate/risk.py
@@ -0,0 +1,58 @@
+
+import numpy as np
+from scipy import stats
+
+from .native.pyrisk import python_estimate_downside_risk, python_estimate_upside_risk
+from .native.cythonrisk import cython_estimate_downside_risk, cython_estimate_upside_risk
+from .constants import dividing_factors_dict
+
+
+def estimate_downside_risk(timestamps, prices, target_return, unit='year', lowlevellang='C'):
+    dividing_factor = dividing_factors_dict[unit]
+
+    ts = np.array(timestamps, dtype='datetime64[s]')
+    ts = np.array(ts, dtype=np.float64) / dividing_factor
+
+    if lowlevellang == 'C':
+        return cython_estimate_downside_risk(ts, prices, target_return)
+    elif lowlevellang == 'P':
+        return python_estimate_downside_risk(ts, prices, target_return)
+    else:
+        raise ValueError(
+            'Unknown low-level language: {}. (Should be "P" (Python), or "C" (Cython))'.format(
+                lowlevellang))
+
+
+def estimate_upside_risk(timestamps, prices, target_return, unit='year', lowlevellang='C'):
+    dividing_factor = dividing_factors_dict[unit]
+
+    ts = np.array(timestamps, dtype='datetime64[s]')
+    ts = np.array(ts, dtype=np.float64) / dividing_factor
+
+    if lowlevellang == 'C':
+        return cython_estimate_upside_risk(ts, prices, target_return)
+    elif lowlevellang == 'N':
+        return python_estimate_upside_risk(ts, prices, target_return)
+    else:
+        raise ValueError(
+            'Unknown low-level language: {}. (Should be "P" (Python), or "C" (Cython).)'.format(
+                lowlevellang))
+
+
+def estimate_beta(timestamps, prices, market_prices, unit='year'):
+    dividing_factor = dividing_factors_dict[unit]
+
+    ts = np.array(timestamps, dtype='datetime64[s]')
+    ts = np.array(ts, dtype=np.float64) / dividing_factor
+    dt = ts[1:] - ts[:-1]
+
+    dlogprices = np.log(prices[1:] / prices[:-1])
+    dlogmarketprices = np.log(market_prices[1:] / market_prices[:-1])
+    assert len(dt) == len(dlogprices)
+    assert len(dt) == len(dlogmarketprices)
+
+    stockyields = dlogprices / dt
+    marketyields = dlogmarketprices / dt
+
+    reg = stats.linregress(x=marketyields, y=stockyields)
+    return reg.slope
