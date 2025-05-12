58a14bbd01ccecdde439ec9a314067d89598a403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 15:04:58 +0000

    Add package pipelex/0.1.5/pipelex-0.1.5.tar.gz

---
+++ packages/pipelex/pipelex-0.1.5.tar.gz/pipelex-0.1.5/pipelex/cogt/image/generated_image.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: © 2025 Evotis S.A.S.
+# SPDX-License-Identifier: Elastic-2.0
+# "Pipelex" is a trademark of Evotis S.A.S.
+
+from pydantic import BaseModel
+
+
+class GeneratedImage(BaseModel):
+    # image_format: str = "jpeg"
+    url: str
+    width: int
+    height: int
