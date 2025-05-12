d51a427984b6faa29d844945752e7c7ec91584a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:06:12 +0000

    Add package random-pass-generator/0.1/random_pass_generator-0.1.tar.gz

---
+++ packages/random-pass-generator/random_pass_generator-0.1.tar.gz/random_pass_generator-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+import os
+
+def read_readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='random-pass-generator',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple library to generate random passwords.',
+    long_description=read_readme(),
+    long_description_content_type='text/markdown',
+    author='vsenikzianyati',
+    author_email='fmsg4341@gmail.com',
+    url='https://github.com/dsadasdasdsaas/random-pass-generator',
+)
