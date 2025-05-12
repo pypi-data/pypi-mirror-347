25d08da1e854f1a8f8725ecdcb47f4006863e4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:22 +0000

    Add package wxautox/39.1.7/wxautox-39.1.7-cp311-cp311-win_amd64.whl

---
+++ packages/wxautox/wxautox-39.1.7-cp311-cp311-win_amd64.whl/wxautox/__init__.py
@@ -0,0 +1,46 @@
+from .wx import (
+    WeChat, 
+    Chat,
+    LoginWnd,
+    get_wx_clients
+)
+from .param import WxParam
+
+# pyinstaller
+from . import (
+    exceptions,
+    languages,
+    logger,
+    param,
+    msgs,
+    ui,
+    uia,
+    utils,
+)
+import comtypes.stream
+import pythoncom
+import win32com.client
+import win32process
+import win32clipboard
+import psutil
+import uuid
+from typing import (
+    Union, 
+    List,
+    Dict,
+    Literal,
+    Callable,
+    TYPE_CHECKING
+)
+from concurrent.futures import ThreadPoolExecutor
+from abc import ABC, abstractmethod
+import threading
+import traceback
+import time
+
+__all__ = [
+    'WeChat',
+    'Chat',
+    'WxParam',
+    'get_wx_clients'
+]
