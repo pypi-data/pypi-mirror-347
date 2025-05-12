a260d6b35886d8e74cb6f11cd407b54ebff5bb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 05:03:58 +0000

    Add package VenusAI/0.0.1/venusai-0.0.1.tar.gz

---
+++ packages/VenusAI/venusai-0.0.1.tar.gz/venusai-0.0.1/VenusAI/__init__.py
@@ -0,0 +1,11 @@
+"""
+VenusAI: A Python package for building powerful AI agents.
+"""
+
+__author__ = "Mert Sırakaya"
+__maintainer__ = "https://github.com/fswair"
+__email__ = "contact@tomris.dev"
+__version__ = "0.0.1"
+__license__ = "MIT"
+
+import this
---
+++ packages/VenusAI/venusai-0.0.1.tar.gz/venusai-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='VenusAI',
+    version='0.0.1',
+    author='Mert Sırakaya',
+    maintainer='fswair',
+    maintainer_email='contact@tomris.dev',
+    description='A Python package for building powerful AI agents.',
+    url='https://github.com/fswair/VenusAI',
+    packages=['VenusAI'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ],
+    python_requires='>=3.10',
+)
+    
