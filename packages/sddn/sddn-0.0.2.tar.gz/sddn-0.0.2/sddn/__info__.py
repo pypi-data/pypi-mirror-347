d97527ab72aa917709d666b49573cde391e040a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:23 +0000

    Add package sddn/0.0.2/sddn-0.0.2.tar.gz

---
+++ packages/sddn/sddn-0.0.2.tar.gz/sddn-0.0.2/sddn/__info__.py
@@ -0,0 +1,16 @@
+__version__ = "0.0.2"
+__description__ = "sddn"
+__license__ = "MIT"
+__author__ = "DIYer22"
+__author_email__ = "ylxx@live.com"
+__maintainer__ = "DIYer22"
+__maintainer_email__ = "ylxx@live.com"
+__github_username__ = "DIYer22"
+__url__ = "https://github.com/DIYer22/sddn"
+__support__ = "https://github.com/DIYer22/sddn/issues"
+__classifiers__ = [
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
