dc8d50a255e2cdba950ce8f5e65b5b0b7ffe5028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:04:32 +0000

    Add package bow-api/4.3.0.2831/bow_api-4.3.0.2831-cp310-none-manylinux2010_x86_64.whl

---
+++ packages/bow-api/bow_api-4.3.0.2831-cp310-none-manylinux2010_x86_64.whl/bow_data/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023, Bettering Our Worlds (BOW) Ltd.
+# All Rights Reserved
+# Author: Daniel Camilleri <daniel.camilleri@bow.ltd>
+
+from . bow_common_pb2 import *
+from . bow_robot_sdk_pb2 import *
+from . bow_data_audio_pb2 import *
+from . bow_data_common_pb2 import *
+from . bow_data_exteroception_pb2 import *
+from . bow_data_interoception_pb2 import *
+from . bow_data_tactile_pb2 import *
+from . bow_data_vision_pb2 import *
+from . bow_data_motor_pb2 import *
+from . bow_client_sdk_pb2 import *
+from . bow_robot_pb2 import *
+from . bow_structs_pb2 import *
+from . bow_data_pb2 import *
+from . bow_codec import *
