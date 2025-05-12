e5553cb1d2fa4739f7be28479295597d3e958655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:04:29 +0000

    Add package lavender-data/0.0.12/lavender_data-0.0.12-py3-none-any.whl

---
+++ packages/lavender-data/lavender_data-0.0.12-py3-none-any.whl/lavender_data/server/registries/__init__.py
@@ -0,0 +1,62 @@
+import sys
+import importlib.util
+from pathlib import Path
+from typing import Optional
+from lavender_data.logging import get_logger
+
+from .collater import CollaterRegistry, Collater
+from .filter import FilterRegistry, Filter
+from .preprocessor import PreprocessorRegistry, Preprocessor
+
+__all__ = [
+    "setup_registries",
+    "CollaterRegistry",
+    "Collater",
+    "FilterRegistry",
+    "Filter",
+    "PreprocessorRegistry",
+    "Preprocessor",
+]
+
+
+def import_from_directory(directory: str):
+    logger = get_logger(__name__)
+    for file in Path(directory).glob("*.py"):
+        before = {
+            "preprocessor": PreprocessorRegistry.list(),
+            "filter": FilterRegistry.list(),
+            "collater": CollaterRegistry.list(),
+        }
+
+        mod_name = file.stem
+        spec = importlib.util.spec_from_file_location(mod_name, file)
+        mod = importlib.util.module_from_spec(spec)
+
+        sys.modules[f"lavender_data.server.registries.{mod_name}"] = mod
+        spec.loader.exec_module(mod)
+
+        after = {
+            "preprocessor": PreprocessorRegistry.list(),
+            "filter": FilterRegistry.list(),
+            "collater": CollaterRegistry.list(),
+        }
+        diff = {
+            key: list(set(after[key]) - set(before[key]))
+            for key in ["preprocessor", "filter", "collater"]
+            if set(after[key]) - set(before[key])
+        }
+        logger.info(f"Imported {file}: {diff}")
+
+
+def setup_registries(
+    modules_dir: Optional[str] = None,
+    initialize_preprocessor: bool = True,
+):
+    if modules_dir:
+        import_from_directory(modules_dir)
+
+    FilterRegistry.initialize()
+    CollaterRegistry.initialize()
+
+    if initialize_preprocessor:
+        PreprocessorRegistry.initialize()
---
+++ packages/lavender-data/lavender_data-0.0.12-py3-none-any.whl/lavender_data/ui/packages/lavender-data-ui/.next/static/chunks/pages/_app-29b4be31bd20dcb4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6704:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(5508)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6704),n(6594))),_N_E=_.O()}]);
