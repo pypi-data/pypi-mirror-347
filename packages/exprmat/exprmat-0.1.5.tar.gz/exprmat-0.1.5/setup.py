5235577b768eee6885fcc9cd823b30d70c12d40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 03:22:27 +0000

    Add package exprmat/0.1.5/exprmat-0.1.5.tar.gz

---
+++ packages/exprmat/exprmat-0.1.5.tar.gz/exprmat-0.1.5/setup.py
@@ -0,0 +1,45 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name                 = 'exprmat',
+    version              = '0.1.5',
+    description          = 'routines to process expression matrices',
+    url                  = 'https://github.com/yang-z-03/exprmat',
+    author               = 'Zheng Yang',
+    author_email         = 'xornent@outlook.com',
+    license              = 'GPLv3',
+    packages             = find_packages(),
+    install_requires     = [
+        'umap-learn >= 0.5.0',
+        'scvi-tools >= 1.3.0',
+        'scanpy',
+        'anndata',
+        'mudata',
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'torch >= 2.0',
+        'annoy',
+        'scikit-image',
+        'scikit-misc',
+        'igraph',
+        'bbknn',
+        'scanorama',
+        'harmonypy',
+        'pymde',
+        'datashader',
+        'fa2_modified',
+        'pynndescent',
+        'gseapy'
+    ],
+    include_package_data = False,
+    package_data         = {
+        'exprmat.data': [
+            'mmu/*.tsv.gz', 
+            'hsa/*.tsv.gz'
+        ]
+    },
+    zip_safe             = False
+)
