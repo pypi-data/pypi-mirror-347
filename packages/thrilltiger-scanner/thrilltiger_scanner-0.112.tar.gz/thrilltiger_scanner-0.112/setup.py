ebd0e5d41bbfca2e0bb0a8048a090301017ed224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:04:31 +0000

    Add package thrilltiger-scanner/0.112/thrilltiger_scanner-0.112.tar.gz

---
+++ packages/thrilltiger-scanner/thrilltiger_scanner-0.112.tar.gz/thrilltiger_scanner-0.112/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='thrilltiger_scanner',
+	version='0.112',
+	packages=['portscanner'],
+	install_requires=[],
+	entry_points={
+		'console_scripts': [
+			'portscanner = portscanner.portscanner:main'
+		]
+	}
+)
