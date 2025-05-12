44d496d53d58feaf6028f8798f0dce0122d72653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 14:05:30 +0000

    Add package flexeval/0.11.1/flexeval-0.11.1-py3-none-any.whl

---
+++ packages/flexeval/flexeval-0.11.1-py3-none-any.whl/flexeval/core/chat_dataset/__init__.py
@@ -0,0 +1,4 @@
+from .base import ChatDataset, ChatInstance
+from .chatbot_bench import ChatbotBench
+from .sacrebleu_dataset import SacreBleuChatDataset
+from .template_based import HFChatDataset, JsonlChatDataset, TemplateChatDataset
