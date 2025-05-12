d405e0c37f16880dd1135f6776cb6d9ac3427434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:38 +0000

    Add package kodexa/8.0.14958192442/kodexa-8.0.14958192442.tar.gz

---
+++ packages/kodexa/kodexa-8.0.14958192442.tar.gz/kodexa-8.0.14958192442/kodexa/steps/__init__.py
@@ -0,0 +1,12 @@
+"""
+Some example steps that can be used locally
+"""
+from .common import (
+    TextParser,
+    RollupTransformer,
+    KodexaProcessingException,
+)
+from kodexa_document.steps import (
+    NodeTagger,
+    NodeTagCopy,
+)
