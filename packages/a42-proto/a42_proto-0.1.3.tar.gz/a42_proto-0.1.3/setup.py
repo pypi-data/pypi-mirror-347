440be585202231aea193800d676cc1271942966e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 16:04:44 +0000

    Add package a42-proto/0.1.3/a42_proto-0.1.3.tar.gz

---
+++ packages/a42-proto/a42_proto-0.1.3.tar.gz/a42_proto-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="a42_proto",
+    version="0.1.3",
+    author="Alexander Baumann",
+    author_email="alexander.baumann@hs-esslingen.de",
+    description="Protobuf Python bindings for A42 sensor data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HSE-VSV/DataReaderA42",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "protobuf==3.20.3",
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
