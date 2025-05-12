7d20eedf77f77efe9c4467be982e1fa53cbb40e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 04:05:26 +0000

    Add package nonebot-plugin-resolver2/1.8.9/nonebot_plugin_resolver2-1.8.9.tar.gz

---
+++ packages/nonebot-plugin-resolver2/nonebot_plugin_resolver2-1.8.9.tar.gz/nonebot_plugin_resolver2-1.8.9/nonebot_plugin_resolver2/matchers/__init__.py
@@ -0,0 +1,25 @@
+from nonebot.matcher import Matcher
+
+from .acfun import acfun
+from .bilibili import bilibili
+from .douyin import douyin
+from .kugou import kugou
+from .ncm import ncm
+from .tiktok import tiktok
+from .twitter import twitter
+from .weibo import weibo
+from .xiaohongshu import xiaohongshu
+from .ytb import ytb
+
+resolvers: dict[str, type[Matcher]] = {
+    "bilibili": bilibili,
+    "acfun": acfun,
+    "douyin": douyin,
+    "ytb": ytb,
+    "kugou": kugou,
+    "ncm": ncm,
+    "twitter": twitter,
+    "tiktok": tiktok,
+    "weibo": weibo,
+    "xiaohongshu": xiaohongshu,
+}
---
+++ packages/nonebot-plugin-resolver2/nonebot_plugin_resolver2-1.8.9.tar.gz/nonebot_plugin_resolver2-1.8.9/tests/test_acfun.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from nonebot import logger
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_parse_acfun_url():
+    from nonebot_plugin_resolver2.parsers import AcfunParser
+
+    urls = ["https://www.acfun.cn/v/ac46593564", "https://www.acfun.cn/v/ac40867941"]
+    acfun_parser = AcfunParser()
+
+    async def parse_acfun_url(url: str) -> None:
+        acid = int(url.split("/")[-1].split("ac")[1])
+        logger.info(f"{url} | 开始解析视频 acid: {acid}")
+        m3u8s_url, video_desc = await acfun_parser.parse_url(url)
+        assert m3u8s_url
+        assert video_desc
+        logger.debug(f"{url} | m3u8s_url: {m3u8s_url}, video_desc: {video_desc}")
+
+        logger.info(f"{url} | 开始下载视频")
+        video_file = await acfun_parser.download_video(m3u8s_url, acid)
+        assert video_file
+        logger.info(f"{url} | 视频下载成功, 文件大小: {video_file.stat().st_size / 1024 / 1024:.2f} MB")
+
+    await asyncio.gather(*[parse_acfun_url(url) for url in urls])
