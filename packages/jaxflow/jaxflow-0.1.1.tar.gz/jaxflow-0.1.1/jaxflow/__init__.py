f417ad31663b3dc438b5d28e98eb74a8192193e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 05:03:47 +0000

    Add package jaxflow/0.1.1/jaxflow-0.1.1.tar.gz

---
+++ packages/jaxflow/jaxflow-0.1.1.tar.gz/jaxflow-0.1.1/jaxflow/__init__.py
@@ -0,0 +1,13 @@
+from .callbacks import callbacks
+from .core.Variable import Variable
+from .Gradient import Gradient
+from .initializers import initializers
+from .layers import *
+from . import losses
+from .optimizers import optimizers
+from . import math 
+from . import models
+
+
+
+__version__ = "0.1.1"
