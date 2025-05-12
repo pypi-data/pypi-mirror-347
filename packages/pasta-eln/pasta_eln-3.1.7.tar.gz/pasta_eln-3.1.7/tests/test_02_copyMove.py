184a40dab59ca1dee5fc3369f60829643936dc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 16:04:41 +0000

    Add package pasta-eln/3.1.7/pasta_eln-3.1.7.tar.gz

---
+++ packages/pasta-eln/pasta_eln-3.1.7.tar.gz/pasta_eln-3.1.7/tests/test_02_copyMove.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+"""TEST using the FULL set of python-requirements: randomly move,copy,delete files to check resistance of scanning """
+import os, shutil, logging, random
+import warnings
+import unittest
+from pathlib import Path
+from pasta_eln.backend import Backend
+from pasta_eln.textTools.stringChanges import outputString
+from pasta_eln.miscTools import DummyProgressBar
+from pasta_eln.installationTools import exampleData
+
+class TestStringMethods(unittest.TestCase):
+  """
+  derived class for this test
+  """
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.be = None
+    self.dirName = ''
+
+  def test_main(self):
+    """
+    main function
+    """
+    # initialization: create database, destroy on filesystem and database and then create new one
+    warnings.filterwarnings('ignore', message='numpy.ufunc size changed')
+    warnings.filterwarnings('ignore', message='invalid escape sequence')
+    warnings.filterwarnings('ignore', category=ResourceWarning, module='PIL')
+    warnings.filterwarnings('ignore', category=ImportWarning)
+    logPath = Path.home()/'pastaELN.log'
+    logging.basicConfig(filename=logPath, level=logging.INFO, format='%(asctime)s|%(levelname)s:%(message)s',
+                        datefmt='%m-%d %H:%M:%S')   #This logging is always info, since for installation only
+    for package in ['urllib3', 'requests', 'asyncio', 'PIL', 'matplotlib.font_manager']:
+      logging.getLogger(package).setLevel(logging.WARNING)
+    logging.info('Start 02 test')
+    self.be = Backend('research')
+    projID = self.be.output('x0').split('|')[-1].strip()
+    self.be.changeHierarchy(projID)
+
+    choices = random.choices(range(100), k=250)
+    # choices =  [34,83,40,8,70,1,14,14,60,42,13,99,94,47,14,7,45,85,96,80,91,35,33,85,85,38,63,1,89,62,24,47,82,1,13,4,97,46,6,82,97,76,5,14,69,7,50,14,33,8,15,69,99,75,69,68,9,70,56,75,44,60,97,93,72,50,46,89,60,63,65,82,50,54,33,6,77,52,72,90,82,1,58,11,81,76,9,3,83,40,26,45,88,56,10,74,66,16,5,31,27,62,65,65,56,7,80,43,56,15,93,36,49,74,15,73,5,80,35,46,17,31,79,51,20,81,5,50,49,31,59,9,28,9,33,65,81,85,10,78,64,79,58,14,99,35,86,98,95,0]
+    print(f'Current choice: [{",".join([str(i) for i in choices])}]')
+    for epoch in range(5):
+      print(f'\nStart epoch: {epoch}')
+      allDirs = []
+      allFiles = []
+      for root, _, files in os.walk(self.be.basePath):
+        if root.endswith('StandardOperatingProcedures') or root==str(self.be.basePath):
+          continue
+        allDirs.append(root)
+        allFiles += [f'{root}{os.sep}{i}' for i in files if i not in ('.id_pastaELN.json','pastaELN.db')]
+      for iFile in allFiles:
+        op = ['keep','copy','copy','move','move','delete'][choices.pop(0)%6]
+        parent = Path(iFile).parent
+        possTargets = [i for i in allDirs if Path(i)!=parent]
+        target = possTargets[choices.pop(0)%len(possTargets)]
+        if choices.pop(0)%4==0:
+          target += f'/NewDirectory/{Path(iFile).name}'
+        print(iFile[len(str(self.be.basePath))+1:], op, target[len(str(self.be.basePath))+1:])
+        try:
+          if op=='keep':
+            continue
+          elif op=='copy':
+            if not Path(target).parent.is_dir():
+              Path(target).parent.mkdir()
+            shutil.copy(iFile, target)
+          elif op=='move':
+            if not Path(target).parent.is_dir():
+              Path(target).parent.mkdir()
+            shutil.move(iFile, target)
+          elif op=='delete':
+            os.unlink(iFile)
+        except shutil.Error:
+          pass  #skip step if invalid step because file already exists in that folder
+      if choices.pop(0)%2==1:
+        print('Start scanning')
+        self.be.scanProject(None, projID)
+      if choices.pop(0)%2==1:
+        print('Start scanning')
+        self.be.scanProject(None, projID)
+    print('Final scanning and verification')
+    self.be.scanProject(None, projID)
+    self.verify()
+    self.be.changeHierarchy(projID)
+    print(self.be.outputHierarchy(False))
+    print(f'{"*"*40}\nEND TEST 02 \n{"*"*40}')
+    return
+
+  def verify(self):
+    #Verify DB
+    output = self.be.checkDB(outputStyle='text')
+    output = '\n'.join(output.split('\n')[8:])
+    print(output)
+    self.assertNotIn('**ERROR', output, 'Error in checkDB')
+    self.assertLessEqual(len(output.split('\n')), 6, 'Check db should have less than 6 almost empty lines')
+    return
+
+  def tearDown(self):
+    logging.info('End 3Projects test')
+    return
+
+if __name__ == '__main__':
+  unittest.main()
