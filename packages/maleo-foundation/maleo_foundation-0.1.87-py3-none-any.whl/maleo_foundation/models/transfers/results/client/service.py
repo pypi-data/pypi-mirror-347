bf31ea368253da05365695a72035f85cb3e9d235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 15:05:13 +0000

    Add package maleo-foundation/0.1.87/maleo_foundation-0.1.87-py3-none-any.whl

---
+++ packages/maleo-foundation/maleo_foundation-0.1.87-py3-none-any.whl/maleo_foundation/models/transfers/results/client/service.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from pydantic import model_validator
+from maleo_foundation.models.schemas.general import BaseGeneralSchemas
+from maleo_foundation.models.schemas.result import BaseResultSchemas
+
+class BaseClientServiceResultsTransfers:
+    class Fail(BaseResultSchemas.Fail): pass
+
+    class NoData(BaseResultSchemas.NoData): pass
+
+    class SingleData(BaseResultSchemas.SingleData): pass
+
+    class UnpaginatedMultipleData(BaseResultSchemas.UnpaginatedMultipleData): pass
+
+    class PaginatedMultipleData(BaseResultSchemas.PaginatedMultipleData):
+        @model_validator(mode="before")
+        @classmethod
+        def calculate_pagination_component(cls, values: dict) -> dict:
+            """Extracts pagination components (page, limit, total_data) before validation."""
+            pagination = values.get("pagination")
+
+            if isinstance(pagination, BaseGeneralSchemas.ExtendedPagination):
+                values["page"] = pagination.page
+                values["limit"] = pagination.limit
+                values["total_data"] = pagination.total_data
+
+            return values
