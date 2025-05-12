632533283d6ae46540ee32918f938626d6783010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-12 15:05:05 +0000

    Add package PermutiveAPI/5.0.3/permutiveapi-5.0.3.tar.gz

---
+++ packages/PermutiveAPI/permutiveapi-5.0.3.tar.gz/permutiveapi-5.0.3/PermutiveAPI/__init__.py
@@ -0,0 +1,11 @@
+
+from PermutiveAPI.Import import Import, ImportList, Segment, SegmentList
+from PermutiveAPI.User import Identity, Alias
+from PermutiveAPI.Cohort import Cohort, CohortList
+from PermutiveAPI.Workspace import Workspace, WorkspaceList
+from PermutiveAPI.Utils import customJSONEncoder
+
+
+
+__all__ = ["Cohort", "CohortList", "Import", "ImportList", "Segment", "SegmentList",
+           "Workspace", "WorkspaceList", "Identity", "Alias", "customJSONEncoder"]
