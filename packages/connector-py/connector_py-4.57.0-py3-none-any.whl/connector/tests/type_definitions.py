3e57dfd1103fa5d6da98e886dbc8b9797dfbc138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:15 +0000

    Add package connector-py/4.57.0/connector_py-4.57.0-py3-none-any.whl

---
+++ packages/connector-py/connector_py-4.57.0-py3-none-any.whl/connector/tests/type_definitions.py
@@ -0,0 +1,22 @@
+import dataclasses
+import typing as t
+
+import httpx
+
+
+@dataclasses.dataclass
+class MockedResponse:
+    status_code: httpx.codes
+    response_body: dict[str, t.Any] | list[t.Any] | str | None
+    headers: dict[str, str] | None = None
+    request_json_body: dict[
+        str, t.Any
+    ] | None = None  # for cases where we want to match the request body
+
+
+ResponseBodyMap: t.TypeAlias = (
+    t.Mapping[str, t.Mapping[str, MockedResponse] | list[MockedResponse]] | None
+)
+# ResponseBodyMap: t.TypeAlias = dict[str, dict[str, MockedResponse]] | None
+RequestHandler: t.TypeAlias = t.Callable[[httpx.Request], httpx.Response]
+ClientContextManager: t.TypeAlias = t.Callable[[str, str, RequestHandler], t.ContextManager[None]]
