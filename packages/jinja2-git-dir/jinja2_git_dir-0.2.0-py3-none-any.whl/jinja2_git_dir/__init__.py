1c8a5f32c348aa4691c34bc81e24b43a5cf03c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:36 +0000

    Add package jinja2-git-dir/0.2.0/jinja2_git_dir-0.2.0-py3-none-any.whl

---
+++ packages/jinja2-git-dir/jinja2_git_dir-0.2.0-py3-none-any.whl/jinja2_git_dir/__init__.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from subprocess import CalledProcessError, CompletedProcess, run
+
+from jinja2.environment import Environment
+from jinja2.ext import Extension
+
+
+def _git_dir(git_path: str) -> bool:
+    # Utilize Path() to sanitize the input and resolve to an absolute path
+    try:
+        git_path = str(Path(git_path).resolve())
+    except TypeError:
+        return False
+    command: list[str] = ["git", "-C", git_path, "rev-parse", "--show-toplevel"]
+    try:
+        result: CompletedProcess[str] = run(command, check=True, capture_output=True, text=True)  # noqa: S603
+        return result.stdout.lower() == git_path.lower()
+    except CalledProcessError:
+        return False
+
+
+class GitDirectoryExtension(Extension):
+    def __init__(self, environment: Environment) -> None:
+        super().__init__(environment)
+        environment.filters["gitdir"] = _git_dir
