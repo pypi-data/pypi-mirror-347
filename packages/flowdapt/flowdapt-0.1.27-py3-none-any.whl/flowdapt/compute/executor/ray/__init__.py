9c6ebd5be5b8f9bf5bcc027543e33ac1664b2380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:33 +0000

    Add package flowdapt/0.1.27/flowdapt-0.1.27-py3-none-any.whl

---
+++ packages/flowdapt/flowdapt-0.1.27-py3-none-any.whl/flowdapt/compute/executor/ray/__init__.py
@@ -0,0 +1,9 @@
+from flowdapt.compute.executor.ray.cluster_memory import RayClusterMemory
+from flowdapt.compute.executor.ray.executor import ExecuteStrategy, RayExecutor
+
+
+__all__ = (
+    "RayExecutor",
+    "RayClusterMemory",
+    "ExecuteStrategy",
+)
