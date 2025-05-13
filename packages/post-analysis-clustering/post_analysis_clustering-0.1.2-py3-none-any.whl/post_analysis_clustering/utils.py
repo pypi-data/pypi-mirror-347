d683937d53402db90d394d5ee11a269d53e2d8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 16:05:35 +0000

    Add package post-analysis-clustering/0.1.2/post_analysis_clustering-0.1.2-py3-none-any.whl

---
+++ packages/post-analysis-clustering/post_analysis_clustering-0.1.2-py3-none-any.whl/post_analysis_clustering/utils.py
@@ -0,0 +1,12 @@
+from functools import wraps
+import time 
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time for {func.__name__}: {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
