17a324ea0b0352b410288b3ff6a8b7582e9ca198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 04:16:16 +0000

    Add package ckanext-chat/0.1.0a0/ckanext_chat-0.1.0a0.tar.gz

---
+++ packages/ckanext-chat/ckanext_chat-0.1.0a0.tar.gz/ckanext_chat-0.1.0a0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from codecs import open  # To use a consistent encoding
+from os import path, environ
+
+from setuptools import find_packages, setup  # Always prefer setuptools over distutils
+
+here = path.abspath(path.dirname(__file__))
+
+
+# Get the long description from the relevant file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="""ckanext-chat""",
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version=environ.get('VERSION', '0.0.0'),
+    description="""Extension adds a pydantic ai chat interface to CKAN, that can run actions with user aware context.""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # The project's main homepage.
+    url="https://github.com/Mat-O-Lab/ckanext-chat",
+    # Author details
+    author="""Thomas Hanke""",
+    author_email="""thomas.hanke@iwm.fraunhofer.de""",
+    # Choose your license
+    license="AGPL",
+    message_extractors={
+        "ckanext": [
+            ("**.py", "python", None),
+            ("**.js", "javascript", None),
+            ("**/templates/**.html", "ckan", None),
+        ],
+    },
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points="""
+        [ckan.plugins]
+        chat=ckanext.chat.plugin:ChatPlugin
+    """,
+)
