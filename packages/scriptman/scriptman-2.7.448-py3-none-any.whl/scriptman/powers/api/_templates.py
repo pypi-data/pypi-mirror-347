be6c1f44cacbb975d79bf7deabc03601695e84f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:34 +0000

    Add package scriptman/2.7.448/scriptman-2.7.448-py3-none-any.whl

---
+++ packages/scriptman/scriptman-2.7.448-py3-none-any.whl/scriptman/powers/api/_templates.py
@@ -0,0 +1,131 @@
+try:
+    from asyncio import iscoroutinefunction
+    from functools import wraps
+    from json import dumps
+    from typing import Any, cast
+
+    from fastapi import status
+    from fastapi.responses import JSONResponse
+    from loguru import logger
+    from pydantic import BaseModel
+
+    from scriptman.powers.api._models import APIRequest, APIResponse
+    from scriptman.powers.api.exceptions import APIException
+    from scriptman.powers.generics import Func, P
+    from scriptman.powers.task import TaskExecutor
+except ImportError as e:
+    raise ImportError(
+        f"An error occurred: {e} \n"
+        "Kindly install the dependencies using 'scriptman[api]'."
+    )
+
+
+def create_successful_response(
+    request: APIRequest, response: dict[str, Any]
+) -> dict[str, Any]:
+    """
+    ✅ Creates a successful response for a completed API request.
+
+    Args:
+        request (APIRequest): The completed API request.
+        response (dict): The response data.
+
+    Returns:
+        dict[str, Any]: Formatted successful API response data.
+    """
+    logger.info(f"✅ Request {request.request_id} completed successfully.")
+    logger.debug(f"📤 Request details: \n{dumps(request.model_dump(), indent=4)}")
+    return APIResponse(
+        request=request,
+        response=response.get("response", response),
+        message=response.get("message", "Request Successful."),
+        status_code=response.get("status_code", status.HTTP_200_OK),
+    ).model_dump(exclude={"stacktrace"})
+
+
+def create_timeout_response(request: APIRequest, task_id: str) -> dict[str, Any]:
+    """
+    ⏳ Creates a timeout response when a request exceeds the expected duration.
+
+    Args:
+        request (APIRequest): The API request that timed out.
+        task_id (str): Identifier for the background task.
+
+    Returns:
+        dict[str, Any]: Formatted timeout API response data.
+    """
+    logger.warning(f"⏳ Request task {task_id} timed out. Continuing in background...")
+    logger.debug(f"📤 Request details: \n{dumps(request.model_dump(), indent=4)}")
+    return APIResponse(
+        request=request,
+        status_code=status.HTTP_202_ACCEPTED,
+        response={"status": "processing", "task_id": task_id},
+        message="Request is taking longer than expected. Please try again later.",
+    ).model_dump(exclude={"stacktrace"})
+
+
+def create_error_response(request: APIRequest, e: Exception) -> dict[str, Any]:
+    """
+    💥 Creates an error response for a failed API request.
+
+    Args:
+        request (APIRequest): The API request that encountered an error.
+        e (Exception): The raised exception.
+
+    Returns:
+        dict[str, Any]: Formatted error API response data.
+    """
+    if not isinstance(e, APIException):
+        e = APIException(f"{e.__class__.__name__}: {str(e)}", exception=e)
+
+    logger.error(f"💥 Request {request.request_id} failed -> {e.message}")
+    logger.debug(f"📤 Request details: \n{dumps(request.model_dump(), indent=4)}")
+    logger.debug(f"🔍 Stacktrace: \n{dumps(e.stacktrace, indent=4)}")
+
+    return APIResponse.from_api_exception(request, e).model_dump()
+
+
+def api_route(func: Func[P, dict[str, Any]]) -> Func[P, JSONResponse]:
+    """
+    🔄 Decorator to wrap synchronous or asynchronous API route functions with
+    standardized request/response handling.
+
+    Args:
+        func (Func): The synchronous function to wrap.
+
+    Returns:
+        Func: Wrapped function with error and success handling.
+    """
+
+    @wraps(func)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Any:
+        request = APIRequest()
+        try:
+            if iscoroutinefunction(func):
+                result = TaskExecutor.await_async(func(*args, **kwargs))
+                result = cast(dict[str, Any], result)
+            else:
+                result = cast(dict[str, Any], func(*args, **kwargs))
+
+            # TODO: Improve Pickling of Stuff
+            if isinstance(result, dict):
+                for key, value in result.items():
+                    try:
+                        dumps(value)
+                    except (TypeError, OverflowError):
+                        logger.debug(
+                            f"Converting non-serializable value {value} "
+                            f"for key '{key}' to string"
+                        )
+                        if isinstance(value, BaseModel):
+                            result[key] = value.model_dump_json()
+                        else:
+                            result[key] = str(value)
+
+            response = create_successful_response(request=request, response=result)
+            return JSONResponse(content=response, status_code=response["status_code"])
+        except Exception as e:
+            response = create_error_response(request=request, e=e)
+            return JSONResponse(content=response, status_code=response["status_code"])
+
+    return wrapper
