0c77f19d5d60b5c9912d67f6be5326f8fd71c78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:40 +0000

    Add package genvarloader/0.14.4/genvarloader-0.14.4-cp313-cp313t-musllinux_1_2_aarch64.whl

---
+++ packages/genvarloader/genvarloader-0.14.4-cp313-cp313t-musllinux_1_2_aarch64.whl/genvarloader/__init__.py
@@ -0,0 +1,24 @@
+import importlib.metadata
+
+from ._bigwig import BigWigs
+from ._dataset._impl import ArrayDataset, Dataset, RaggedDataset
+from ._dataset._reconstruct import Reference
+from ._dataset._write import write
+from ._dummy import get_dummy_dataset
+from ._ragged import Ragged
+from ._utils import read_bedlike, with_length
+
+__version__ = importlib.metadata.version("genvarloader")
+
+__all__ = [
+    "write",
+    "Dataset",
+    "BigWigs",
+    "read_bedlike",
+    "with_length",
+    "Ragged",
+    "get_dummy_dataset",
+    "ArrayDataset",
+    "RaggedDataset",
+    "Reference",
+]
