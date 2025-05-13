2470f782531be7511c9f737e32b5f51e3f6f8d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 15:05:10 +0000

    Add package projectdavid-common/0.17.0/projectdavid_common-0.17.0-py3-none-any.whl

---
+++ packages/projectdavid-common/projectdavid_common-0.17.0-py3-none-any.whl/projectdavid_common/schemas/enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class ProviderEnum(str, Enum):
+    openai = "openai"
+    deepseek = "deepseek"
+    hyperbolic = "Hyperbolic"
+    togetherai = "togetherai"
+    local = "local"
+
+
+class StatusEnum(str, Enum):
+    deleted = "deleted"
+    active = "active"
+    queued = "queued"
+    in_progress = "in_progress"
+    pending_action = "action_required"
+    completed = "completed"
+    failed = "failed"
+    cancelling = "cancelling"
+    cancelled = "cancelled"
+    pending = "pending"
+    processing = "processing"
+    expired = "expired"
+    retrying = "retrying"
