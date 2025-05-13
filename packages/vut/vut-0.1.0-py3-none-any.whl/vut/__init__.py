d25b8b860d0d3c178f1c342765f87a44720fd703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 16:06:31 +0000

    Add package vut/0.1.0/vut-0.1.0-py3-none-any.whl

---
+++ packages/vut/vut-0.1.0-py3-none-any.whl/vut/__init__.py
@@ -0,0 +1,3 @@
+from .util import init_seed
+
+__all__ = ["init_seed"]
---
+++ packages/vut/vut-0.1.0-py3-none-any.whl/vut/util.py
@@ -0,0 +1,16 @@
+import random
+
+import numpy as np
+import torch
+
+
+def init_seed(seed: int = 42):
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.cuda.manual_seed(seed)
