e4a57e968c89811577dd6830d4bc5a1b3800c9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 08:04:38 +0000

    Add package pybgproutesapi/0.1.3/pybgproutesapi-0.1.3.tar.gz

---
+++ packages/pybgproutesapi/pybgproutesapi-0.1.3.tar.gz/pybgproutesapi-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pybgproutesapi",
+    version="0.1.3",
+    description="Python bindings for the bgproutes.io API",
+    author="Thomas Holterbach and Thomas Alfroy",
+    author_email="contact@bgproutes.io",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
