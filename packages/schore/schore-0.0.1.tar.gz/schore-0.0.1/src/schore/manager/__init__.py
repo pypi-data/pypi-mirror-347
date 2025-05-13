5bd309803f95ad9895a53ace398670b39bc2d4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 04:16:11 +0000

    Add package schore/0.0.1/schore-0.0.1.tar.gz

---
+++ packages/schore/schore-0.0.1.tar.gz/schore-0.0.1/src/schore/manager/__init__.py
@@ -0,0 +1,8 @@
+from .base import DfManager, Table2DManager
+from .processing_time_manager import JobStageProcessingTimeManager
+
+__all__ = [
+    "DfManager",
+    "Table2DManager",
+    "JobStageProcessingTimeManager",
+]
