1fdfa07cfc2d7cc5d57f9ffd9963d8999d0e9adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 16:05:00 +0000

    Add package uv-vapi-runtime/2.44.1/uv_vapi_runtime-2.44.1.tar.gz

---
+++ packages/uv-vapi-runtime/uv_vapi_runtime-2.44.1.tar.gz/uv_vapi_runtime-2.44.1/README.md
@@ -0,0 +1,19 @@
+# vAPI Runtime
+ 
+vAPI Runtime is a client library which facilitates invocations of the vSphere Automation APIs from a Python application. It contains HTTP client, API messages serializers, and API input/output validators and converters to native Python types.
+ 
+This library is required by other SDK modules that contain Python language binding for specific APIs and is installed automatically with them.
+ 
+[Source code](https://github.com/vmware/vsphere-automation-sdk-python/tree/master/lib/src/vapi-runtime) | [Package (PyPI)](https://pypi.org/project/vapi-runtime/)
+ 
+## Getting started
+ 
+### Prerequisites
+ 
+- Python 3.8+ is required to use this package.
+ 
+### Install the package
+ 
+```bash
+pip install vapi-runtime
+```
---
+++ packages/uv-vapi-runtime/uv_vapi_runtime-2.44.1.tar.gz/uv_vapi_runtime-2.44.1/setup.py
@@ -0,0 +1,51 @@
+"""
+Setup file for creating Runtime package
+"""
+__author__ = 'VMware, Inc.'
+__copyright__ = 'Copyright 2015, 2017-2020, 2022 VMware, Inc.  All rights reserved. -- VMware Confidential'  # pylint: disable=line-too-long
+
+import os
+
+from egg_utils import get_command_options_for_custom_egg_name  # pylint: disable=import-error
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages  # pylint: disable=ungrouped-imports
+
+name = 'uv_vapi_runtime'
+
+version = '2.44.1'
+
+setup(
+    name=name,
+    version=version,
+    description='vAPI Runtime',
+    classifiers=[],
+    keywords='VMware',
+    author='VMware',
+    namespace_packages=['vmware'],
+    data_files=[('', ['requirements.txt'])],
+    packages=find_packages(),
+    package_data={
+        'vmware': ['vapi/settings/*.properties'],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['pyOpenSSL',
+                      'requests>=2.0.0,<3.0.0',
+                      'six>=1.0.0,<2.0.0'],
+    extras_require={
+        'twisted': ['twisted>=18.0.0', 'service_identity>=18.0.0',
+                    'lxml>=4.3.0', 'werkzeug>=0.14.1', 'urllib3>=1.25.1'],
+        'server': ['lxml>=4.3.0', 'werkzeug>=0.14.1', 'urllib3>=1.25.1'],
+    },
+    entry_points={
+        'console_scripts': [
+            'vapi-server = vmware.vapi.server.vapid:main'
+        ]
+    },
+    command_options=get_command_options_for_custom_egg_name(name, version)
+)
