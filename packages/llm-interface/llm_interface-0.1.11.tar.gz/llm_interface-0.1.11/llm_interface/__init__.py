233fa67fb8abd80482c58c4249f5845f29d86761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:23 +0000

    Add package llm-interface/0.1.11/llm_interface-0.1.11.tar.gz

---
+++ packages/llm-interface/llm_interface-0.1.11.tar.gz/llm_interface-0.1.11/llm_interface/__init__.py
@@ -0,0 +1,16 @@
+from ollama import ListResponse
+
+from .llm_config import llm_from_config
+from .llm_interface import LLMInterface, ModelError
+from .llm_tool import tool
+from .token_usage import TokenUsage
+
+__version__ = "0.1.11"
+__all__ = [
+    "LLMInterface",
+    "llm_from_config",
+    "tool",
+    "ListResponse",
+    "TokenUsage",
+    "ModelError",
+]
