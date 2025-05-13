e83308a1c53d8a9ae44adb979e539f9cc675bda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 04:16:16 +0000

    Add package terapilot/1.0.2/terapilot-1.0.2.tar.gz

---
+++ packages/terapilot/terapilot-1.0.2.tar.gz/terapilot-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terapilot",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        'cohere>=4.0',
+        'requests>=2.25',
+        'python-dotenv>=0.19',
+        'pathlib>=1.0; python_version < "3.4"'
+    ],
+    entry_points={
+        'console_scripts': [
+            'terapilot=terapilot.cli:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
