4c2e4afa6688658f4d8361f7c38278a8c238cbcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:24 +0000

    Add package kececifractals/0.1.0/kececifractals-0.1.0.tar.gz

---
+++ packages/kececifractals/kececifractals-0.1.0.tar.gz/kececifractals-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="kececifractals",
+    version="0.1.0",
+    description="eçeci Fractals: Keçeci-style circle fractal.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Mehmet Keçeci",
+    maintainer="Mehmet Keçeci",
+    author_email="bilginomi@yaani.com",
+    maintainer_email="bilginomi@yaani.com",
+    url="https://github.com/WhiteSymmetry/kececifractals",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+    extras_require={
+
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    license="MIT",
+)
