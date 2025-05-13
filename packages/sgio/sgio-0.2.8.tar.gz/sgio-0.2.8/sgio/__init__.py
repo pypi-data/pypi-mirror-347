fc073755b7b3efff2fc57930a7caf55238ddc278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 15:04:52 +0000

    Add package sgio/0.2.8/sgio-0.2.8.tar.gz

---
+++ packages/sgio/sgio-0.2.8.tar.gz/sgio-0.2.8/sgio/__init__.py
@@ -0,0 +1,74 @@
+from ._version import __version__
+
+from ._global import logger, configure_logging
+
+from ._exceptions import (
+    SwiftCompLicenseError,
+    VABSLicenseError,
+    SwiftCompIOError,
+    VABSIOError,
+    SwiftCompError,
+    VABSError,
+    OutputFileError,
+)
+
+from .core import (
+    StructureGene,
+    check_isolated_nodes,
+    renumber_elements,
+    )
+from .core.builder import buildSG1D
+# from .core.solid import MaterialProperty
+# from .core.shell import ShellProperty
+# from .core.beam import BeamProperty
+from .core.merge import combineSG
+
+from .model import *
+
+from .iofunc import (
+    read,
+    readOutput,
+    readOutputModel,
+    readOutputState,
+    write,
+    convert,
+    readLoadCsv,
+    addCellDictDataToMesh,
+    addPointDictDataToMesh,
+    # write_points_cells,
+    # register_format,
+    # deregister_format,
+    # extension_to_filetypes,
+    # sg_reader_map,
+    # sg_writer_map,
+    # overridden_formats
+)
+
+from .execu import run
+
+# from . import utils
+
+# from .meshio import *
+
+__all__ = [
+    "configure_logging",
+    "read",
+    "readOutput",
+    "readOutputModel",
+    "readOutputState",
+    "write",
+    "convert",
+    "readLoadCsv",
+    'run',
+    'buildSG1D',
+    'combineSG'
+    "SGMesh",
+    'StructureGene',
+    "SwiftCompLicenseError",
+    "VABSLicenseError",
+    "SwiftCompIOError",
+    "VABSIOError",
+    "SwiftCompError",
+    "VABSError",
+    "OutputFileError",
+]
---
+++ packages/sgio/sgio-0.2.8.tar.gz/sgio-0.2.8/sgio/iofunc/vabs/__init__.py
@@ -0,0 +1,8 @@
+from ._output import (
+    _readOutputElementStrainStressCase,
+    )
+from .main import (
+    read_buffer,
+    read_output_buffer,
+    write_buffer,
+    )
