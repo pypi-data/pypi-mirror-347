e0f564ea5cabb94ca1be388f64d7981830ff4a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:43 +0000

    Add package policyengine-us/1.270.1/policyengine_us-1.270.1.tar.gz

---
+++ packages/policyengine-us/policyengine_us-1.270.1.tar.gz/policyengine_us-1.270.1/policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/charitable_deduction.py
@@ -0,0 +1,32 @@
+from policyengine_us.model_api import *
+
+
+class charitable_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Charitable deduction"
+    unit = USD
+    documentation = "Deduction from taxable income for charitable donations."
+    definition_period = YEAR
+    reference = "https://www.law.cornell.edu/uscode/text/26/170"
+
+    def formula(tax_unit, period, parameters):
+        cash_donations = add(tax_unit, period, ["charitable_cash_donations"])
+        non_cash_donations = add(
+            tax_unit, period, ["charitable_non_cash_donations"]
+        )
+        positive_agi = tax_unit("positive_agi", period)
+        p = parameters(period).gov.irs.deductions.itemized.charity.ceiling
+        capped_non_cash_donations = min_(
+            non_cash_donations, p.non_cash * positive_agi
+        )
+        charitable_deduction_for_non_itemizers = tax_unit(
+            "charitable_deduction_for_non_itemizers", period
+        )
+        reduced_cash_donations = max_(
+            cash_donations - charitable_deduction_for_non_itemizers, 0
+        )
+        total_cap = p.all * positive_agi
+        return min_(
+            capped_non_cash_donations + reduced_cash_donations, total_cap
+        )
