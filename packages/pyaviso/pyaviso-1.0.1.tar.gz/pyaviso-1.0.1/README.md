d9b225e718cec2dcde14db1055340bb013bc66d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:34 +0000

    Add package pyaviso/1.0.1/pyaviso-1.0.1.tar.gz

---
+++ packages/pyaviso/pyaviso-1.0.1.tar.gz/pyaviso-1.0.1/README.md
@@ -0,0 +1,16 @@
+# Aviso
+
+Aviso is a software developed by ECMWF that allows the notification of  **time-critical events** across HPC and Cloud systems in 
+order to enable workflows among multiple domains.
+
+It allows users to:
+
+* Define events that require notification
+* Define triggers to be executed once a notification is received
+* Send and receive notifications
+
+This enables the creation of automatic workflows, timely triggered as events are notified.
+ 
+
+The documentation can be found at https://pyaviso.readthedocs.io/ \
+Examples demonstrating the usage of aviso for ECMWF notifications can be found at https://github.com/ecmwf/aviso-examples
---
+++ packages/pyaviso/pyaviso-1.0.1.tar.gz/pyaviso-1.0.1/pyaviso/authentication/ecmwf_auth.py
@@ -0,0 +1,24 @@
+# (C) Copyright 1996- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+from ..user_config import UserConfig
+from .etcd_auth import EtcdAuth
+
+
+class EcmwfAuth(EtcdAuth):
+    """
+    This class set the authentication credentials for the ECMWF SSO authentication
+    """
+
+    def __init__(self, config: UserConfig):
+        super(EcmwfAuth, self).__init__(config)
+        self._password = config.password
+        self._username = config.username
+
+    def header(self):
+        return {"Authorization": f"Bearer {self._password}", "X-Auth-Type": "ecmwf"}
