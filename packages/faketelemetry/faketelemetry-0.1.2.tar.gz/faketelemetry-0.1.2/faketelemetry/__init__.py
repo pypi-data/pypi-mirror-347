c661e72a07ae8b0e6bedc6cae31434638afe725f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:31 +0000

    Add package faketelemetry/0.1.2/faketelemetry-0.1.2.tar.gz

---
+++ packages/faketelemetry/faketelemetry-0.1.2.tar.gz/faketelemetry-0.1.2/faketelemetry/__init__.py
@@ -0,0 +1,6 @@
+from .telemetry_generator import TelemetryGenerator
+from .multi_channel import MultiChannelTelemetryGenerator
+from .noise_injector import NoiseInjector
+from .enums import WaveformType
+
+__version__ = "0.1.0"
