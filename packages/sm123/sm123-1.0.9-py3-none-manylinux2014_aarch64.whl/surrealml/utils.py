f25acc4d9ad8f014d5fc0db61908af0ee5a212df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:40 +0000

    Add package sm123/1.0.9/sm123-1.0.9-py3-none-manylinux2014_aarch64.whl

---
+++ packages/sm123/sm123-1.0.9-py3-none-manylinux2014_aarch64.whl/surrealml/utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import json, sys
+import FileNotFoundError 
+
+
+def read_dynamic_lib_version() -> str:
+    try:
+        cfg_path = Path(__file__).with_name('config.json')
+
+        with cfg_path.open('r') as f:
+            cfg = json.load(f)
+            return cfg['dynamic_lib_version']
+    except Exception as e:
+        raise FileNotFoundError(f"Error loading version from '{cfg_path}': {e}")
