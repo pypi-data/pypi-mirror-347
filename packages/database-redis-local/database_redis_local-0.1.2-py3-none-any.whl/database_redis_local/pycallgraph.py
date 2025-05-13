a6327fb03228a5b5b681ccc2b24268dcf35d57ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:35 +0000

    Add package database-redis-local/0.1.2/database_redis_local-0.1.2-py3-none-any.whl

---
+++ packages/database-redis-local/database_redis_local-0.1.2-py3-none-any.whl/database_redis_local/pycallgraph.py
@@ -0,0 +1,6 @@
+# https://stackoverflow.com/questions/582336/how-do-i-profile-a-python-script
+# from pycallgraph import PyCallGraph
+# from pycallgraph.output import GraphvizOutput
+
+# with PyCallGraph(output=GraphvizOutput()):
+#    code_to_profile()
