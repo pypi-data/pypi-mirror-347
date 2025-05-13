1c45540f89e43a16b14b2148df78e2e78f63e841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:35 +0000

    Add package peptacular/2.4.0/peptacular-2.4.0-py3-none-any.whl

---
+++ packages/peptacular/peptacular-2.4.0-py3-none-any.whl/peptacular/sequence/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module contains functions for generating and manipulating peptide sequences.
+"""
+
+from . import combinatoric
+from . import mod_builder
+from . import sequence_funcs
+
+from .combinatoric import *
+from .mod_builder import *
+from .sequence_funcs import *
