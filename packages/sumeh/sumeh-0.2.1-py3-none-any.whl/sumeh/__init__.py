1660f7e8468da6da38e925292d1c50dde46cfb80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 06:15:57 +0000

    Add package sumeh/0.2.1/sumeh-0.2.1-py3-none-any.whl

---
+++ packages/sumeh/sumeh-0.2.1-py3-none-any.whl/sumeh/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Sumeh DQ."""
+
+__author__ = """Demetrius Albuquerque"""
+__email__ = "demetrius.albuquerque@yahoo.com.br"
+__version__ = "0.1.0"
+
+from .core import report
+from .services.config import *
+from .engine import *
