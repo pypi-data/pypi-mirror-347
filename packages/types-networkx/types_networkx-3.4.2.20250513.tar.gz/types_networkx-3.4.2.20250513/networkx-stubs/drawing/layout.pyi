b0c9f204d1641ed13c4c6183e46ad19621d5f212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:55 +0000

    Add package types-networkx/3.4.2.20250513/types_networkx-3.4.2.20250513.tar.gz

---
+++ packages/types-networkx/types_networkx-3.4.2.20250513.tar.gz/types_networkx-3.4.2.20250513/networkx-stubs/drawing/layout.pyi
@@ -0,0 +1,101 @@
+from _typeshed import Incomplete
+
+import numpy
+
+__all__ = [
+    "bipartite_layout",
+    "circular_layout",
+    "forceatlas2_layout",
+    "kamada_kawai_layout",
+    "random_layout",
+    "rescale_layout",
+    "rescale_layout_dict",
+    "shell_layout",
+    "spring_layout",
+    "spectral_layout",
+    "planar_layout",
+    "fruchterman_reingold_layout",
+    "spiral_layout",
+    "multipartite_layout",
+    "bfs_layout",
+    "arf_layout",
+]
+
+def random_layout(G, center: Incomplete | None = None, dim: int = 2, seed: Incomplete | None = None): ...
+def circular_layout(G, scale: float = 1, center: Incomplete | None = None, dim: int = 2): ...
+def shell_layout(
+    G,
+    nlist: Incomplete | None = None,
+    rotate: Incomplete | None = None,
+    scale: float = 1,
+    center: Incomplete | None = None,
+    dim: int = 2,
+): ...
+def bipartite_layout(
+    G, nodes, align: str = "vertical", scale: float = 1, center: Incomplete | None = None, aspect_ratio: float = ...
+): ...
+def spring_layout(
+    G,
+    k: Incomplete | None = None,
+    pos: Incomplete | None = None,
+    fixed: Incomplete | None = None,
+    iterations: int = 50,
+    threshold: float = 0.0001,
+    weight: str = "weight",
+    scale: float = 1,
+    center: Incomplete | None = None,
+    dim: int = 2,
+    seed: Incomplete | None = None,
+): ...
+
+fruchterman_reingold_layout = spring_layout
+
+def kamada_kawai_layout(
+    G,
+    dist: Incomplete | None = None,
+    pos: Incomplete | None = None,
+    weight: str = "weight",
+    scale: float = 1,
+    center: Incomplete | None = None,
+    dim: int = 2,
+): ...
+def spectral_layout(G, weight: str = "weight", scale: float = 1, center: Incomplete | None = None, dim: int = 2): ...
+def planar_layout(G, scale: float = 1, center: Incomplete | None = None, dim: int = 2): ...
+def spiral_layout(
+    G, scale: float = 1, center: Incomplete | None = None, dim: int = 2, resolution: float = 0.35, equidistant: bool = False
+): ...
+def multipartite_layout(
+    G, subset_key: str = "subset", align: str = "vertical", scale: float = 1, center: Incomplete | None = None
+): ...
+def arf_layout(
+    G,
+    pos: Incomplete | None = None,
+    scaling: float = 1,
+    a: float = 1.1,
+    etol: float = 1e-06,
+    dt: float = 0.001,
+    max_iter: int = 1000,
+    *,
+    seed: int | numpy.random.RandomState | None = None,
+): ...
+def forceatlas2_layout(
+    G,
+    pos=None,
+    *,
+    max_iter=100,
+    jitter_tolerance=1.0,
+    scaling_ratio=2.0,
+    gravity=1.0,
+    distributed_action=False,
+    strong_gravity=False,
+    node_mass=None,
+    node_size=None,
+    weight=None,
+    dissuade_hubs=False,
+    linlog=False,
+    seed=None,
+    dim=2,
+) -> dict[Incomplete, Incomplete]: ...
+def rescale_layout(pos, scale: float = 1): ...
+def rescale_layout_dict(pos, scale: float = 1): ...
+def bfs_layout(G, start, *, align="vertical", scale=1, center=None) -> dict[Incomplete, Incomplete]: ...
---
+++ packages/types-networkx/types_networkx-3.4.2.20250513.tar.gz/types_networkx-3.4.2.20250513/networkx-stubs/generators/harary_graph.pyi
@@ -0,0 +1,10 @@
+from _typeshed import Incomplete
+
+from networkx.utils.backends import _dispatchable
+
+__all__ = ["hnm_harary_graph", "hkn_harary_graph"]
+
+@_dispatchable
+def hnm_harary_graph(n, m, create_using: Incomplete | None = None): ...
+@_dispatchable
+def hkn_harary_graph(k, n, create_using: Incomplete | None = None): ...
