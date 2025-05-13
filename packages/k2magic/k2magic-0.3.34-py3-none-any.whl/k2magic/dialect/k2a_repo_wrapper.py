98df4f8db34e69d6d42368203467087ee69dbb12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:56 +0000

    Add package k2magic/0.3.34/k2magic-0.3.34-py3-none-any.whl

---
+++ packages/k2magic/k2magic-0.3.34-py3-none-any.whl/k2magic/dialect/k2a_repo_wrapper.py
@@ -0,0 +1,9 @@
+import re
+from requests.auth import HTTPBasicAuth
+from sqlalchemy import make_url, URL, TextClause, Column, BinaryExpression, Table
+
+
+
+
+def print_element(element):
+    print(element)
