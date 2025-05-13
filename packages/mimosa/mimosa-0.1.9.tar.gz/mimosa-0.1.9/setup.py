5d35328c48692f36b5aa7484fc3205b0b3513e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 04:16:09 +0000

    Add package mimosa/0.1.9/mimosa-0.1.9.tar.gz

---
+++ packages/mimosa/mimosa-0.1.9.tar.gz/mimosa-0.1.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md") as fh:
+    description = fh.read()
+
+with open("LICENSE") as fh:
+    license_txt = fh.read()
+
+setup(
+    name="mimosa",
+    version="0.1.9",
+    author="Kaj-Ivar van der Wijst",
+    author_email="k.vanderwijst@uu.nl",
+    packages=["mimosa"],
+    url="https://utrechtuniversity.github.io/mimosa/",
+    license=license_txt,
+    description="MIMOSA: Integrated Assessment Model for Cost-Benefit Analysis",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    install_requires=["numpy", "pandas", "pyomo", "pint", "pyyaml", "scipy"],
+    include_package_data=True,
+)
