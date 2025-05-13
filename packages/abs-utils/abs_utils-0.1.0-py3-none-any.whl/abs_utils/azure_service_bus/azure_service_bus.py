3c2590029c9c99a99acb420d333ec055379a80fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 17:06:37 +0000

    Add package abs-utils/0.1.0/abs_utils-0.1.0-py3-none-any.whl

---
+++ packages/abs-utils/abs_utils-0.1.0-py3-none-any.whl/abs_utils/azure_service_bus/azure_service_bus.py
@@ -0,0 +1,19 @@
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+import json
+
+class AzureServiceBus:
+    def __init__(self, connection_string: str, queue_name: str):
+        self.client = ServiceBusClient.from_connection_string(conn_str=connection_string)
+        self.queue_name = queue_name
+
+    async def send(self, event_payload: dict):
+        """Send message to Azure Service Bus"""
+        if not self.client or not self.queue_name:
+            return
+
+        try:
+            sender = self.client.get_queue_sender(queue_name=self.queue_name)
+            with sender:
+                sender.send_messages(ServiceBusMessage(json.dumps(event_payload)))
+        except Exception as e:
+            print(f"Failed to send message to Azure Service Bus: {e}") 
