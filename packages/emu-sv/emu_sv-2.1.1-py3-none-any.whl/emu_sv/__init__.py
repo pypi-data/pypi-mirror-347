05b6a1d92d3c1d79e04c8c13a80e7b303c484160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:34 +0000

    Add package emu-sv/2.1.1/emu_sv-2.1.1-py3-none-any.whl

---
+++ packages/emu-sv/emu_sv-2.1.1-py3-none-any.whl/emu_sv/__init__.py
@@ -0,0 +1,39 @@
+from pulser.backend.results import Results
+
+from pulser.backend import (
+    BitStrings,
+    CorrelationMatrix,
+    Energy,
+    EnergyVariance,
+    EnergySecondMoment,
+    Expectation,
+    Fidelity,
+    Occupation,
+    StateResult,
+)
+
+from .dense_operator import DenseOperator
+from .sv_backend import SVBackend, SVConfig
+from .state_vector import StateVector, inner
+
+
+__all__ = [
+    "__version__",
+    "BitStrings",
+    "CorrelationMatrix",
+    "DenseOperator",
+    "Energy",
+    "EnergySecondMoment",
+    "EnergyVariance",
+    "Expectation",
+    "Fidelity",
+    "Occupation",
+    "Results",
+    "SVBackend",
+    "SVConfig",
+    "StateResult",
+    "StateVector",
+    "inner",
+]
+
+__version__ = "2.1.1"
