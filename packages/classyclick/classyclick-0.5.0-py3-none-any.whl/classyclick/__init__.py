57f33dfa116fccec73316b6fee7895d8e80f2b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 15:05:18 +0000

    Add package classyclick/0.5.0/classyclick-0.5.0-py3-none-any.whl

---
+++ packages/classyclick/classyclick-0.5.0-py3-none-any.whl/classyclick/__init__.py
@@ -0,0 +1,5 @@
+from .__version__ import __version__, version
+from .command import command
+from .fields import argument, context, context_meta, context_obj, option
+
+__all__ = ['__version__', 'version', 'command', 'option', 'argument', 'context_obj', 'context', 'context_meta']
