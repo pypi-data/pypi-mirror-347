2ac1e94364444591f2a9de7c72374cd9fbb8559a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 04:15:59 +0000

    Add package zeolite/0.1.1/zeolite-0.1.1-py3-none-any.whl

---
+++ packages/zeolite/zeolite-0.1.1-py3-none-any.whl/zeolite/column/checks.py
@@ -0,0 +1,15 @@
+from ._validation import (
+    IsValueEmpty,
+    IsValueDuplicated,
+    IsValueInvalidDate,
+    IsValueEqualTo,
+)
+# -----------------------------------------------------------------------------------------------------------
+# Check/Validation Columns
+# -----------------------------------------------------------------------------------------------------------
+
+
+check_is_value_empty = IsValueEmpty
+check_is_value_duplicated = IsValueDuplicated
+check_is_value_invalid_date = IsValueInvalidDate
+check_is_value_equal_to = IsValueEqualTo
