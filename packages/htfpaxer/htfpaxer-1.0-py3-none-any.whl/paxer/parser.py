e9b9d3cb90c0aa7ae25eafb50f759faecb712230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 08:04:40 +0000

    Add package htfpaxer/1.0/htfpaxer-1.0-py3-none-any.whl

---
+++ packages/htfpaxer/htfpaxer-1.0-py3-none-any.whl/paxer/parser.py
@@ -0,0 +1,16 @@
+import argparse
+
+def get_parse() -> argparse.Namespace:
+
+    parser = argparse.ArgumentParser(description="WinRAR ripoff made by HaxoTF")
+    parser_sub = parser.add_subparsers(dest="command", required=True)
+
+    parser_sub_pack = parser_sub.add_parser("pack")
+    parser_sub_pack.add_argument("-i", "--input",  dest="input",  type=str, help="Directory path")
+    parser_sub_pack.add_argument("-o", "--output", dest="output", type=str, help="PXD path")
+
+    parser_sub_free = parser_sub.add_parser("free")
+    parser_sub_free.add_argument("-i", "--input",  dest="input",  type=str, help="PXD path")
+    parser_sub_free.add_argument("-o", "--output", dest="output", type=str, help="Where to free?")
+    
+    return parser.parse_args()
