becb55b2c5ec29a7d17bd5bac838f8e8c059865e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:42 +0000

    Add package PyMouPosTracker/0.1.0/pymoupostracker-0.1.0-py3-none-any.whl

---
+++ packages/PyMouPosTracker/pymoupostracker-0.1.0-py3-none-any.whl/PyMouPosTracker/PyMouPosTracker.py
@@ -0,0 +1,21 @@
+import pyautogui
+import keyboard
+import time
+
+class trackMousePosition():
+    def __init__(self):
+        pass
+
+    def mousePosition(self):
+        print("Press the 'Enter' key to quit.")
+
+        while not keyboard.is_pressed('Enter'):
+            x, y = pyautogui.position()
+
+            position = f'X: {x:4} Y:{y:4}' 
+
+            print(position, end='')
+            print(end='\r', flush=True)
+            time.sleep(0.1)
+
+        print('\n Program has exited.')
