dd5b51952b72fb3e0dc5fdcd1d29106c4c5dfc2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:36 +0000

    Add package uipath-nightly/2.0.52.dev1003360261/uipath_nightly-2.0.52.dev1003360261.tar.gz

---
+++ packages/uipath-nightly/uipath_nightly-2.0.52.dev1003360261.tar.gz/uipath_nightly-2.0.52.dev1003360261/src/uipath/telemetry/_constants.py
@@ -0,0 +1,11 @@
+_CONNECTION_STRING = "InstrumentationKey=d3da6dfc-5231-4d72-9696-f680a64683b8;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=9b73b209-6c6a-4d4a-9549-b922cef418d5"
+
+_APP_INSIGHTS_EVENT_MARKER_ATTRIBUTE = "APPLICATION_INSIGHTS_EVENT_MARKER_ATTRIBUTE"
+_OTEL_RESOURCE_ATTRIBUTES = "OTEL_RESOURCE_ATTRIBUTES"
+_SDK_VERSION = "SDK_VERSION"
+
+_CODE_FILEPATH = "code.filepath"
+_CODE_FUNCTION = "code.function"
+_CODE_LINENO = "code.lineno"
+
+_UNKNOWN = "unknown"
