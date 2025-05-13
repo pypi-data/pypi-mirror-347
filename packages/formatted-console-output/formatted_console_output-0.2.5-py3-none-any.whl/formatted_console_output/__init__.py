73c95426a9df0180f51518bef21a765e619621df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:04:53 +0000

    Add package formatted-console-output/0.2.5/formatted_console_output-0.2.5-py3-none-any.whl

---
+++ packages/formatted-console-output/formatted_console_output-0.2.5-py3-none-any.whl/formatted_console_output/__init__.py
@@ -0,0 +1,15 @@
+from .formatted_console_output import (
+    ForegroundColor,
+    BackgroundColor,
+    TextFormat,
+    FormattedPhrase,
+    output_formatted_message,
+)
+
+__all__ = [
+    "ForegroundColor",
+    "BackgroundColor",
+    "TextFormat",
+    "FormattedPhrase",
+    "output_formatted_message",
+]
