9cfe010512165674d905bf60f03a4c71fd3e676f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 04:15:58 +0000

    Add package blackbox-gradient-sensing/0.1.6/blackbox_gradient_sensing-0.1.6.tar.gz

---
+++ packages/blackbox-gradient-sensing/blackbox_gradient_sensing-0.1.6.tar.gz/blackbox_gradient_sensing-0.1.6/train.py
@@ -0,0 +1,62 @@
+from blackbox_gradient_sensing import BlackboxGradientSensing, Actor
+
+# sim environment, example using gymansium
+
+import gymnasium as gym
+
+sim = gym.make('LunarLander-v3', render_mode = 'rgb_array')
+
+dim_state = sim.observation_space.shape[0]
+
+# hyperparams
+
+num_noises = 100   # number of noise perturbations, from which top is chosen for a weighted update - in paper this was 200 for sim, 3 for real
+num_selected = 15  # number of elite perturbations chosen
+num_repeats = 5    # number of repeats (j in eq) - in paper they did ~10 for sim, then 3 for real
+
+# instantiate BlackboxGradientSensing with the Actor (with right number of actions), and then forward your environment for the actor to learn from it
+# you can also supply your own Actor, which simply receives a state tensor and outputs action logits
+
+actor = Actor(
+    dim_state = dim_state,
+    num_actions = sim.action_space.n
+)
+
+bgs = BlackboxGradientSensing(
+    actor,
+    dim_state = dim_state,
+    noise_pop_size = num_noises,
+    num_selected = num_selected,
+    num_rollout_repeats = num_repeats,
+    actor_is_recurrent = True,
+    optim_step_post_hook = lambda: actor.norm_weights_(),
+    torch_compile_actor = True
+)
+
+# recording logic
+
+if bgs.is_main:
+    from math import ceil
+    from shutil import rmtree
+
+    video_folder = './recording'
+    rmtree(video_folder, ignore_errors = True)
+
+    den = bgs.num_episodes_per_learning_cycle
+    total_eps_before_update = ceil(500 / den) * den
+
+    sim = gym.wrappers.RecordVideo(
+        env = sim,
+        video_folder = video_folder,
+        name_prefix = 'lunar-lander',
+        episode_trigger = lambda eps_num: (eps_num % total_eps_before_update == 0),
+        disable_logger = True
+    )
+
+# pass the simulation environment in - say for 1000 interactions with env
+
+bgs(sim, 1000)
+
+# after much training, save and then finetune on real environment
+
+bgs.save('./sim-trained-actor-and-state-norm.pt')
