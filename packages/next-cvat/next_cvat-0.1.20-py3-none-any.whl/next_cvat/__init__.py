c1726abc4a12be959f87c646d86131b477096c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 06:16:50 +0000

    Add package next-cvat/0.1.20/next_cvat-0.1.20-py3-none-any.whl

---
+++ packages/next-cvat/next_cvat-0.1.20-py3-none-any.whl/next_cvat/__init__.py
@@ -0,0 +1,7 @@
+from .app import app
+
+pass
+
+from .annotations import Annotations
+from .client import Client
+from .types import Attribute, Box, Label, LabelAttribute, Mask, Polygon, Project, Task
