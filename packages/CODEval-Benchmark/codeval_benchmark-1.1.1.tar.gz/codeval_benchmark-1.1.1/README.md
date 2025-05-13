41cc70ea79bc9ab1e6d35a7c5331da71f5371fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 17:06:36 +0000

    Add package CODEval-Benchmark/1.1.1/codeval_benchmark-1.1.1.tar.gz

---
+++ packages/CODEval-Benchmark/codeval_benchmark-1.1.1.tar.gz/codeval_benchmark-1.1.1/README.md
@@ -0,0 +1,7 @@
+Title: CODEVAL<br>
+Description: A LLM Benchmark that used to evaluate small Large Language Model.<br>
+Version: 1.0.0<br>
+Author: R Kiran Kumar Reddy<br>
+Email: rkirankumarreddy599@gmail.com<br>
+License: MIT<br>
+URL: # Not Opensourced yet
---
+++ packages/CODEval-Benchmark/codeval_benchmark-1.1.1.tar.gz/codeval_benchmark-1.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'CODEval-Benchmark',
+    version = '1.1.1',
+    packages= find_packages(),
+    install_requires = [
+        'langchain',
+        'langchain-core',
+        'langchain-ollama',
+        'langchain-text-splitters',
+        'langsmith'
+    ],
+    description="A Benchmark used to evaluate small large language models.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",  
+    entrt_points = {
+        "console_scripts":[
+            "CODEval = CODEval.About:about"
+        ]
+    }
+)
