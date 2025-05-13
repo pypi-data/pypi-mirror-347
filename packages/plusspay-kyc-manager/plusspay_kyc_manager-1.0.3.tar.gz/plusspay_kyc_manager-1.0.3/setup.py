92f1dc3703e77f2a6afc99717b13601c76885899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:47 +0000

    Add package plusspay-kyc-manager/1.0.3/plusspay_kyc_manager-1.0.3.tar.gz

---
+++ packages/plusspay-kyc-manager/plusspay_kyc_manager-1.0.3.tar.gz/plusspay_kyc_manager-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+setup(
+    name='plusspay_kyc_manager',
+    packages=['plusspay_kyc_manager'],  # this must be the same as the name above
+    version='1.0.3',
+    description='Librería de Plusspay© para la gestión de procesos KYC (Know Your Customer) a través de diferentes proveedores.',
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author='Plusspay',
+    author_email='',
+    # use the URL to the github repo
+    url='https://github.com/Pluss-Pay/kyc-didi',
+    download_url='https://github.com/Pluss-Pay/kyc-didi/tarball/1.0.1',
+    keywords=['plusspay'],
+    license='MIT',
+    install_requires=[
+    	'requests',
+	'python-dotenv',
+	'httpx'
+    ]
+)
