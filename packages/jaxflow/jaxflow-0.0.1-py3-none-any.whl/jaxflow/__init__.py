687e369de12855fbdd3ed5f2f6ce16366017f86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 17:06:27 +0000

    Add package jaxflow/0.0.1/jaxflow-0.0.1-py3-none-any.whl

---
+++ packages/jaxflow/jaxflow-0.0.1-py3-none-any.whl/jaxflow/__init__.py
@@ -0,0 +1,12 @@
+from jaxflow.core import Variable
+from jaxflow.Gradient import Gradient
+from jaxflow.initializers import initializers
+from jaxflow.layers import *
+from jaxflow  import losses
+from jaxflow.optimizers import *
+from jaxflow  import math 
+from jaxflow import models
+
+
+
+__version__ = "0.1.1"
