6c5539c5d9777fc4138b4ca852650ddaec1aa02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 04:16:09 +0000

    Add package gdoc-dumper/0.2.2/gdoc_dumper-0.2.2-py3-none-any.whl

---
+++ packages/gdoc-dumper/gdoc_dumper-0.2.2-py3-none-any.whl/gdoc_dumper/enums/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Formats(Enum):
+    PDF = "pdf"
+    TXT = "txt"
+    DOCX = "docx"
+    HTML = "html"
+    RICH_TEXT = "rtf"
+    OPEN_DOCUMENT_TEXT = "odt"
+    EPUB = "epub"
+    ZIP = "zip"
