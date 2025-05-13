67f472f65e875927d9e581bee11e0736dd6117b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:26 +0000

    Add package uc-vapi-runtime/2.44.0/uc_vapi_runtime-2.44.0.tar.gz

---
+++ packages/uc-vapi-runtime/uc_vapi_runtime-2.44.0.tar.gz/uc_vapi_runtime-2.44.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="uc-vapi-runtime",
+    version="2.44.0",
+    description="uc-vapi-runtime",
+    long_description="uc-vapi-runtime",
+    long_description_content_type="text/markdown",
+    author="VMware",
+    author_email="support@vmware.com",
+    url="https://github.com/vmware/vsphere-automation-sdk-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],  # Add dependencies if required
+)
