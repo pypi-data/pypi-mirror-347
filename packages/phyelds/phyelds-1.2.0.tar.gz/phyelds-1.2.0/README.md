73b6e8cc82a299b90f6a7a4ac295f6f300f96bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:42 +0000

    Add package phyelds/1.2.0/phyelds-1.2.0.tar.gz

---
+++ packages/phyelds/phyelds-1.2.0.tar.gz/phyelds-1.2.0/README.md
@@ -0,0 +1,5 @@
+# Phyelds
+
+A lightweight Python library to support aggregate computing!
+## Features
+- *Pythonic*: FieldPy is designed to be easy to use and understand, with a focus on simplicity and readability.
---
+++ packages/phyelds/phyelds-1.2.0.tar.gz/phyelds-1.2.0/src/phyelds/simulator/actions.py
@@ -0,0 +1,17 @@
+"""
+Sent of actions related to node movements and interactions in the simulator
+"""
+
+
+def move_with_velocity(simulator, delta_time, node, velocity):
+    """
+    Move the node with a given velocity.
+    """
+    # Update the node's position based on its velocity and the delta time
+    (x, y) = node.position
+    (vx, vy) = velocity
+    new_position = (x + vx * delta_time, y + vy * delta_time)
+    node.update(new_position)
+    simulator.schedule_event(
+        delta_time, move_with_velocity, simulator, delta_time, node, velocity
+    )
