b960c8a49919f56a8d88bd61fdadce00be23d85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:55 +0000

    Add package structureboost/0.7.0/structureboost-0.7.0-cp312-cp312-macosx_14_0_arm64.whl

---
+++ packages/structureboost/structureboost-0.7.0-cp312-cp312-macosx_14_0_arm64.whl/structureboost/__init__.py
@@ -0,0 +1,19 @@
+"""structureboost is a Python package for Gradient Boosting using categorical structure"""
+
+import graphs
+from structure_gb import StructureBoost
+from structure_gb_multi import StructureBoostMulti
+from structure_dt import StructureDecisionTree
+from structure_dt_multi import StructureDecisionTreeMulti
+# from structure_rf import StructureRF
+# from structure_rfdt import StructureRFDecisionTree
+from prob_regr_unit import ProbRegressorUnit
+from prob_regressor import PrestoBoost
+from coarsage import Coarsage
+from pdf_discrete import PdfDiscrete, average_densities
+from pdf_group import PdfGroup, log_loss_pdf, test_between_quantiles_pdf, test_in_pred_regions
+from pdf_set import PdfSet
+from pdf_conformal import PdfConformalizer
+from .utils import get_basic_config, apply_defaults, default_config_dict, ice_plot, log_loss
+
+__version__ = '0.7.0'
