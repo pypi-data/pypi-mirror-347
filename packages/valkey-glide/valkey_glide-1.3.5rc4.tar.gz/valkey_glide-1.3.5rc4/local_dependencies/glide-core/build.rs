793723280000951aa833ba1f415c2d33644953e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:41 +0000

    Add package valkey-glide/1.3.5rc4/valkey_glide-1.3.5rc4.tar.gz

---
+++ packages/valkey-glide/valkey_glide-1.3.5rc4.tar.gz/valkey_glide-1.3.5rc4/local_dependencies/glide-core/build.rs
@@ -0,0 +1,22 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+#[cfg(feature = "socket-layer")]
+fn build_protobuf() {
+    let customization_options = protobuf_codegen::Customize::default()
+        .lite_runtime(false)
+        .tokio_bytes(true)
+        .tokio_bytes_for_string(true);
+    protobuf_codegen::Codegen::new()
+        .cargo_out_dir("protobuf")
+        .include("src")
+        .input("src/protobuf/command_request.proto")
+        .input("src/protobuf/response.proto")
+        .input("src/protobuf/connection_request.proto")
+        .customize(customization_options)
+        .run_from_script();
+}
+
+fn main() {
+    #[cfg(feature = "socket-layer")]
+    build_protobuf();
+}
