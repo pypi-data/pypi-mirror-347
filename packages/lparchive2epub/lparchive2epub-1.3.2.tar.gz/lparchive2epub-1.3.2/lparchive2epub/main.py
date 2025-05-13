eb43eae34f7fe122997ae2bf17adb2860a52bcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 15:05:17 +0000

    Add package lparchive2epub/1.3.2/lparchive2epub-1.3.2.tar.gz

---
+++ packages/lparchive2epub/lparchive2epub-1.3.2.tar.gz/lparchive2epub-1.3.2/lparchive2epub/main.py
@@ -0,0 +1,32 @@
+import asyncio
+from argparse import ArgumentParser, ArgumentTypeError
+from urllib.parse import urlparse
+
+from lparchive2epub.lib import lparchive2epub
+
+
+def is_lparchive_url(arg):
+    url = urlparse(arg)
+    if not "lparchive.org" in url.geturl():
+        raise ArgumentTypeError(f"{arg} is not an lparchive.org URL")
+    if not url.path or url.path == "/":
+        raise ArgumentTypeError(f"{arg} is not a let's play archive")
+    return arg
+
+
+arg_parser = ArgumentParser(
+    prog="lparchive2epub",
+    description="A tool to transform a Let's Play from lparchive.org to epub format."
+)
+
+arg_parser.add_argument("url", metavar="URL", nargs=1, type=is_lparchive_url)
+arg_parser.add_argument("output", metavar="OUTPUT_FILE", nargs=1, type=str)
+
+
+def main():
+    args = arg_parser.parse_args()
+    asyncio.run(lparchive2epub(args.url[0], args.output[0]))
+
+
+if __name__ == '__main__':
+    main()
