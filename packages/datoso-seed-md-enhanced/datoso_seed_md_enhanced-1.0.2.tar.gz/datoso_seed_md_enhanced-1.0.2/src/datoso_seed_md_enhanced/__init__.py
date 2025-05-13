10a658e5806e85e905a002085868874dadc1cc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 15:05:38 +0000

    Add package datoso-seed-md-enhanced/1.0.2/datoso_seed_md_enhanced-1.0.2.tar.gz

---
+++ packages/datoso-seed-md-enhanced/datoso_seed_md_enhanced-1.0.2.tar.gz/datoso_seed_md_enhanced-1.0.2/src/datoso_seed_md_enhanced/__init__.py
@@ -0,0 +1,6 @@
+"""Initialize seed variables."""
+__all__ = ['__author__', '__description__', '__version__']
+__version__ = '1.0.2'
+__author__ = 'Lacides Miranda'
+__description__ = 'MegaDrive - Genesis Enhanced'
+__prefix__ = 'md_enhanced'
---
+++ packages/datoso-seed-md-enhanced/datoso_seed_md_enhanced-1.0.2.tar.gz/datoso_seed_md_enhanced-1.0.2/src/datoso_seed_md_enhanced/rules.py
@@ -0,0 +1,9 @@
+"""Rules for the Mega Drive Enhanced seed."""
+
+rules = [
+]
+
+
+def get_rules() -> list:
+    """Get the rules."""
+    return rules
