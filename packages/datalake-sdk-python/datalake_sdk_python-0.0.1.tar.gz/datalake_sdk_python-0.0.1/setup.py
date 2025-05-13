1bbf8b309c43c29eb0e07548e2c11361166d5cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:05:24 +0000

    Add package datalake-sdk-python/0.0.1/datalake_sdk_python-0.0.1.tar.gz

---
+++ packages/datalake-sdk-python/datalake_sdk_python-0.0.1.tar.gz/datalake_sdk_python-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='datalake-sdk-python',
+    packages=find_packages(include=['datalake-sdk-python']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
