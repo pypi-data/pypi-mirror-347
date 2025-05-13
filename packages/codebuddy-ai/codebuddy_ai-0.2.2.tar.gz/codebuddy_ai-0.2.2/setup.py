acd5721d6afd5640942d925c14b119466ac4c554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:06:08 +0000

    Add package codebuddy-ai/0.2.2/codebuddy_ai-0.2.2.tar.gz

---
+++ packages/codebuddy-ai/codebuddy_ai-0.2.2.tar.gz/codebuddy_ai-0.2.2/setup.py
@@ -0,0 +1,37 @@
+# setup.py
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the README file, using the correct encoding.
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="codebuddy-ai", #old name: "commit-generator",
+    version="0.2.2",
+    description="AI-powered CLI to generate commit messages, review code, and suggest fixes",
+    author="Naman Kansal",
+    author_email="namankansal91@gmail.com",
+    url="https://github.com/UP11SRE/agent-client",  # GitHub URL
+    license="Proprietary",
+    long_description=long_description,  # This is your README content.
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+    "console_scripts": [
+        "codebuddy=app.cli:main",  # Main CLI command
+    ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
