8786d42cd8ae21a5c2f4803affb6ca9780a5ddd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:19 +0000

    Add package phyelds/1.2.2/phyelds-1.2.2-py3-none-any.whl

---
+++ packages/phyelds/phyelds-1.2.2-py3-none-any.whl/phyelds/libraries/device.py
@@ -0,0 +1,35 @@
+"""
+Device library:
+Set of function used to get the device information.
+"""
+from phyelds import engine
+
+
+def local_id():
+    """
+    Get the local id of the device.
+    :return:
+    """
+    return engine.node_context.node_id
+
+def context():
+    """
+    Get the context of the device.
+    :return:
+    """
+    return engine.node_context
+
+def sense(sensor: str) -> any:
+    """
+    Get the value of the sensor.
+    :param sensor: The name of the sensor.
+    :return: The value of the sensor.
+    """
+    return engine.node_context.sensors[sensor]
+
+def local_position():
+    """
+    Get the position of the device.
+    :return: The position of the device.
+    """
+    return engine.node_context.position()
