1e0d85ef645f9ba9b9752fe8c632b22c8161d9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 17:06:22 +0000

    Add package Advantech-EdgeSync.edge/0.0.0a1/advantech_edgesync_edge-0.0.0a1-py3-none-any.whl

---
+++ packages/Advantech-EdgeSync.edge/advantech_edgesync_edge-0.0.0a1-py3-none-any.whl/advantech/__init__.py
@@ -0,0 +1,9 @@
+
+from .iplatforminformation import IPlatformInformation
+from .ionboardsensors import IOnboardSensors
+from .igpio import IGpio
+from .imemory import IMemory
+from .idisk import IDisk
+from .susiiot import SusiIot
+
+from .edge import Device
