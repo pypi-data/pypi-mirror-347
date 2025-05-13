68a7b779b7c651e38cf19a01512cd8e857ba1c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 17:06:26 +0000

    Add package rss-provider/1.0.0/rss_provider-1.0.0.tar.gz

---
+++ packages/rss-provider/rss_provider-1.0.0.tar.gz/rss_provider-1.0.0/src/rss_provider/__init__.py
@@ -0,0 +1,14 @@
+"""Init and utils."""
+
+from zope.i18nmessageid import MessageFactory
+
+
+__version__ = "1.0.0"
+import logging
+
+
+PACKAGE_NAME = "rss_provider"
+
+_ = MessageFactory("rss_provider")
+
+logger = logging.getLogger("rss_provider")
