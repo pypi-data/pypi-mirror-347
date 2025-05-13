56ea06fa18c7135254aed1c93eea26bbdcc76ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 15:05:01 +0000

    Add package hikyuu/2.6.1/hikyuu-2.6.1-py3-none-win_amd64.whl

---
+++ packages/hikyuu/hikyuu-2.6.1-py3-none-win_amd64.whl/hikyuu/include/hikyuu/plugin/interface/plugins.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2025 hikyuu.org
+ *
+ *  Created on: 2025-04-10
+ *      Author: fasiondog
+ */
+
+#pragma once
+
+#include "BackTestPluginInterface.h"
+#include "DevicePluginInterface.h"
+#include "DataServerPluginInterface.h"
+#include "ImportKDataToHdf5PluginInterface.h"
+
+namespace hku {
+
+#define HKU_PLUGIN_BACKTEST "backtest"
+#define HKU_PLUGIN_DEVICE "device"
+#define HKU_PLUGIN_DATASERVER "dataserver"
+#define HKU_PLUGIN_IMPORTKDATATOHDF5 "import2hdf5"
+
+}
