c9375dfa1a3b208a630ac852b8a05202814e3bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 16:06:21 +0000

    Add package pygohcl/1.2.5/pygohcl-1.2.5-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pygohcl/pygohcl-1.2.5-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pygohcl/build_cffi.py
@@ -0,0 +1,26 @@
+from cffi import FFI
+
+ffi = FFI()
+
+ffi.set_source(
+    "pygohcl._pygohcl",
+    None,
+    include_dirs=[],
+    extra_compile_args=["-march=native"],
+    libraries=[],
+)
+
+ffi.cdef(
+    """
+        typedef struct {
+            char *json;
+            char *err;
+        } parseResponse;
+ 
+        parseResponse Parse(char* a, int keepInterpFlag);
+        parseResponse ParseAttributes(char* a);
+        char* EvalValidationRule(char* c, char* e, char* n, char* v);
+        void free(void *ptr);
+        """
+)
+ffi.compile()
