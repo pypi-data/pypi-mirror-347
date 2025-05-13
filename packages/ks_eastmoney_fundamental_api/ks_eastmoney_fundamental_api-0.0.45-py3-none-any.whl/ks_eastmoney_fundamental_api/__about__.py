857e2bbc235741d2d587eba9e84b15a676d30771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 14:06:07 +0000

    Add package ks_eastmoney_fundamental_api/0.0.45/ks_eastmoney_fundamental_api-0.0.45-py3-none-any.whl

---
+++ packages/ks_eastmoney_fundamental_api/ks_eastmoney_fundamental_api-0.0.45-py3-none-any.whl/ks_eastmoney_fundamental_api/__about__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024-present liuyanzhi08 <702368372@qq.com>
+#
+# SPDX-License-Identifier: MIT
+# __version__ = "0.0.30"
+# __version__ = "0.0.32" # 增加YOYOR,YOYNI,CAGRTOR
+# __version__ = "0.0.35" # 增加css_mry函数，支持批量获取最近N年年报数据
+# __version__ = "0.0.36" # 增加css_mry和css_mrq的自动化测试用例
+# __version__ = "0.0.39" # 增加毛利率
+# __version__ = "0.0.40" # 修正css_mry的顺序问题
+# __version__="0.0.42" # 增加净利率，经营活动现金流净值多年数据
+# __version__="0.0.44" # 增加股息率，胜利支付率
+__version__="0.0.45" # 增加股息率TTM，PETTM
