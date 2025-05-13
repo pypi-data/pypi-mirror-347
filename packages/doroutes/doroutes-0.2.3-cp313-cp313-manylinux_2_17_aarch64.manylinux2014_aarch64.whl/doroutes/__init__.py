3b8cb7814ee443b680a6dca7e149f643e56b2789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 17:06:14 +0000

    Add package doroutes/0.2.3/doroutes-0.2.3-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/doroutes/doroutes-0.2.3-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/doroutes/__init__.py
@@ -0,0 +1,26 @@
+"""DoRoutes: A package providing advanced routing for gdsfactory."""
+
+__version__ = "0.2.3"
+__author__ = "Floris Laporte"
+
+__all__ = [
+    "add_bundle_astar",
+    "add_fan_in",
+    "add_route_astar",
+    "add_route_from_corners",
+    "add_route_from_steps",
+    "find_route_astar",
+    "pcells",
+    "types",
+    "util",
+]
+
+from . import pcells, types, util
+from .bundles import add_bundle_astar
+from .fanin import add_fan_in
+from .routing import (
+    add_route_astar,
+    add_route_from_corners,
+    add_route_from_steps,
+    find_route_astar,
+)
