89a34983898388629660b5aefc02416ca7996062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:38 +0000

    Add package resonate-sdk/0.5.1/resonate_sdk-0.5.1-py3-none-any.whl

---
+++ packages/resonate-sdk/resonate_sdk-0.5.1-py3-none-any.whl/resonate/clocks/step.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+
+class StepClock:
+    def __init__(self) -> None:
+        self._time = 0.0
+
+    def step(self, time: float) -> None:
+        assert time >= self._time, "The arrow of time only flows forward."
+        self._time = time
+
+    def time(self) -> float:
+        """Return the current time in seconds."""
+        return self._time
+
+    def strftime(self, format: str, /) -> str:
+        raise NotImplementedError
