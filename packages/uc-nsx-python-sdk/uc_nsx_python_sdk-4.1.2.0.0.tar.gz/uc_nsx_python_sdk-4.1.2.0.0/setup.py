5f84ea2cf4460c96c2638bf6bd13805e4fc55b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 06:16:01 +0000

    Add package uc-nsx-python-sdk/4.1.2.0.0/uc_nsx_python_sdk-4.1.2.0.0.tar.gz

---
+++ packages/uc-nsx-python-sdk/uc_nsx_python_sdk-4.1.2.0.0.tar.gz/uc_nsx_python_sdk-4.1.2.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="uc_nsx_python_sdk",
+    version="4.1.2.0.0",
+    description="uc_nsx_python_sdk",
+    long_description="uc_nsx_python_sdk",
+    long_description_content_type="text/markdown",
+    author="VMware",
+    author_email="support@vmware.com",
+    url="https://github.com/vmware/vsphere-automation-sdk-python",
+    packages=find_packages(),  # Automatically find all packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],  # Add dependencies if required
+)
