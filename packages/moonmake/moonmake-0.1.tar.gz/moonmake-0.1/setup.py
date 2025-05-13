105f1ac7f5614648f40906da2c6b0781c2b6ccf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 16:05:14 +0000

    Add package moonmake/0.1/moonmake-0.1.tar.gz

---
+++ packages/moonmake/moonmake-0.1.tar.gz/moonmake-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+setup(
+    name="moonmake",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts":[
+            "moonmake-new=moonmake.create:__main"
+        ]
+    },
+    install_requires=[
+        "requests==2.32.3"
+    ]
+)
