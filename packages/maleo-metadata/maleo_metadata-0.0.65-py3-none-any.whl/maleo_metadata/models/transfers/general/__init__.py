6b756169246573f123a64b70f4128e576b335931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 15:05:01 +0000

    Add package maleo-metadata/0.0.65/maleo_metadata-0.0.65-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.0.65-py3-none-any.whl/maleo_metadata/models/transfers/general/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from .blood_type import BloodTypeTransfers
+from .gender import GenderTransfers
+from .organization_role import OrganizationRoleTransfers
+from .organization_type import OrganizationTypeTransfers
+from .service import ServiceTransfers
+from .system_role import SystemRoleTransfers
+from .user_type import UserTypeTransfers
+
+class MaleoMetadataGeneralTransfers:
+    BloodType = BloodTypeTransfers
+    Gender = GenderTransfers
+    OrganizationRole = OrganizationRoleTransfers
+    OrganizationType = OrganizationTypeTransfers
+    Service = ServiceTransfers
+    SystemRole = SystemRoleTransfers
+    UserType = UserTypeTransfers
