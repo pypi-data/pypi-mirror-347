47d84ba3eefc4eaabcc684d336109dcdd053cf04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 16:05:01 +0000

    Add package ipulse-shared-data-eng-ftredge/9.1.1/ipulse_shared_data_eng_ftredge-9.1.1.tar.gz

---
+++ packages/ipulse-shared-data-eng-ftredge/ipulse_shared_data_eng_ftredge-9.1.1.tar.gz/ipulse_shared_data_eng_ftredge-9.1.1/src/ipulse_shared_data_eng_ftredge/domain_etl/fincore/__init__.py
@@ -0,0 +1,3 @@
+from .market import (source_eod_record_for_date_multiple_symbols_extended,
+                     market_multi_symbol_provider_preproc,
+                     market_multi_symbol_common_preproc)
