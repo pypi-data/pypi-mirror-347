175664c0c24be3e35d22acada728f3d9ce6d19d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:35 +0000

    Add package fastlink/0.1.1/fastlink-0.1.1-py3-none-any.whl

---
+++ packages/fastlink/fastlink-0.1.1-py3-none-any.whl/fastlink/__init__.py
@@ -0,0 +1,13 @@
+from fastlink.client import schemas
+from fastlink.client.httpx import HttpxClient
+from fastlink.google.client import GoogleOAuth
+from fastlink.telegram.auth import TelegramAuth
+from fastlink.yandex.client import YandexOAuth
+
+__all__ = [
+    "GoogleOAuth",
+    "HttpxClient",
+    "TelegramAuth",
+    "YandexOAuth",
+    "schemas",
+]
---
+++ packages/fastlink/fastlink-0.1.1-py3-none-any.whl/fastlink/jwt/__init__.py
@@ -0,0 +1,8 @@
+from fastlink.jwt.manager import JWTManager
+from fastlink.jwt.schemas import JWTConfig, JWTPayload
+
+__all__ = [
+    "JWTConfig",
+    "JWTManager",
+    "JWTPayload",
+]
