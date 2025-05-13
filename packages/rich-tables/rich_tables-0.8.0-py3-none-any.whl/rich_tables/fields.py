58ce5eeee81712cc09d0314077adcf71a9730fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 16:06:00 +0000

    Add package rich-tables/0.8.0/rich_tables-0.8.0-py3-none-any.whl

---
+++ packages/rich-tables/rich_tables-0.8.0-py3-none-any.whl/rich_tables/fields.py
@@ -0,0 +1,306 @@
+from __future__ import annotations
+
+import re
+from collections import defaultdict
+from collections.abc import Iterable, MutableMapping
+from contextlib import suppress
+from datetime import datetime, timezone
+from functools import singledispatch
+from itertools import islice
+from typing import TYPE_CHECKING, Any, Callable
+
+from rich.console import ConsoleRenderable
+from rich.text import Text
+
+from .diff import pretty_diff
+from .utils import (
+    BOLD_GREEN,
+    BOLD_RED,
+    HashableDict,
+    HashableList,
+    JSONDict,
+    border_panel,
+    duration2human,
+    fmt_time,
+    format_string,
+    format_with_color,
+    format_with_color_on_black,
+    get_country,
+    human_dt,
+    md_panel,
+    progress_bar,
+    simple_panel,
+    split_with_color,
+    sql_syntax,
+    syntax,
+    timestamp2timestr,
+    wrap,
+)
+
+if TYPE_CHECKING:
+    from rich.console import RenderableType
+
+
+MATCH_COUNT_HEADER = re.compile(r"duration|(?:_sum$|_?count$)")
+MAX_BPM_COLOR = (("green", 135), ("yellow", 165), ("red", 400))
+
+
+def add_count_bars(
+    data: HashableList[HashableDict], count_key: str
+) -> HashableList[HashableDict]:
+    all_keys = list(data[0].keys())
+    subcount_key = next((k for k in all_keys if k.endswith("_subcount")), None)
+    if subcount_key:
+        new_count_key = f"{subcount_key.removesuffix('_subcount')}/{count_key.removesuffix('_count')}"  # noqa: E501
+    else:
+        new_count_key = count_key
+
+    all_counts = [float(i[count_key]) for i in data]
+    max_value = max(all_counts)
+
+    bar_key = f"{count_key}_bar"
+    for item in data:
+        subcount = None
+        inverse = count_key.endswith("duration")
+        count = item[count_key]
+        if subcount_key:
+            subcount = float(item[subcount_key])
+            count_val = f"{subcount}/{count}"
+        elif count_key.endswith("duration"):
+            count_val = duration2human(count)
+        else:
+            count_val = str(count)
+
+        item.pop(count_key, None)
+        item[new_count_key] = count_val
+        item[bar_key] = progress_bar(
+            end=subcount, width=max_value, size=count, inverse=inverse
+        )
+
+    if count_key in {"duration", "total_duration"}:
+        data.append(
+            HashableDict(
+                {
+                    all_keys[0]: "TOTAL",
+                    count_key: duration2human(sum(all_counts)),
+                    bar_key: "",
+                }
+            )
+        )
+
+    return data
+
+
+FIELDS_MAP: MutableMapping[str, Callable[..., RenderableType]] = defaultdict(
+    lambda: str,
+    diff=lambda x: pretty_diff(*x),
+    albumtypes=lambda x: " ".join(
+        map(
+            format_with_color,
+            (
+                ("; ".join(x) if isinstance(x, (list, HashableList, tuple)) else x)
+                .replace("compilation", "comp")
+                .replace("dj-mix; broadcast", "dj-mix")
+                .replace("broadcast; dj-mix", "dj-mix")
+            ).split("; "),
+        )
+    ),
+    author=lambda x: (
+        format_with_color_on_black(x)
+        if isinstance(x, (str, list, HashableList, tuple, set))
+        else x
+    ),
+    labels=lambda x: (
+        wrap(" ".join(wrap(y["name"], f"#{y['color']}") for y in x), "b")
+        if isinstance(x, (list, HashableList, tuple))
+        else format_with_color(x)
+        if isinstance(x, str)
+        else x
+    ),
+    since=lambda x: (
+        x
+        if isinstance(x, str)
+        else datetime.fromtimestamp(float(x), tz=timezone.utc).strftime("%F %H:%M")
+    ),
+    wait_per_play=lambda x: wrap(
+        " ".join(islice(fmt_time(int(float(x))), 1)), BOLD_GREEN
+    ),
+    bpm=lambda x: (
+        wrap(str(x), next(c for c, m in MAX_BPM_COLOR if x < m))
+        if isinstance(x, int)
+        else x
+    ),
+    length=timestamp2timestr,
+    tracktotal=lambda x: (
+        (wrap("{}", "b cyan") + "/" + wrap("{}", "b cyan")).format(*x)
+        if isinstance(x, Iterable) and not isinstance(x, str)
+        else str(x)
+    ),
+    category=lambda x: "/".join(map(format_with_color, x.split("/"))),
+    country=get_country,
+    helicopta=lambda x: ":fire: " if x == 1 else "",
+    hidden=lambda x: ":shit: " if x == 1 else "",
+    keywords=format_with_color_on_black,
+    ingr=lambda x: simple_panel(format_with_color(x)),
+    released=lambda x: x.replace("-00", "") if isinstance(x, str) else str(x),
+    duration=lambda x: duration2human(x) if isinstance(x, (int, float)) else x,
+    plays=lambda x: wrap(x, BOLD_GREEN),
+    skips=lambda x: wrap(x, BOLD_RED),
+    new=lambda x: (
+        wrap(":heavy_check_mark: ", BOLD_GREEN)
+        if x
+        else wrap(":cross_mark_button: ", BOLD_RED)
+    ),
+    link=lambda name: (
+        wrap(f" {name} ", "b black on red")
+        if name == "blocks"
+        else wrap(name, BOLD_RED)
+        if name == "is blocked by"
+        else name
+    ),
+    code=lambda x: syntax(x, "python"),
+    context=lambda x: syntax(x, "python"),
+    python=lambda x: syntax(x, "python"),
+    CreatedBy=lambda x: syntax(x.replace(";", "\n"), "sh"),
+    file=lambda x: "/".join(map(format_with_color, x.split("/"))),
+    field=lambda x: ".".join(map(format_with_color, x.split("."))),
+    unified_diff=lambda x: syntax(x, "diff"),
+    diffHunk=lambda x: syntax(x, "diff"),
+    snippet=lambda x: border_panel(syntax(x, "python", indent_guides=True)),
+    query=lambda x: Text(x, style="bold"),
+    sql=lambda x: sql_syntax("---\n\n" + x.replace(r"\[", "[")),
+    slug=format_with_color_on_black,
+)
+fields_by_func: dict[Callable[..., RenderableType], Iterable[str]] = {
+    format_with_color: (
+        "__typename",
+        "album",
+        "albumtype",
+        "app",
+        "app_label",
+        "area",
+        "assignee",
+        "brand",
+        "calendar",
+        "catalognum",
+        "categories",
+        "Category",
+        "classname",
+        "data_source",
+        "default_start_time",
+        "default_end_time",
+        "Description",
+        "endpoint",
+        "entity",
+        "environment",
+        "epic_key",
+        "epic_priority",
+        "epic_status",
+        "event",
+        "from",
+        "full_name",
+        "group_source",
+        "issuetype",
+        "kind",
+        "label",
+        "mastering",
+        "media",
+        "module",
+        "operation",
+        "primary",
+        "priority",
+        "project",
+        "reporter",
+        "short_name",
+        "source",
+        "status",
+        "style",
+        "subtask_assignee",
+        "subtask_key",
+        "subtask_priority",
+        "subtask_status",
+        "symbol",
+        "table",
+        "to",
+        "tables",
+        "test",
+        "type",
+        "type_name",
+        "user",
+        "client",
+        "env",
+    ),
+    split_with_color: ("genre", "genres", "Interests"),
+    human_dt: (
+        "added",
+        "committedDate",
+        "created",
+        "due",
+        "done_date",
+        "start",
+        "end",
+        "entry",
+        "first_active",
+        "last_active",
+        "last_played",
+        "mtime",
+        "updated",
+        "providerPublishTime",
+        "release_date",
+    ),
+    md_panel: (
+        "answer",
+        "benefits",
+        "body",
+        "bodyHTML",
+        "comment",
+        "comments",
+        "creditText",
+        "description",
+        "Description",
+        "desc",
+        "doc",
+        "content",
+        "interview",
+        "notes",
+        "text",
+    ),
+}
+for func, fields in fields_by_func.items():
+    for field in fields:
+        FIELDS_MAP[field] = func
+
+
+def _get_val(value: float | str | RenderableType | None, field: str) -> RenderableType:
+    if value is None:
+        return "None"
+
+    if field.endswith(".py"):
+        return border_panel(syntax(value, "python"), title=field)
+
+    if isinstance(value, str):
+        value = format_string(value)
+
+    if isinstance(value, ConsoleRenderable):
+        return value
+
+    if field in FIELDS_MAP:
+        with suppress(Exception):
+            return FIELDS_MAP[field](value)
+
+    return str(value)
+
+
+@singledispatch
+def get_val(obj: JSONDict | object, field: str) -> Any:
+    """Definition of a generic get_val function."""
+
+
+@get_val.register
+def _(obj: dict, field: str) -> Any:  # type: ignore[type-arg]
+    return _get_val(obj.get(field), field)
+
+
+@get_val.register
+def _(obj: object, field: str) -> Any:
+    return _get_val(getattr(obj, field, None), field)
---
+++ packages/rich-tables/rich_tables-0.8.0-py3-none-any.whl/rich_tables/github.py
@@ -0,0 +1,569 @@
+"""Functionality to display data from GitHub API."""
+
+from __future__ import annotations
+
+import time
+from collections import defaultdict
+from dataclasses import dataclass
+from itertools import islice
+from typing import TYPE_CHECKING, Any, Callable, Protocol
+
+from rich import box
+from typing_extensions import Literal, TypedDict
+
+from .fields import FIELDS_MAP, get_val
+from .generic import flexitable
+from .utils import (
+    JSONDict,
+    border_panel,
+    fmt_time,
+    format_with_color,
+    format_with_color_on_black,
+    list_table,
+    md_panel,
+    new_table,
+    predictably_random_color,
+    simple_panel,
+    sortgroup_by,
+    syntax,
+    timestamp2datetime,
+    wrap,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable, Mapping
+
+    from rich.console import ConsoleRenderable, RenderableType
+    from rich.panel import Panel
+    from rich.syntax import Syntax
+    from rich.table import Table
+
+SECONDS_PER_DAY = 86400
+
+
+def b_green(text: str) -> str:
+    """Make the text bold green."""
+    return wrap(text, "b green")
+
+
+def b_red(text: str) -> str:
+    """Make the text bold red."""
+    return wrap(text, "b red")
+
+
+def diff_dt(timestamp: str | float, acc: int = 2) -> str:
+    try:
+        datetime = timestamp2datetime(timestamp)
+    except ValueError:
+        return str(timestamp)
+
+    diff = datetime.timestamp() - time.time()
+    fmted = " ".join(islice(fmt_time(int(diff)), acc))
+
+    strtime = datetime.strftime("%F" if abs(diff) >= SECONDS_PER_DAY else "%T")
+
+    return f"{(b_red if diff < 0 else b_green)(fmted)} {strtime}"
+
+
+def fmt_add_del(added: int, deleted: int) -> list[str]:
+    """Format added and deleted diff counts."""
+    additions = f"+{added}" if added else ""
+    deletions = f"-{deleted}" if deleted else ""
+    return [b_green(additions.rjust(5)), b_red(deletions.rjust(3))]
+
+
+def gh_md_panel(body: str, *args: Any, **kwargs: Any) -> Panel:
+    return md_panel(
+        body.replace(":rofl:", ":rolling_on_the_floor_laughing:").replace(
+            "suggestion", "python"
+        ),
+        *args,
+        **kwargs,
+    )
+
+
+COLOR_BY_STATE = defaultdict(
+    lambda: "default",
+    {
+        "True": "green",
+        True: "green",
+        "APPROVED": "green",
+        "RESOLVED": "s green",
+        "OPEN": "green",
+        "MERGED": "magenta",
+        "CLOSED": "b red",
+        "PENDING": "yellow",
+        "OUTDATED": "yellow",
+        "COMMENTED": "yellow",
+        "CHANGES_REQUESTED": "b red",
+        "REVIEW_REQUIRED": "red",
+        "DISMISSED": "gray42",
+        "False": "red",
+    },
+)
+
+
+def fmt_state(state: str) -> str:
+    return wrap(state, f"b {COLOR_BY_STATE[state]}")
+
+
+def diff_panel(title: str, rows: list[list[str]]) -> Panel:
+    return border_panel(
+        new_table(rows=rows),
+        title=title,
+        border_style=f"dim {predictably_random_color(title)}",
+    )
+
+
+PR_FIELDS_MAP: Mapping[str, Callable[..., RenderableType]] = {
+    "statusCheckRollup": lambda x: {
+        "SUCCESS": ":green_square:",
+        "FAILURE": ":red_square:",
+        "PENDING": ":yellow_square:",
+        "None": "",
+    }[x],
+    "state": lambda x: wrap(fmt_state(x), "b"),
+    "reviewDecision": lambda x: wrap(fmt_state(x), "b"),
+    "dates": lambda x: new_table(
+        rows=[
+            [b_green(r" ⬤ "), diff_dt(x[0])],
+            [wrap(r" ◯ ", "b yellow"), diff_dt(x[1])],
+        ]
+    ),
+    "path": lambda x: wrap(x, "b"),
+    "message": lambda x: wrap(x, "i"),
+    "files": lambda files: diff_panel(
+        "files",
+        [
+            [*fmt_add_del(f["additions"], f["deletions"]), get_val(f, "path")]
+            for f in files
+        ],
+    ),
+    "reviewRequests": format_with_color_on_black,
+    "participants": lambda x: "\n".join(
+        map(format_with_color, map("{:^20}".format, x))
+    ),
+}
+
+
+class Diff(TypedDict):
+    additions: int
+    deletions: int
+
+
+class File(Diff):
+    path: str
+
+
+@dataclass
+class Commit:
+    additions: int
+    deletions: int
+    committedDate: str
+    message: str
+    statusCheckRollup: str
+
+    @property
+    def diff(self) -> list[str]:
+        return fmt_add_del(self.additions, self.deletions)
+
+    @property
+    def parts(self) -> list[str]:
+        return [
+            *self.diff,
+            get_val(self, "statusCheckRollup"),
+            get_val(self, "message"),
+            get_val(self, "committedDate"),
+        ]
+
+
+@dataclass
+class Commits:
+    commits: list[Commit]
+
+    @property
+    def panel(self) -> Panel:
+        return diff_panel("commits", [commit.parts for commit in self.commits])
+
+
+@dataclass
+class Reaction:
+    user: str
+    content: str
+
+    def __str__(self) -> str:
+        return f":{self.content.lower()}: {get_val(self, 'user')}"
+
+
+class CreatedMixin(Protocol):
+    @property
+    def created(self) -> str:
+        raise NotImplementedError
+
+
+@dataclass
+class PanelMixin:
+    def get_title(self, fields: list[str]) -> str:
+        return " ".join(get_val(self, f) for f in fields)
+
+    @property
+    def panel(self) -> Panel:
+        raise NotImplementedError
+
+
+@dataclass
+class CreatedPanelMixin(CreatedMixin, PanelMixin):
+    pass
+
+
+@dataclass
+class Content(CreatedPanelMixin):
+    createdAt: str
+    author: str
+    body: str
+
+    @property
+    def created_at(self) -> str:
+        return f"""[white]{self.createdAt.replace("T", " ").replace("Z", "")}[/]"""
+
+    @property
+    def created(self) -> str:
+        return self.created_at
+
+
+@dataclass
+class Comment(Content):
+    reactions: list[Reaction]
+
+    @classmethod
+    def make(cls, reactions: list[JSONDict], **kwargs: Any) -> Comment:
+        kwargs["reactions"] = [Reaction(**c) for c in reactions]
+        return cls(**kwargs)
+
+    @property
+    def title(self) -> str:
+        return self.get_title(["author", "created_at"])
+
+    @property
+    def subtitle(self) -> str:
+        return (
+            " ".join(map(str, self.reactions))
+            .replace(":laugh:", ":laughing:")
+            .replace(":hooray:", ":party_popper:")
+        )
+
+
+@dataclass
+class IssueComment(Comment):
+    @property
+    def panel(self) -> Panel:
+        return border_panel(
+            list_table([gh_md_panel(self.body)]),
+            border_style="b yellow",
+            title=self.title,
+            subtitle=self.subtitle,
+            box=box.ROUNDED,
+        )
+
+
+@dataclass
+class ReviewComment(Comment):
+    outdated: bool
+    path: str
+    diffHunk: str
+    pullRequestReview: str
+
+    @property
+    def diff(self) -> Syntax:
+        return syntax(self.diffHunk, "diff")
+
+    @property
+    def review_id(self) -> str:
+        return self.pullRequestReview
+
+    @property
+    def panel(self) -> Panel:
+        return gh_md_panel(self.body, title=self.title, subtitle=self.subtitle)
+
+
+class ResolvedMixin:
+    @property
+    def resolved(self) -> bool:
+        raise NotImplementedError
+
+    @property
+    def border_color(self) -> str:
+        return "green" if self.resolved else "yellow"
+
+
+@dataclass
+class ReviewThread(CreatedPanelMixin, ResolvedMixin):
+    path: str
+    isResolved: bool
+    isOutdated: bool
+    resolvedBy: str
+    comments: list[ReviewComment]
+    verbose: bool
+
+    @property
+    def resolved(self) -> bool:
+        return self.isResolved
+
+    @classmethod
+    def make(cls, comments: list[JSONDict], **kwargs: Any) -> ReviewThread:
+        kwargs["comments"] = [ReviewComment.make(**c) for c in comments]
+        return cls(**kwargs)
+
+    @property
+    def review_id(self) -> str:
+        return self.comments[0].review_id
+
+    @property
+    def created(self) -> str:
+        return self.comments[0].created_at
+
+    @property
+    def formatted_state(self) -> str:
+        return (
+            (
+                fmt_state("RESOLVED")
+                + wrap(" by ", "white")
+                + format_with_color(self.resolvedBy)
+            )
+            if self.isResolved
+            else fmt_state("PENDING")
+        )
+
+    @property
+    def title(self) -> str:
+        return " ".join(
+            [
+                wrap(self.path, "b magenta"),
+                self.formatted_state,
+                fmt_state("OUTDATED") if self.isOutdated else "",
+            ]
+        )
+
+    @property
+    def panel(self) -> Panel:
+        comments = self.comments
+        content: RenderableType
+        if self.verbose or not self.resolved:
+            comments_col = list_table((c.panel for c in comments), padding=(1, 0, 0, 0))
+            content = new_table(
+                rows=[[self.comments[0].diff, simple_panel(comments_col)]],
+                highlight=False,
+            )
+        else:
+            content = ""
+        return border_panel(
+            content,
+            highlight=False,
+            border_style=self.border_color,
+            title=self.title,
+        )
+
+
+@dataclass
+class Review(Content, ResolvedMixin):
+    id: str
+    state: str
+    threads: list[ReviewThread]
+    comments: list[ReviewComment]
+
+    verbose: bool
+
+    @property
+    def resolved(self) -> bool:
+        return all(t.resolved for t in self.threads)
+
+    @property
+    def panel(self) -> Panel:
+        rows = []
+        if self.body:
+            rows.append(gh_md_panel(self.body))
+
+        if not self.resolved or self.verbose:
+            self.threads.sort(key=lambda t: t.resolved)
+            rows.extend(t.panel for t in self.threads)
+
+        return border_panel(
+            list_table(rows),
+            subtitle=self.state,
+            border_style=self.border_color,
+            title=self.title,
+            box=box.HEAVY,
+        )
+
+    @property
+    def status(self) -> str:
+        resolved_count = sum(t.isResolved for t in self.threads)
+        if total_count := len(self.threads):
+            return (
+                wrap("RESOLVED", COLOR_BY_STATE["RESOLVED"])
+                if total_count == resolved_count
+                else b_green(" ⬤ " * resolved_count)
+                + b_red(" ◯ " * (total_count - resolved_count))
+            )
+        return ""
+
+    @property
+    def title(self) -> str:
+        return self.get_title(["state", "author", "created_at", "status"])
+
+
+@dataclass
+class Issue:
+    number: int
+    title: str
+    state: Literal["OPEN", "CLOSED"]
+    url: str
+
+    @property
+    def status(self) -> str:
+        return {"OPEN": "[b green][/]", "CLOSED": "[b magenta][/]"}[self.state]
+
+    @property
+    def fmt(self) -> str:
+        return f"{self.title} {self.url}"
+
+
+@dataclass
+class PullRequest:
+    id: str
+    createdAt: str
+    author: str
+    body: str
+    additions: int
+    commits: Commits
+    deletions: int
+    files: list[File]
+    headRefName: str
+    labels: list[str]
+    participants: list[str]
+    repository: str
+    reviewDecision: str
+    reviewRequests: list[str]
+    reviewThreads: list[ReviewThread]
+    reviews: list[Review]
+    comments: list[Comment]
+    state: str
+    title: str
+    updatedAt: str
+    url: str
+    issues: list[Issue]
+
+    verbose: bool
+
+    @property
+    def dates(self) -> tuple[str, str]:
+        return self.createdAt, self.updatedAt
+
+
+@dataclass
+class PullRequestTable(PullRequest):
+    @classmethod
+    def make(cls, reviews: list[JSONDict], **kwargs: Any) -> PullRequestTable:
+        verbose = kwargs["verbose"]
+        threads = [
+            ReviewThread.make(**rt, verbose=verbose) for rt in kwargs["reviewThreads"]
+        ]
+        comments_by_review_id = dict(
+            sortgroup_by(
+                (c for t in threads for c in t.comments),
+                lambda c: c.review_id,
+            )
+        )
+        threads_by_review_id = dict(sortgroup_by(threads, lambda t: t.review_id))
+        return cls(
+            commits=Commits([Commit(**c) for c in kwargs.pop("commits", [])]),
+            comments=[IssueComment.make(**c) for c in kwargs.pop("comments", [])],
+            reviews=[
+                Review(
+                    **r,
+                    threads=threads_by_review_id.pop(r["id"], []),
+                    comments=comments_by_review_id.pop(r["id"], []),
+                    verbose=verbose,
+                )
+                for r in reviews
+                if (r["id"] in threads_by_review_id or r["body"])
+            ],
+            issues=[Issue(**i) for i in kwargs.pop("closingIssuesReferences")],
+            **kwargs,
+        )
+
+    @property
+    def name(self) -> str:
+        return wrap(self.title, f"b {predictably_random_color(self.title)}")
+
+    @property
+    def repo(self) -> str:
+        return wrap(self.repository, f"b {predictably_random_color(self.repository)}")
+
+    @property
+    def pr_state(self) -> str:
+        return "MERGED" if self.state == "MERGED" else self.reviewDecision
+
+    @property
+    def fixed_issues(self) -> RenderableType:
+        return list_table([i.fmt for i in self.issues])
+
+    @property
+    def info(self) -> Panel:
+        fields = (
+            "author",
+            "dates",
+            "headRefName",
+            "participants",
+            "reviewRequests",
+            "fixed_issues",
+        )
+        pairs = {f: v for f in fields if (v := getattr(self, f))}
+        field_rows = flexitable(pairs)
+        return border_panel(
+            new_table(
+                rows=[[field_rows], [gh_md_panel(self.body)], [self.files_commits]]
+            ),
+            title=f"{self.name} @ {self.repo}",
+            box=box.DOUBLE_EDGE,
+            border_style=COLOR_BY_STATE[self.pr_state],
+            subtitle=(
+                wrap(
+                    " ".join(
+                        [
+                            fmt_state(self.reviewDecision),
+                            wrap("//", "white"),
+                            fmt_state(self.state),
+                        ]
+                    ),
+                    "b",
+                )
+            ),
+            vertical_align="middle",
+            title_align="center",
+            subtitle_align="center",
+        )
+
+    @property
+    def files_commits(self) -> Table:
+        return new_table(rows=[[get_val(self, "files"), self.commits.panel]])
+
+    @property
+    def timestamped_contents(self) -> list[CreatedPanelMixin]:
+        return [*self.reviews, *self.comments]
+
+    @property
+    def panels(self) -> Iterable[Panel]:
+        for content in sorted(self.timestamped_contents, key=lambda c: c.created):
+            yield content.panel
+
+
+def pulls_table(
+    data: list[Mapping[str, Any]], **kwargs: Any
+) -> Iterable[str | ConsoleRenderable]:
+    FIELDS_MAP.update(PR_FIELDS_MAP)
+
+    pr = data[0]
+    pr_table = PullRequestTable.make(**pr, verbose=kwargs["verbose"])
+    yield pr_table.info
+    yield from pr_table.panels
