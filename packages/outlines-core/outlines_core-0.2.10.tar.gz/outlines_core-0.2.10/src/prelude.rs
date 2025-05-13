a55f419eb78b05bc4d9903dc0a79a94bcdf61a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 06:17:00 +0000

    Add package outlines-core/0.2.10/outlines_core-0.2.10.tar.gz

---
+++ packages/outlines-core/outlines_core-0.2.10.tar.gz/outlines_core-0.2.10/src/prelude.rs
@@ -0,0 +1,8 @@
+//! Library's interface essentials.
+
+pub use tokenizers::FromPretrainedParameters;
+
+pub use super::index::Index;
+pub use super::json_schema;
+pub use super::primitives::{StateId, Token, TokenId};
+pub use super::vocabulary::Vocabulary;
