0aa3f4ef73fbfea91e0f54333379aa4a046d10e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:20 +0000

    Add package crunch-cli/6.4.2/crunch_cli-6.4.2-py3-none-any.whl

---
+++ packages/crunch-cli/crunch_cli-6.4.2-py3-none-any.whl/crunch/command/test.py
@@ -0,0 +1,53 @@
+import importlib.util
+import os
+import sys
+import typing
+
+from .. import api, tester, unstructured
+
+
+def test(
+    main_file_path: str,
+    model_directory_path: str,
+    force_first_train: bool,
+    train_frequency: int,
+    round_number: str,
+    has_gpu: bool,
+    checks: bool,
+    no_determinism_check: typing.Optional[bool],
+):
+    _, project = api.Client.from_project()
+    competition = project.competition.reload()
+
+    runner_module = None
+    if competition.format == api.CompetitionFormat.UNSTRUCTURED:
+        loader = unstructured.deduce_code_loader(competition.name, "runner")
+        runner_module = unstructured.RunnerModule.load(loader)
+
+    spec = importlib.util.spec_from_file_location("user_code", main_file_path)
+    module = importlib.util.module_from_spec(spec)
+
+    sys.path.insert(0, os.getcwd())
+    spec.loader.exec_module(module)
+
+    prediction = tester.run(
+        module,
+        runner_module,
+        model_directory_path,
+        force_first_train,
+        train_frequency,
+        round_number,
+        competition,
+        has_gpu,
+        checks,
+        no_determinism_check,
+    )
+
+    if prediction is not None:
+        logger = tester.logger
+        logger.warning('prediction=\n%s', prediction)
+        logger.warning('')
+        logger.warning('local test succesfully run!')
+        logger.warning('')
+
+    return prediction
