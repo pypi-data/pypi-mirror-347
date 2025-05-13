a24a53ad252ae02c40408331b8a5a887134dd300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 03:25:51 +0000

    Add package nr-metadata/2.0.70/nr_metadata-2.0.70-py2.py3-none-any.whl

---
+++ packages/nr-metadata/nr_metadata-2.0.70-py2.py3-none-any.whl/nr_metadata/common/resources/records/resource.py
@@ -0,0 +1,8 @@
+from invenio_drafts_resources.resources import RecordResource
+
+
+class CommonResource(RecordResource):
+    """CommonRecord resource."""
+
+    # here you can for example redefine
+    # create_url_rules function to add your own rules
