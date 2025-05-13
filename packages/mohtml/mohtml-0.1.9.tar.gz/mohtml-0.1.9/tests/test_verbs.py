36d8ebd229161e80646e5ba6813d97b77b52f969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:16 +0000

    Add package mohtml/0.1.9/mohtml-0.1.9.tar.gz

---
+++ packages/mohtml/mohtml-0.1.9.tar.gz/mohtml-0.1.9/tests/test_verbs.py
@@ -0,0 +1,10 @@
+from mohtml import div, p, br
+
+def test_basic_div_p():
+    assert str(div(p("hello"))) == "<div><p>hello</p></div>"
+
+def test_basic_div_p_br():
+    assert str(div(p("hello"), br())) == "<div><p>hello</p><br/></div>"
+
+def test_basic_klass():
+    assert str(div(p("hello"), klass="foo bar")) == "<div class=\"foo bar\"><p>hello</p></div>"
