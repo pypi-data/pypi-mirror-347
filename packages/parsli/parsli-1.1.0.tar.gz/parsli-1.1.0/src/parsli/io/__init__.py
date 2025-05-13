490db87147dbdae7d57eb8fc5d997c327ca9efcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:17 +0000

    Add package parsli/1.1.0/parsli-1.1.0.tar.gz

---
+++ packages/parsli/parsli-1.1.0.tar.gz/parsli-1.1.0/src/parsli/io/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from .coast import VtkCoastLineSource
+from .mesh import VtkMeshReader
+from .segment import VtkSegmentReader
+
+__all__ = [
+    "VtkCoastLineSource",
+    "VtkMeshReader",
+    "VtkSegmentReader",
+]
