2299de0d5d665ad06497d45f694c887e6d03444a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 05:13:51 +0000

    Add package ln-markets/2.0.10/ln_markets-2.0.10.tar.gz

---
+++ packages/ln-markets/ln_markets-2.0.10.tar.gz/ln_markets-2.0.10/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='ln-markets',
+    version='2.0.10',
+    packages=['lnmarkets'],
+    description='LN Markets API python implementation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ln-markets/api-python',
+    author='Romain ROUPHAEL',
+    license='MIT',
+    keywords='lnmarkets trading rest api bitcoin lightning network futures options',
+    install_requires=[
+        'requests',
+        'websocket-client'
+      ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
