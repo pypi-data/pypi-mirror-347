3460b0576c3e0e7e9c1684d1c6230032d30a9adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 06:16:21 +0000

    Add package proknow/0.22.0/proknow-0.22.0-py2.py3-none-any.whl

---
+++ packages/proknow/proknow-0.22.0-py2.py3-none-any.whl/tests/pktestconfig.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+base_url = "http://dev.proknow.test:3005"
+credentials_id = "681ba85457806cfdc666d0acc3bebfcd"
+credentials_secret = "YnuY4CO0J8vKvRD5FkGOf2ccDB1EcEZOcl5czbHgtOghnbAfYlLkiT8WFb86lQaK"
