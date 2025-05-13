c0cd25d721a9a46b191ae134eb6dcbec0890ff5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 17:06:22 +0000

    Add package Advantech-EdgeSync.edge/0.0.0a1/advantech_edgesync_edge-0.0.0a1.tar.gz

---
+++ packages/Advantech-EdgeSync.edge/advantech_edgesync_edge-0.0.0a1.tar.gz/advantech_edgesync_edge-0.0.0a1/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "Advantech-EdgeSync.edge",
+    version = "0.0.0a1",
+    #version = "0.0.0a6",       # for TestPyPI
+    author = 'Advantech Co., Ltd.',
+    author_email = '',
+    packages = find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires = ["setuptools"]
+)
