dc73ebdd78fcdb2db9c23f4341c15636e48fa3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-13 07:07:17 +0000

    Add package text-prettifier/1.2.0/text_prettifier-1.2.0-py3-none-any.whl

---
+++ packages/text-prettifier/text_prettifier-1.2.0-py3-none-any.whl/text_prettifier/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utility modules for text_prettifier.
+
+This package contains utility classes and functions used by TextPrettifier.
+"""
+
+from .patterns import Patterns
+
+__all__ = ['Patterns']
