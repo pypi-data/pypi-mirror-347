3022519ea4a7c0b7625af970baf18b607513d9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 07:07:55 +0000

    Add package dandi/0.68.1/dandi-0.68.1-py3-none-any.whl

---
+++ packages/dandi/dandi-0.68.1-py3-none-any.whl/dandi/cli/tests/test_cmd_validate.py
@@ -0,0 +1,131 @@
+import json
+from pathlib import Path
+
+from click.testing import CliRunner
+import pytest
+
+from ..cmd_validate import _process_issues, validate
+from ...tests.fixtures import BIDS_ERROR_TESTDATA_SELECTION
+from ...validate_types import (
+    Origin,
+    OriginType,
+    Scope,
+    Severity,
+    ValidationResult,
+    Validator,
+)
+
+
+@pytest.mark.parametrize("dataset", BIDS_ERROR_TESTDATA_SELECTION)
+def test_validate_bids_error(bids_error_examples: Path, dataset: str) -> None:
+    broken_dataset = bids_error_examples / dataset
+    with (broken_dataset / ".ERRORS.json").open() as f:
+        expected_errors = json.load(f)
+    r = CliRunner().invoke(validate, [str(broken_dataset)])
+    # Does it break?
+    assert r.exit_code == 1
+    # Does it detect all errors?
+    for key in expected_errors:
+        assert key in r.output
+
+
+def test_validate_severity(organized_nwb_dir3: Path) -> None:
+    """
+    Can we specify a severity floor?
+    """
+    r = CliRunner().invoke(
+        validate, ["--grouping=path", "--min-severity=ERROR", str(organized_nwb_dir3)]
+    )
+    # Is the usage correct?
+    assert r.exit_code == 0
+    # Is the WARNING-level issue reporting suppressed?
+    assert "NWBI.check_data_orientation" not in r.output
+
+
+def test_validate_nwb_error(simple3_nwb: Path) -> None:
+    """Do we fail on critical NWB validation errors?"""
+    r = CliRunner().invoke(validate, [str(simple3_nwb)])
+    # does it fail? as per:
+    # https://github.com/dandi/dandi-cli/pull/1157#issuecomment-1312546812
+    assert r.exit_code != 0
+
+
+def test_validate_ignore(simple2_nwb: Path) -> None:
+    r = CliRunner().invoke(validate, [str(simple2_nwb)])
+    assert r.exit_code != 0
+    assert "DANDI.NO_DANDISET_FOUND" in r.output
+    r = CliRunner().invoke(validate, ["--ignore=NO_DANDISET_FOUND", str(simple2_nwb)])
+    assert r.exit_code == 0, r.output
+    assert "DANDI.NO_DANDISET_FOUND" not in r.output
+
+
+def test_validate_nwb_path_grouping(organized_nwb_dir4: Path) -> None:
+    """
+    Does grouping of issues by path work?
+    """
+    r = CliRunner().invoke(validate, ["--grouping=path", str(organized_nwb_dir4)])
+    assert r.exit_code == 0
+
+    # Do paths with issues appear only once?
+    assert r.output.count("sub-mouse004.nwb") == 1
+    assert r.output.count("sub-mouse001.nwb") == 1
+
+    # Do issues affecting multiple paths get listed multiple times?
+    assert r.output.count("NWBI.check_data_orientation") >= 2
+
+
+def test_process_issues(capsys):
+    origin_validation_nwbinspector = Origin(
+        type=OriginType.VALIDATION,
+        validator=Validator.nwbinspector,
+        validator_version="",
+    )
+
+    issues = [
+        ValidationResult(
+            id="NWBI.check_data_orientation",
+            origin=origin_validation_nwbinspector,
+            scope=Scope.FILE,
+            message="Data may be in the wrong orientation.",
+            path=Path("dir0/sub-mouse004/sub-mouse004.nwb"),
+            severity=Severity.WARNING,
+        ),
+        ValidationResult(
+            id="NWBI.check_data_orientation",
+            origin=origin_validation_nwbinspector,
+            scope=Scope.FILE,
+            message="Data may be in the wrong orientation.",
+            path=Path("dir1/sub-mouse001/sub-mouse001.nwb"),
+            severity=Severity.WARNING,
+        ),
+        ValidationResult(
+            id="NWBI.check_missing_unit",
+            origin=origin_validation_nwbinspector,
+            scope=Scope.FILE,
+            message="Missing text for attribute 'unit'.",
+            path=Path("dir1/sub-mouse001/sub-mouse001.nwb"),
+            severity=Severity.WARNING,
+        ),
+    ]
+    _process_issues(issues, grouping="path")
+    captured = capsys.readouterr().out
+
+    # Do paths with issues appear only once?
+    assert captured.count("sub-mouse004.nwb") == 1
+    assert captured.count("sub-mouse001.nwb") == 1
+
+    # Do issues affecting multiple paths get listed multiple times?
+    assert captured.count("NWBI.check_data_orientation") >= 2
+
+
+def test_validate_bids_error_grouping_notification(
+    bids_error_examples: Path, dataset: str = "invalid_asl003"
+) -> None:
+    """Test user notification for unimplemented parameter value."""
+    broken_dataset = bids_error_examples / dataset
+    r = CliRunner().invoke(validate, ["--grouping=error", str(broken_dataset)])
+    # Does it break?
+    assert r.exit_code == 2
+    # Does it notify the user correctly?
+    notification_substring = "Invalid value for '--grouping'"
+    assert notification_substring in r.output
