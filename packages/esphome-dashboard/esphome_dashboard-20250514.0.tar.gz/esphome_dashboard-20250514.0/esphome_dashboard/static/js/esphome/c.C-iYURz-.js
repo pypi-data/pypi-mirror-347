9cc34464464bc10769fbc8f638945107f9b53d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:05:58 +0000

    Add package esphome-dashboard/20250514.0/esphome_dashboard-20250514.0.tar.gz

---
+++ packages/esphome-dashboard/esphome_dashboard-20250514.0.tar.gz/esphome_dashboard-20250514.0/esphome_dashboard/static/js/esphome/c.C-iYURz-.js
@@ -0,0 +1,2 @@
+import{Z as s,$ as a}from"./index-D2ez82lt.js";const i="wifi_ssid",n="wifi_password",o=async()=>{const s=await a();return s.includes(i)&&s.includes(n)},r=async(a,o)=>s({[i]:a,[n]:o},"Your Wi-Fi SSID and password");export{n as S,i as a,o as c,r as s};
+//# sourceMappingURL=c.C-iYURz-.js.map
