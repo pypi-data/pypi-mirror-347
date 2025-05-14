2b3cb70b462abf3a7e9d9d7311daeb62300c2df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 16:05:54 +0000

    Add package tol-sdk/1.5.1/tol_sdk-1.5.1-py3-none-any.whl

---
+++ packages/tol-sdk/tol_sdk-1.5.1-py3-none-any.whl/tol/sources/tolid.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+import os
+
+from .defaults import Defaults
+from ..api_client import (
+    create_api_datasource
+)
+from ..core import (
+    core_data_object
+)
+
+
+def tolid(retries: int = 5):
+    tolid = create_api_datasource(
+        api_url=os.getenv('TOLID_URL', Defaults.TOLID_URL)
+        + os.getenv('TOLID_API_PATH', Defaults.TOLID_API_PATH),
+        token=os.getenv('TOLID_API_KEY'),
+        data_prefix=os.getenv('TOLID_API_DATA_PATH', Defaults.TOLID_API_DATA_PATH),
+        retries=retries
+    )
+    core_data_object(tolid)
+    return tolid
