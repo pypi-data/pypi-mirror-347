6a5c3c4762293e05252a6bf200bd76cb5d2a2290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 05:05:16 +0000

    Add package saviialib/0.8.0/saviialib-0.8.0.tar.gz

---
+++ packages/saviialib/saviialib-0.8.0.tar.gz/saviialib-0.8.0/src/saviialib/services/epii/use_cases/constants/upload_backup_to_sharepoint_constants.py
@@ -0,0 +1,5 @@
+SHAREPOINT_BASE_URL = "/sites/uc365_CentrosyEstacionesRegionalesUC/Shared%20Documents/General/Test_Raspberry"
+DESTINATION_FOLDERS = {
+    "CAM1": "Camaras_Trampa",
+    "RECORDS1": "Grabaciones",
+}
