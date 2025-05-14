8603de354e7505bc9e9036d8be7c8e7c8ce1819e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:05:39 +0000

    Add package types-networkx/3.4.2.20250514/types_networkx-3.4.2.20250514.tar.gz

---
+++ packages/types-networkx/types_networkx-3.4.2.20250514.tar.gz/types_networkx-3.4.2.20250514/networkx-stubs/algorithms/components/attracting.pyi
@@ -0,0 +1,13 @@
+from _typeshed import Incomplete
+from collections.abc import Generator
+
+from networkx.utils.backends import _dispatchable
+
+__all__ = ["number_attracting_components", "attracting_components", "is_attracting_component"]
+
+@_dispatchable
+def attracting_components(G) -> Generator[Incomplete, None, None]: ...
+@_dispatchable
+def number_attracting_components(G): ...
+@_dispatchable
+def is_attracting_component(G): ...
