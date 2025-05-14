16d0d5094237987aee88ee72686283729dfac93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:14 +0000

    Add package pygameControls/0.2.7/pygamecontrols-0.2.7.tar.gz

---
+++ packages/pygameControls/pygamecontrols-0.2.7.tar.gz/pygamecontrols-0.2.7/README.md
@@ -0,0 +1,6 @@
+# PyGame-Controller
+
+Small module to make it easy to add controller support.
+
+![Tests](https://gitpot-lerking.servehttp.com/api/v1/badges/CodingPirates/PyGame-Controller/workflows/test.yml)
+![Version](https://img.shields.io/badge/version-0.2.5-blue)
---
+++ packages/pygameControls/pygamecontrols-0.2.7.tar.gz/pygamecontrols-0.2.7/pygameControls/controller.py
@@ -0,0 +1,19 @@
+import pygame
+from . import globals
+
+__version__ = "0.2.7"
+
+class Controllers:
+    def __init__(self, joy):
+        globals.init()
+        self.controllers = []
+        cont = self.detect_controller(joy.get_guid())
+        self.controllers.append(cont(joy))
+
+    def detect_controller(self, guid):
+        for gp in globals.GAMEPADS:
+            for p in globals.GAMEPADS[gp]:
+                if p["guid"] != guid:
+                    continue
+                return p["class"]
+        return globals.CONTROLLERS["Generic Controller"]
