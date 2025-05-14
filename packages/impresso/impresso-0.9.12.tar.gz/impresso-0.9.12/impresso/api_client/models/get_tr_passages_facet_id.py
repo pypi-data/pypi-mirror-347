382022a61adce75494ae0d9272bef2880543cfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:32 +0000

    Add package impresso/0.9.12/impresso-0.9.12.tar.gz

---
+++ packages/impresso/impresso-0.9.12.tar.gz/impresso-0.9.12/impresso/api_client/models/get_tr_passages_facet_id.py
@@ -0,0 +1,46 @@
+from enum import Enum
+from typing import Literal
+
+
+class GetTrPassagesFacetId(str, Enum):
+    COLLECTION = "collection"
+    CONNECTEDCLUSTERS = "connectedClusters"
+    COUNTRY = "country"
+    DATERANGE = "daterange"
+    LANGUAGE = "language"
+    LOCATION = "location"
+    NAG = "nag"
+    NEWSPAPER = "newspaper"
+    PERSON = "person"
+    TEXTREUSECLUSTER = "textReuseCluster"
+    TEXTREUSECLUSTERDAYDELTA = "textReuseClusterDayDelta"
+    TEXTREUSECLUSTERLEXICALOVERLAP = "textReuseClusterLexicalOverlap"
+    TEXTREUSECLUSTERSIZE = "textReuseClusterSize"
+    TOPIC = "topic"
+    TYPE = "type"
+    YEAR = "year"
+    YEARMONTH = "yearmonth"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+GetTrPassagesFacetIdLiteral = Literal[
+    "collection",
+    "connectedClusters",
+    "country",
+    "daterange",
+    "language",
+    "location",
+    "nag",
+    "newspaper",
+    "person",
+    "textReuseCluster",
+    "textReuseClusterDayDelta",
+    "textReuseClusterLexicalOverlap",
+    "textReuseClusterSize",
+    "topic",
+    "type",
+    "year",
+    "yearmonth",
+]
