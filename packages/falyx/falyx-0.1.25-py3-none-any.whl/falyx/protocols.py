811c4eed3386faf246ece7d0d8deefbf023a7f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:07:04 +0000

    Add package falyx/0.1.25/falyx-0.1.25-py3-none-any.whl

---
+++ packages/falyx/falyx-0.1.25-py3-none-any.whl/falyx/protocols.py
@@ -0,0 +1,11 @@
+# Falyx CLI Framework — (c) 2025 rtj.dev LLC — MIT Licensed
+"""protocols.py"""
+from __future__ import annotations
+
+from typing import Any, Protocol
+
+from falyx.action.action import BaseAction
+
+
+class ActionFactoryProtocol(Protocol):
+    def __call__(self, *args: Any, **kwargs: Any) -> BaseAction: ...
