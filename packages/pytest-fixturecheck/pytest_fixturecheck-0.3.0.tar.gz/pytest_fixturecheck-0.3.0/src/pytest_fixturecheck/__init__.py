0cfe14d67414287b458010593f4b9128efc0955d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:07 +0000

    Add package pytest-fixturecheck/0.3.0/pytest_fixturecheck-0.3.0.tar.gz

---
+++ packages/pytest-fixturecheck/pytest_fixturecheck-0.3.0.tar.gz/pytest_fixturecheck-0.3.0/src/pytest_fixturecheck/__init__.py
@@ -0,0 +1,46 @@
+"""pytest-fixturecheck - A pytest plugin to validate fixtures before tests."""
+
+from .decorator import fixturecheck
+from .validators import (
+    is_instance_of,
+    has_required_fields,
+    has_required_methods,
+    has_property_values,
+    combines_validators,
+)
+from .django import (
+    is_django_model,
+    django_model_has_fields,
+    django_model_validates,
+)
+from .utils import creates_validator
+from .validators_fix import (
+    property_values_validator,
+    check_property_values,
+    with_property_values,
+)
+
+__version__ = "0.3.0"
+
+__all__ = [
+    "fixturecheck",
+    "creates_validator",
+    
+    # Validators
+    "is_instance_of",
+    "has_required_fields",
+    "has_required_methods",
+    "has_property_values",  # Keep for backward compatibility
+    "property_values_validator",  # Fixed property validator that works with a dictionary
+    "check_property_values",  # Fixed property validator that works with keyword arguments
+    "with_property_values",    # Fixed with_property_values factory function
+    "combines_validators",
+    
+    # Django validators
+    "is_django_model",
+    "django_model_has_fields",
+    "django_model_validates",
+]
+
+# Add our fixed with_property_values to fixturecheck
+fixturecheck.with_property_values = with_property_values
