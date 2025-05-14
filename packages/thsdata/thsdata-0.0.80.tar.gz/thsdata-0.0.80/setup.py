8a801a6a04722e75b3f4eb069c39e23b53cfdd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:07:35 +0000

    Add package thsdata/0.0.80/thsdata-0.0.80.tar.gz

---
+++ packages/thsdata/thsdata-0.0.80.tar.gz/thsdata-0.0.80/setup.py
@@ -0,0 +1,45 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.80'
+DESCRIPTION = '金融行情数据查询API'
+LONG_DESCRIPTION = 'a python package for thsdata. 金融行情数据查询API。'
+
+# Setting up
+setup(
+    name="thsdata",
+    version=VERSION,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(where='.', exclude=(), include=('*',)),
+    include_package_data=True,
+    install_requires=[
+        'pandas>=2.2.2',
+        'thsdk>=0.0.7',
+        'pytz>=2025.1',
+        'requests>=2.22.2',
+        'setuptools>=75.2.0',
+    ],
+    keywords=['python', 'thsdata'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_data={
+        'thsdata': ['*'],
+    },
+)
