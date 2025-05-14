e2db047935494edffdec6bbe4551e08362fad725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:14 +0000

    Add package gcp-connector/0.1.3/gcp_connector-0.1.3.tar.gz

---
+++ packages/gcp-connector/gcp_connector-0.1.3.tar.gz/gcp_connector-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gcp_connector",
+    version="0.1.3",
+    description="""Libreria de conexion a proyectos GCP,
+    uso BigQuery, Google Sheets.""",
+    author="Daniel Cristancho",
+    author_email="daniersdan81@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==2.2.3",
+        "google-auth==2.38.0",
+        "google-auth-oauthlib==1.2.1",
+        "google-api-python-client==2.160.0",
+        "google-auth-httplib2==0.2.0",
+        "google-cloud-bigquery==3.29.0",
+        "google-crc32c==1.6.0",
+        "pygsheets==2.0.6",
+        "pyarrow==19.0.0",
+        "db-dtypes==1.4.2",
+    ],
+    python_requires=">=3.9",
+)
