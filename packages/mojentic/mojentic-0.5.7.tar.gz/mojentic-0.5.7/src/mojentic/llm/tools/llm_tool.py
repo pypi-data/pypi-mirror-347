396e550b8b9c4b8ca6a3d76327e25e7b50cd0561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:06:17 +0000

    Add package mojentic/0.5.7/mojentic-0.5.7.tar.gz

---
+++ packages/mojentic/mojentic-0.5.7.tar.gz/mojentic-0.5.7/src/mojentic/llm/tools/llm_tool.py
@@ -0,0 +1,33 @@
+import json
+
+from mojentic.llm.gateways.models import TextContent
+
+
+class LLMTool:
+    def run(self, **kwargs):
+        raise NotImplementedError
+
+    def call_tool(self, **kwargs):
+        result = self.run(**kwargs)
+        if isinstance(result, dict):
+            result = json.dumps(result)
+        return {
+            "content": [
+                TextContent(type="text", text=result).model_dump(),
+            ]
+        }
+
+    @property
+    def descriptor(self):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        return self.descriptor["function"]["name"]
+
+    @property
+    def description(self):
+        return self.descriptor["function"]["description"]
+
+    def matches(self, name: str):
+        return name == self.name
