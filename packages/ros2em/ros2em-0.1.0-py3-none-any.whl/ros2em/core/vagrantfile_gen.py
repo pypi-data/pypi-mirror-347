d3c8ad1aa6be404f9f179e743a70e446fcc1cbef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:23 +0000

    Add package ros2em/0.1.0/ros2em-0.1.0-py3-none-any.whl

---
+++ packages/ros2em/ros2em-0.1.0-py3-none-any.whl/ros2em/core/vagrantfile_gen.py
@@ -0,0 +1,20 @@
+def generate_vagrantfile(env_path, name, host_ports, cpu, ram):
+    content = f"""
+Vagrant.configure("2") do |config|
+    config.vm.define "{name}" do |vm|
+        vm.vm.box = ros2em/ros2-humble"
+        vm.vm.hostname = "{name}"
+
+        vm.vm.network "forwarded_port", guest: 6080, host: {host_ports['vnc']}, host_ip: "127.0.0.1"
+        
+        vm.vm.provider "virtualbox" do |vb|
+            vb.name = "{name}"
+            vb.memory = "{ram}"
+            vb.cpus = {cpu}
+        end
+    end
+end
+""".strip()
+    
+    with open(env_path / "Vagrantfile", "w") as f:
+        f.write(content)
