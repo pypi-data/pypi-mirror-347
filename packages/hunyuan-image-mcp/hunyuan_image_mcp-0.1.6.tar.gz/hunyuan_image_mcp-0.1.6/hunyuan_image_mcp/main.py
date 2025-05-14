e9d10b498f754be7b0b7712b56dca02dd65d2874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:07:08 +0000

    Add package hunyuan-image-mcp/0.1.6/hunyuan_image_mcp-0.1.6.tar.gz

---
+++ packages/hunyuan-image-mcp/hunyuan_image_mcp-0.1.6.tar.gz/hunyuan_image_mcp-0.1.6/hunyuan_image_mcp/main.py
@@ -0,0 +1,104 @@
+from mcp.server.fastmcp import FastMCP
+from mcp.types import TextContent
+import os
+import logging
+import httpx
+from dataclasses import dataclass, asdict
+from typing import Any, Optional, List, Dict, Annotated
+from pydantic import Field
+
+from hunyuan_image_mcp.exceptions import *
+
+# Initialize FastMCP server
+mcp = FastMCP(
+    "hunyuan-image-mcp",
+    host="0.0.0.0",
+    port=os.getenv("PORT", 8000)
+)
+
+apiKey = os.getenv("API_KEY", "")
+
+headers = {
+    "X-Source": "web",
+    "Content-Type": "application/json",
+    "Authorization": apiKey
+}
+
+
+@mcp.tool(
+    description="""通过文字描述生成图片，或者根据要求修改图片
+        
+    Args:
+        imageUrl: 需要修改的图片url地址，如不提供则生成新图片
+        prompt: 用于生成或修改图片的详细提示词，建议提供具体描述
+
+    Returns:
+        dict: 包含生成的图片url地址的字典
+    """
+)
+async def hunyuanText2Image(
+    imageUrl: Annotated[
+        str, 
+        Field(default="", description="需要修改的图片url地址，如不提供则生成新图片")], 
+    prompt: Annotated[str, Field(description="用于生成或修改图片的详细提示词，建议提供具体描述")]
+) -> dict:
+    # """
+    # 通过文字描述生成图片，或者根据要求修改图片
+    
+    # Args:
+    #     imageUrl: 需要修改的图片url地址
+    #     prompt: 用于生成图片或者修改图片的提示词
+    # """
+    
+    hunyuanText2ImageUrl = "https://yuanqi.tencent.com/openapi/v1/tools/call"
+
+    payload = {
+        "jsonrpc": "2.0",
+        "id": "22222",
+        "method": "tools/call",
+        "params": {
+            "name":"hunyuanText2Image",
+            "arguments": {
+                "image_url": imageUrl,
+                "prompt": prompt
+            }
+        }
+    }
+
+    logging.info(f"开始调用混元文本转图像")
+
+    try:
+        timeout = httpx.Timeout(60.0, connect=30.0)
+        response = httpx.post(hunyuanText2ImageUrl, headers=headers, json=payload, timeout=timeout)
+        response_json = response.json()
+        if "error" in response_json:
+            error_data = response_json["error"]
+            return TextContent(
+                type="text",
+                text=str(error_data)
+            )
+        
+        return TextContent(
+            type="text",
+            text=response_json["result"]["content"][0]["text"]
+        )
+    except YuanqiAPIError as e:
+        return TextContent(
+            type="text",
+            text=f"调用工具失败: {str(e)}"
+        )
+
+
+def run_mcp():
+    print("starting")
+    if os.getenv("TYPE") == "sse":
+        print("starting sse")
+        mcp.run(transport="sse")
+    else:
+        print("starting stdio")
+        mcp.run(transport="stdio")
+
+if __name__ == '__main__':
+    print("starting main")
+    logging.info("开始运行腾讯混元生图")
+    run_mcp()
