456cf36ed69e6942f97a9fe2904ed3d7a4d1ccad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 16:06:06 +0000

    Add package nle/1.2.0/nle-1.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nle/nle-1.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nle/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# TODO(heiner): Consider using __all__ as ruff suggests.
+# ruff: noqa
+
+# This is needed to register envs
+import nle.env
+
+from .version import __version__
