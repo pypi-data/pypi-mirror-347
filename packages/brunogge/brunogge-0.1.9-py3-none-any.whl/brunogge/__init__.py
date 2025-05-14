a1dd329ad55d4b87e3fc8606be20ae78a47dcc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 06:08:29 +0000

    Add package brunogge/0.1.9/brunogge-0.1.9-py3-none-any.whl

---
+++ packages/brunogge/brunogge-0.1.9-py3-none-any.whl/brunogge/__init__.py
@@ -0,0 +1,6 @@
+from .get_ws import ggs_ws
+from .ggs_stuff import ggs_login, keeping
+from .just_funcs import fakescanning, check_updates
+from .getconfig import getconfig
+__all__ = [ "ggs_ws", "ggs_login", "fakescanning", "getconfig", "keeping", "check_updates" ]
+__version__ = "0.1.9"
