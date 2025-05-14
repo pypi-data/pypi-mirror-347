418b7fc949631b153836cd8d5dff8cbeeb5a0862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:23 +0000

    Add package gcsecs/0.1/gcsecs-0.1.tar.gz

---
+++ packages/gcsecs/gcsecs-0.1.tar.gz/gcsecs-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gcsecs",
+    version="0.1",
+    description="AQA GCSE Computer Science Python Support Library",
+    packages=find_packages(),
+    author="Your Name",
+    author_email="you@example.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+    ],
+)
