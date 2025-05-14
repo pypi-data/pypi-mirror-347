0e5e310c94defab0f55c26f23d8f891dc5f638f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 17:06:23 +0000

    Add package myocr-kit/0.1.0/myocr_kit-0.1.0.tar.gz

---
+++ packages/myocr-kit/myocr_kit-0.1.0.tar.gz/myocr_kit-0.1.0/myocr/pipelines/structured_output_pipeline.py
@@ -0,0 +1,35 @@
+import logging
+from typing import Union
+
+import numpy as np
+import yaml  # type: ignore
+
+from ..extractor.chat_extractor import OpenAiChatExtractor
+from ..pipelines.common_ocr_pipeline import CommonOCRPipeline
+
+logger = logging.getLogger(__name__)
+
+
+class StructuredOutputOCRPipeline(CommonOCRPipeline):
+    def __init__(self, device, json_schema):
+        super().__init__(device)
+
+        parts = __file__.split(".")[0].rsplit("/", 1)
+        with open(parts[0] + "/config/" + parts[1] + ".yaml", "r", encoding="utf-8") as f:
+            config = yaml.safe_load(f)
+
+        self.extractor = OpenAiChatExtractor(
+            model=config["chat_bot"]["model"],
+            base_url=config["chat_bot"]["base_url"],
+            api_key=config["chat_bot"]["api_key"],
+        )
+        self.set_response_format(json_schema)
+
+    def process(self, img: Union[bytes, str, np.ndarray]):
+        rec = super().process(img)
+        return self.extractor.extract_with_format(rec.get_content_text(), self.response_format)
+
+    def set_response_format(self, json_schema):
+        from pydantic import BaseModel
+
+        self.response_format: BaseModel = json_schema
