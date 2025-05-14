1228705798c1ee72847b947e356012ef7774f0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:12 +0000

    Add package colav-protobuf-utils/0.1.5/colav_protobuf_utils-0.1.5.tar.gz

---
+++ packages/colav-protobuf-utils/colav_protobuf_utils-0.1.5.tar.gz/colav_protobuf_utils-0.1.5/src/colav_protobuf_utils/serialization/serializer.py
@@ -0,0 +1,39 @@
+from colav_protobuf import (
+    MissionRequest,
+    MissionResponse, 
+    ObstaclesUpdate,
+    AgentUpdate,
+    ControllerFeedback,
+    UnsafeSet
+)
+from typing import Union
+
+
+def serialize_protobuf(
+    protobuf: Union[
+        MissionRequest,
+        MissionResponse,
+        AgentUpdate,
+        ObstaclesUpdate,
+        ControllerFeedback,
+        UnsafeSet
+
+    ]
+) -> bytes:
+    if not isinstance(
+        protobuf,
+        (
+            MissionRequest,
+            MissionResponse,
+            AgentUpdate,
+            ObstaclesUpdate,
+            ControllerFeedback,
+            UnsafeSet,
+        ),
+    ):
+        raise TypeError("protobuf must be one of the defined types in the Union")
+
+    try:
+        return protobuf.SerializeToString()
+    except Exception as e:
+        raise Exception(f"Error serializing protobuf: {e}")
