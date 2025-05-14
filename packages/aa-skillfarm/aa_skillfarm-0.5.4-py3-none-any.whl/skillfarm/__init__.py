f7c58454adf8c95fe879ad26beef8b0e11003886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:06:13 +0000

    Add package aa-skillfarm/0.5.4/aa_skillfarm-0.5.4-py3-none-any.whl

---
+++ packages/aa-skillfarm/aa_skillfarm-0.5.4-py3-none-any.whl/skillfarm/__init__.py
@@ -0,0 +1,9 @@
+"""Initialize the app"""
+
+# AA Skillfarm
+from skillfarm.app_settings import SKILLFARM_APP_NAME
+
+__version__ = "0.5.4"
+__title__ = "Skillfarm"
+
+USER_AGENT_TEXT = f"{SKILLFARM_APP_NAME} v{__version__}"
