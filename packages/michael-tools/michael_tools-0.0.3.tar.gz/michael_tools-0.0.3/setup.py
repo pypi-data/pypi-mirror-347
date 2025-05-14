b5e32328a604cd272fe8604e2cdb2441ee9f556e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:06:11 +0000

    Add package michael-tools/0.0.3/michael_tools-0.0.3.tar.gz

---
+++ packages/michael-tools/michael_tools-0.0.3.tar.gz/michael_tools-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='michael_tools',
+    version='0.0.3',
+    author='Michael Smith',
+    author_email='1422749310@qq.com',
+    description='This is my personal toolkit',
+    packages=find_packages(),  # 自动发现所有模块
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.8',
+    install_requires=[  # 依赖项（可选）
+        # 'requests',
+    ],
+)
