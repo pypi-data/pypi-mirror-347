737ec23a719d3d6024e9b49eb3917d5f77fbcaf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:28 +0000

    Add package renardo-reapy/0.11.0.dev4/renardo_reapy-0.11.0.dev4-py3-none-any.whl

---
+++ packages/renardo-reapy/renardo_reapy-0.11.0.dev4-py3-none-any.whl/renardo_reapy/core/window/window.pyi
@@ -0,0 +1,19 @@
+import renardo_reapy
+from renardo_reapy.core import ReapyObject
+import renardo_reapy.reascript_api as RPR
+import typing as ty
+
+
+class Window(ReapyObject):
+    id: bytes
+
+    def __init__(self, id: bytes) -> None:
+        ...
+
+    @property
+    def _args(self) -> ty.Tuple[bytes]:
+        ...
+
+    def refresh(self) -> None:
+        """Refresh window."""
+        ...
