0cb9d3fe32cff539bfcac60c95f09b8b2c92ca9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:25 +0000

    Add package qa-pytest-examples/0.0.7/qa_pytest_examples-0.0.7.tar.gz

---
+++ packages/qa-pytest-examples/qa_pytest_examples-0.0.7.tar.gz/qa_pytest_examples-0.0.7/tests/swagger_petstore_tests.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2025 Adrian Herscu
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from hamcrest import is_  # type: ignore
+from qa_pytest_examples.swagger_petstore_configuration import SwaggerPetstoreConfiguration
+from qa_pytest_examples.swagger_petstore_steps import SwaggerPetstoreSteps
+from qa_pytest_examples.model.swagger_petstore_pet import SwaggerPetstorePet
+from qa_pytest_rest.rest_tests import RestTests
+from qa_testing_utils.matchers import traced, yields_item
+
+
+class SwaggerPetstoreTests(
+        RestTests[SwaggerPetstoreSteps, SwaggerPetstoreConfiguration]):
+    _steps_type = SwaggerPetstoreSteps
+    _configuration = SwaggerPetstoreConfiguration()
+
+    def should_add(self):
+        random_pet = SwaggerPetstorePet.random()
+        (self.steps
+            .given.configuration(self._configuration)
+            .and_.swagger_petstore(self._rest_session)
+            .when.adding(random_pet)
+            .then.the_available_pets(yields_item(traced(is_(random_pet)))))
