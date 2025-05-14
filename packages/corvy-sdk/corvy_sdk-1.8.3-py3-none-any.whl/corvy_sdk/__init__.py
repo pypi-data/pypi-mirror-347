28ecbb0eaa56676c5964ef4b930ec22ea880d30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:06:29 +0000

    Add package corvy-sdk/1.8.3/corvy_sdk-1.8.3-py3-none-any.whl

---
+++ packages/corvy-sdk/corvy_sdk-1.8.3-py3-none-any.whl/corvy_sdk/__init__.py
@@ -0,0 +1,6 @@
+from .messages import Message, MessageUser
+from .command_parsing import Greedy
+from .corvybot import CorvyBot
+
+__version__ = "1.8.3"
+__all__ = ["Greedy", "MessageUser", "Message", "CorvyBot"]
