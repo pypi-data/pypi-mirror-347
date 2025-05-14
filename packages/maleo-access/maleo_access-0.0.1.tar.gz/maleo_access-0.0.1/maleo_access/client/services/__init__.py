dca088b103988ac92017f28976260c96484f7327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:06:00 +0000

    Add package maleo-access/0.0.1/maleo_access-0.0.1.tar.gz

---
+++ packages/maleo-access/maleo_access-0.0.1.tar.gz/maleo_access-0.0.1/maleo_access/client/services/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from pydantic import Field
+from maleo_foundation.managers.client.base import ClientServices
+from maleo_access.client.services.authentication import MaleoAccessAuthenticationClientService
+
+class MaleoAccessServices(ClientServices):
+    authentication:MaleoAccessAuthenticationClientService = Field(..., description="Authentication's service")
