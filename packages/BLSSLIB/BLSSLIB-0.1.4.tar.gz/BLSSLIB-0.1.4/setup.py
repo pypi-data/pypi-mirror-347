80436d471b2cf3a4c38ba97a26a17e969cadaa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:12 +0000

    Add package BLSSLIB/0.1.4/BLSSLIB-0.1.4.tar.gz

---
+++ packages/BLSSLIB/BLSSLIB-0.1.4.tar.gz/BLSSLIB-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BLSSLIB",
+    version="0.1.4",
+    author="Back",
+    description="A Python library for encoding and decoding Build logic save data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="GPL-3.0-or-later",
+    license_files=("LICENSE"),
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
