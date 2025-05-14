1957ae59a804298341cb5fb993e2b12245985dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:07:03 +0000

    Add package asflow/0.0.1/asflow-0.0.1-py3-none-any.whl

---
+++ packages/asflow/asflow-0.0.1-py3-none-any.whl/asflow/serializer.py
@@ -0,0 +1,42 @@
+import json
+
+
+class Serializer:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+
+class TextSerializer(Serializer):
+    def encode(self, data: str) -> bytes:
+        return data.encode()
+
+
+class JsonSerializer(Serializer):
+    def encode(self, data) -> bytes:
+        return json.dumps(data).encode()
+
+
+class JsonlSerializer(Serializer):
+    def encode(self, data) -> bytes:
+        return json.dumps(data).encode() + b"\n"
+
+
+class ParquetSerializer(Serializer):
+    def encode(self, data) -> bytes:
+        return json.dumps(data).encode() + b"\n"
+
+
+def create_serializer(path, serializer):
+    if isinstance(serializer, Serializer):
+        return serializer
+
+    serializer = serializer or {}
+    suffixes = path.suffixes
+    if ".json" in suffixes:
+        return JsonSerializer(**serializer)
+    elif ".jsonl" in suffixes:
+        return JsonlSerializer(**serializer)
+    elif ".parquet" in suffixes:
+        return ParquetSerializer(**serializer)
+    else:
+        return TextSerializer()
