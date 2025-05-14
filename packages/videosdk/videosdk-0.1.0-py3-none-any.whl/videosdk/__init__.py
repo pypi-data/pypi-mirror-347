fa7c42fae09e7873b97ca58f098c60d963d4c273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:05:33 +0000

    Add package videosdk/0.1.0/videosdk-0.1.0-py3-none-any.whl

---
+++ packages/videosdk/videosdk-0.1.0-py3-none-any.whl/videosdk/__init__.py
@@ -0,0 +1,35 @@
+import logging
+from .exceptions import (
+    SDKError,
+    InternalServerError,
+    InvalidMeetingConfigError,
+    MeetingError,
+    MeetingInitializationError,
+)
+from .event_handler import MeetingEventHandler, ParticipantEventHandler
+from .participant import Participant
+from .videosdk import MeetingConfig, VideoSDK
+from .stream import Stream
+from .meeting import Meeting
+from .custom_audio_track import CustomAudioTrack
+from .custom_video_track import CustomVideoTrack
+from .utils import (
+    RecordingConfig,
+    TranscriptionConfig,
+    PostTranscriptionConfig,
+    SummaryConfig,
+    PubSubPublishConfig,
+    PubSubSubscribeConfig,
+)
+
+from .worker import (
+    CharacterWorker,
+    CharacterState,
+    CharacterContext,
+    CharacterResponseStatus,
+    state,
+)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__version__ = "0.1.0"
