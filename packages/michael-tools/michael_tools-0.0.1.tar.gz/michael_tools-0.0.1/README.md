d6495207ff694ab13c7c78e04ec671be069457b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:06:01 +0000

    Add package michael-tools/0.0.1/michael_tools-0.0.1.tar.gz

---
+++ packages/michael-tools/michael_tools-0.0.1.tar.gz/michael_tools-0.0.1/README.md
@@ -0,0 +1,3 @@
+This is my personal toolkit
+
+`0.0.1` 第一版本
---
+++ packages/michael-tools/michael_tools-0.0.1.tar.gz/michael_tools-0.0.1/michael_tools/__init__.py
@@ -0,0 +1,6 @@
+from michael_tools.file_op.create_file import create_file
+from michael_tools.file_op.delete_file import delete_file
+from michael_tools.file_op.read_file import read_file
+from michael_tools.file_op.unique_filepath import get_unique_filepath
+from michael_tools.file_op.write_file import write_to_file, append_to_file
+
