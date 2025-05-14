546c7671121a47611a843014a4e2e5b1f7315b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:51 +0000

    Add package formatted-console-output/0.3.0/formatted_console_output-0.3.0.tar.gz

---
+++ packages/formatted-console-output/formatted_console_output-0.3.0.tar.gz/formatted_console_output-0.3.0/formatted_console_output/__init__.py
@@ -0,0 +1,19 @@
+from .formatted_console_output import (
+    ForegroundColor,
+    BackgroundColor,
+    TextFormat,
+    FormattedPhrase,
+    CustomColor,
+    output_formatted_message,
+    output_many_format_message
+)
+
+__all__ = [
+    "ForegroundColor",
+    "BackgroundColor",
+    "TextFormat",
+    "FormattedPhrase",
+    "CustomColor",
+    "output_formatted_message",
+    "output_many_format_message"
+]
---
+++ packages/formatted-console-output/formatted_console_output-0.3.0.tar.gz/formatted_console_output-0.3.0/formatted_console_output/test.py
@@ -0,0 +1,15 @@
+from formatted_console_output import (
+    ForegroundColor,
+    BackgroundColor,
+    TextFormat,
+    FormattedPhrase,
+    CustomColor,
+    output_formatted_message as print,
+    output_many_format_message as printf,
+)
+
+print(
+    "The quick brown foo jumped over the lazy bar",
+    fg_color=ForegroundColor.YELLOW,
+    bg_color=CustomColor(r=156, g=101, b=0, is_fg=False),
+)
