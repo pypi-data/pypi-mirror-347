3feb5c8e9cc06e49373beef226665606c5211428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 17:06:43 +0000

    Add package web3evm/0.1.1/web3evm-0.1.1.tar.gz

---
+++ packages/web3evm/web3evm-0.1.1.tar.gz/web3evm-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="web3evm",
+    version="0.1.1",
+    author="bryains",
+    author_email="",
+    description="evm utilities",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="", 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
