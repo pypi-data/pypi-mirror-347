4e469d9f2a7b36b4b1b85c0561e7c397ed9596d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:14 +0000

    Add package euriai/0.3.7/euriai-0.3.7.tar.gz

---
+++ packages/euriai/euriai-0.3.7.tar.gz/euriai-0.3.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="euriai",
+    version="0.3.7",
+    description="Python client for EURI LLM API (euron.one) with CLI and interactive wizard",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="euron.one",
+    author_email="sudhanshu@euron.one",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "langchain-core"
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "euriai=euriai.cli:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+    ],
+    license="MIT",
+)
