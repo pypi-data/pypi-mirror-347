5fa7de388c219facee3b5a147f63e4ec2e40215b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:06:14 +0000

    Add package kasr/0.1/kasr-0.1.tar.gz

---
+++ packages/kasr/kasr-0.1.tar.gz/kasr-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kasr',
+    version='0.1',
+    packages=find_packages(),
+    description='کتابخانه ساده عملیات کسر',
+    author='sama',
+    author_email='bagherisama78@gmail.com',
+)
