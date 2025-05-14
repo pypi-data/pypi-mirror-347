36d02f5af4579802d476f5795b5094fdc7bd9fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:05 +0000

    Add package VDP/0.1.0/VDP-0.1.0-py3-none-any.whl

---
+++ packages/VDP/VDP-0.1.0-py3-none-any.whl/VDP/__init__.py
@@ -0,0 +1,12 @@
+from layers import (
+    VDP_first_Conv,
+    VDP_intermediate_Conv,
+    VDP_MaxPooling,
+    VDP_Flatten_and_FC,
+    mysoftmax,
+    VDPReLU,
+    VDPELU,
+    VDPDropout,
+    VDPBatch_Normalization,
+    Density_prop_CNN
+)
