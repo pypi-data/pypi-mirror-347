c4066ecb328a371297c1c86a5f8323ee9656e304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:15 +0000

    Add package abovo/0.1.2/abovo-0.1.2.tar.gz

---
+++ packages/abovo/abovo-0.1.2.tar.gz/abovo-0.1.2/src/nn/Loss.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+
+#include "nn/Loss.hpp"
+#include "nn/loss/MSE.hpp"
+#include "nn/loss/CrossEntropy.hpp"
+
+namespace nn {
+
+double Loss::loss(const Matrix& y_pred, const Matrix& y_true, LossType type) const {
+    switch (type) {
+        case LossType::CROSS_ENTROPY:
+            return loss::cross_entropy(y_pred, y_true);
+        default:
+            return loss::mse(y_pred, y_true);
+    }
+}
+
+Matrix Loss::loss_derivative(const Matrix& y_pred, const Matrix& y_true, LossType type) const {
+    switch (type) {
+        case LossType::CROSS_ENTROPY:
+            return loss::cross_entropy_derivative(y_pred, y_true);
+        default:
+            return loss::mse_derivative(y_pred, y_true);
+    }
+}
+
+}
