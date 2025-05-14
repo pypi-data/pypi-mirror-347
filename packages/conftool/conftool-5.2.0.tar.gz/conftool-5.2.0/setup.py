84cc7d8397ac73d28a817964a8a74ea2ffbd345e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 16:06:04 +0000

    Add package conftool/5.2.0/conftool-5.2.0.tar.gz

---
+++ packages/conftool/conftool-5.2.0.tar.gz/conftool-5.2.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="conftool",
+    version="5.2.0",
+    description="Tools to interoperate with distributed k/v stores",
+    author="Joe",
+    author_email="joe@wikimedia.org",
+    url="https://github.com/wikimedia/operations-software-conftool",
+    install_requires=[
+        "python-etcd>=0.4.3",
+        "pyyaml",
+        "jsonschema",
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "conftool-sync = conftool.cli.syncer:main",
+            "confctl = conftool.cli.tool:main",
+            "dbctl = conftool.extensions.dbconfig:main [with-dbctl]",
+            "requestctl = conftool.extensions.reqconfig:main [with-requestctl]",
+            "conftool2git = conftool.cli.conftool2git:main [with-conftool2git]",
+        ],
+    },
+    extras_require={
+        "with-dbctl": [],  # No extra dependencies, but allow to mark it
+        "with-requestctl": ["pyparsing", "tabulate", "wmflib", "sympy"],
+        "with-conftool2git": ["pygit2", "aiohttp"],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: System :: Clustering",
+    ],
+)
