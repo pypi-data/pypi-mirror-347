3eeaa553678c9754d23ffae41cfbc8a5640e742e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 17:06:18 +0000

    Add package nr-metadata/2.0.73/nr_metadata-2.0.73-py2.py3-none-any.whl

---
+++ packages/nr-metadata/nr_metadata-2.0.73-py2.py3-none-any.whl/nr_metadata/documents/resources/records/resource.py
@@ -0,0 +1,8 @@
+from invenio_drafts_resources.resources import RecordResource
+
+
+class DocumentsResource(RecordResource):
+    """DocumentsRecord resource."""
+
+    # here you can for example redefine
+    # create_url_rules function to add your own rules
