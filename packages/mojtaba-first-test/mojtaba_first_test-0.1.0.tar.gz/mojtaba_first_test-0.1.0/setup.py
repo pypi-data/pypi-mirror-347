b19af32f5d6a1905aa6c0eae48715aa1d58f5138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 07:06:49 +0000

    Add package mojtaba-first-test/0.1.0/mojtaba_first_test-0.1.0.tar.gz

---
+++ packages/mojtaba-first-test/mojtaba_first_test-0.1.0.tar.gz/mojtaba_first_test-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mojtaba-first-test',
+    version='0.1.0',
+    author='Mojtaba',
+    author_email='your-email@example.com',
+    description='My first test Python package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/mojtaba-first-test',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+) 
