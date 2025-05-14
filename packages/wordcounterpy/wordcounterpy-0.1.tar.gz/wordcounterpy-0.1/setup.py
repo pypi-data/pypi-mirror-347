a9ececfba1ea5ba9deb131b4ee2851fffada8456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:10 +0000

    Add package wordcounterpy/0.1/wordcounterpy-0.1.tar.gz

---
+++ packages/wordcounterpy/wordcounterpy-0.1.tar.gz/wordcounterpy-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wordcounterpy',
+    version='0.1',
+    packages=find_packages(),
+    description='A very simple word counting library',
+    author='elaheh121',
+    author_email='shelahe065@gmail.com',
+    keywords=['text', 'word count', 'simple'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
---
+++ packages/wordcounterpy/wordcounterpy-0.1.tar.gz/wordcounterpy-0.1/wordcounter/__init__.py
@@ -0,0 +1,1 @@
+from .wordcounter.wordcounter import count_words, word_frequencies, most_common_word
