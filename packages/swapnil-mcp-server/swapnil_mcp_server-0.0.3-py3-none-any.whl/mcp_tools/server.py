88955955c4dd7d77d7559543cd0c79e2f66752fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 17:06:17 +0000

    Add package swapnil-mcp-server/0.0.3/swapnil_mcp_server-0.0.3-py3-none-any.whl

---
+++ packages/swapnil-mcp-server/swapnil_mcp_server-0.0.3-py3-none-any.whl/mcp_tools/server.py
@@ -0,0 +1,30 @@
+from mcp.server.fastmcp import FastMCP
+import httpx
+import os 
+from dotenv import load_dotenv 
+mcp = FastMCP("MCP_Tools")
+
+@mcp.tool()
+async def get_Activities(unit : str) -> str:
+    """Get Latest Activities for UNIT
+    Args:
+        unit: Unit Name
+    """
+    load_dotenv()
+    
+    #return f"HTTP Proxy set : {os.environ.get("HTTP_PROXY", "HTTP_PROXY")} , HTTPS Proxy set : {os.environ.get("HTTPS_PROXY", "HTTPS_PROXY")}"
+    proxies = {
+    "http": os.environ.get("HTTP_PROXY"), 
+    "https":os.environ.get("HTTPS_PROXY"),
+    }
+    url = 'https://fakerestapi.azurewebsites.net/api/v1/Activities'
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, proxies=proxies)
+        if response.status_code == 200:
+            data = response.json()
+            return f"Activities For Unit {unit} are : {data})"
+        else:
+            return f"No Activities Info. Error: {response.status_code}"
+
+     
+     
