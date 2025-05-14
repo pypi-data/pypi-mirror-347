87406629fe0ac9a24782776fae2f6ff6a2a54076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:06:01 +0000

    Add package wdphottools/0.0.10/wdphottools-0.0.10.tar.gz

---
+++ packages/wdphottools/wdphottools-0.0.10.tar.gz/wdphottools-0.0.10/test/test_util.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Testing the global spline in 2D"""
+
+import numpy as np
+import pytest
+
+x = np.arange(10)
+y = np.arange(10) ** 2.0
+z = np.arange(10) ** 3.0
+
+pass
