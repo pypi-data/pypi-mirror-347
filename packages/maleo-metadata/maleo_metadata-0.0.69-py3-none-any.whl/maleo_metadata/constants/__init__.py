191ccf7000a5c34df1881e829fd48d4bde02a179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 16:05:57 +0000

    Add package maleo-metadata/0.0.69/maleo_metadata-0.0.69-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.0.69-py3-none-any.whl/maleo_metadata/constants/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from .blood_type import MaleoMetadataBloodTypeConstants
+from .gender import MaleoMetadataGenderConstants
+from .organization_role import MaleoMetadataOrganizationRoleConstants
+from .organization_type import MaleoMetadataOrganizationTypeConstants
+from .service import MaleoMetadataServiceConstants
+from .system_role import MaleoMetadataSystemRoleConstants
+from .user_type import MaleoMetadataUserTypeConstants
+
+class MaleoMetadataConstants:
+    BloodType = MaleoMetadataBloodTypeConstants
+    Gender = MaleoMetadataGenderConstants
+    OrganizationRole = MaleoMetadataOrganizationRoleConstants
+    OrganizationType = MaleoMetadataOrganizationTypeConstants
+    Service = MaleoMetadataServiceConstants
+    SystemRole = MaleoMetadataSystemRoleConstants
+    UserType = MaleoMetadataUserTypeConstants
