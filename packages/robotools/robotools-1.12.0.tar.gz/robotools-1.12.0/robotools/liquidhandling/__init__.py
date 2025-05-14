49c5d45d2420372340912e552d32763010f872c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:27 +0000

    Add package robotools/1.12.0/robotools-1.12.0.tar.gz

---
+++ packages/robotools/robotools-1.12.0.tar.gz/robotools-1.12.0/robotools/liquidhandling/__init__.py
@@ -0,0 +1,6 @@
+from robotools.liquidhandling.exceptions import (
+    VolumeOverflowError,
+    VolumeUnderflowError,
+    VolumeViolationException,
+)
+from robotools.liquidhandling.labware import Labware, Trough
