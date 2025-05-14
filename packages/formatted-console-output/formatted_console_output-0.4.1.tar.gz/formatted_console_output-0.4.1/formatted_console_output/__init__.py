c2d462693ef893525c83bbdac9b4bbe1f5f7131d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 07:05:59 +0000

    Add package formatted-console-output/0.4.1/formatted_console_output-0.4.1.tar.gz

---
+++ packages/formatted-console-output/formatted_console_output-0.4.1.tar.gz/formatted_console_output-0.4.1/formatted_console_output/__init__.py
@@ -0,0 +1,27 @@
+from .formatted_console_output import (
+    ForegroundColor,
+    BackgroundColor,
+    TextFormat,
+    FormattedPhrase,
+    CustomColor,
+    ErrorType,
+    output_formatted_message,
+    output_error_message,
+    output_many_format_message,
+    log_exception_stacktrace,
+    RESET,
+)
+
+__all__ = [
+    "ForegroundColor",
+    "BackgroundColor",
+    "TextFormat",
+    "FormattedPhrase",
+    "CustomColor",
+    "ErrorType",
+    "output_formatted_message",
+    "output_error_message",
+    "output_many_format_message",
+    "log_exception_stacktrace",
+    "RESET",
+]
