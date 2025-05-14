240bb9cdaeebab32361ee87e522219fb87c53bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 05:05:25 +0000

    Add package universalinit/0.1.31/universalinit-0.1.31.tar.gz

---
+++ packages/universalinit/universalinit-0.1.31.tar.gz/universalinit-0.1.31/src/universalinit/templates/vite/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        strictPort: true,
+        cors: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        watch: {
+            usePolling: true
+        }
+    }
+})
