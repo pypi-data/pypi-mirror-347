0a78c0ccefd9978122f9e6e17804f177e302f881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:15 +0000

    Add package corvy-sdk/1.7.0.post1/corvy_sdk-1.7.0.post1-py3-none-any.whl

---
+++ packages/corvy-sdk/corvy_sdk-1.7.0.post1-py3-none-any.whl/corvy_sdk/__init__.py
@@ -0,0 +1,6 @@
+from .messages import Message, MessageUser
+from .command_parsing import Greedy
+from .corvybot import CorvyBot
+
+__version__ = "1.7.0"
+__all__ = ["Greedy", "MessageUser", "Message", "CorvyBot"]
