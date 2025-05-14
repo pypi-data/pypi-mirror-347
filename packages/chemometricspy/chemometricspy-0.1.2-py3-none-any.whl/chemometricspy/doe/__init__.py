71bdd1313c29cc8625c88790ba407d30e06f95e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:15 +0000

    Add package chemometricspy/0.1.2/chemometricspy-0.1.2-py3-none-any.whl

---
+++ packages/chemometricspy/chemometricspy-0.1.2-py3-none-any.whl/chemometricspy/doe/__init__.py
@@ -0,0 +1,5 @@
+from .factorial import Factorial
+from .fractional_factorial import FractionalFactorial
+from .ccd import CentralCompositeDesign
+from .bbd import BoxBehnkenDesign
+from .regression import RegressionAnalysis
