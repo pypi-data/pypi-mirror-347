07f9138dc4236131bb5d08779397fc753de6f40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:14 +0000

    Add package kensho-kfinance/2.1.2/kensho_kfinance-2.1.2.tar.gz

---
+++ packages/kensho-kfinance/kensho_kfinance-2.1.2.tar.gz/kensho_kfinance-2.1.2/kfinance/tool_calling/get_security_id_from_identifier.py
@@ -0,0 +1,16 @@
+from typing import Type
+
+from pydantic import BaseModel
+
+from kfinance.constants import Permission
+from kfinance.tool_calling.shared_models import KfinanceTool, ToolArgsWithIdentifier
+
+
+class GetSecurityIdFromIdentifier(KfinanceTool):
+    name: str = "get_security_id_from_identifier"
+    description: str = "Get the security id associated with an identifier."
+    args_schema: Type[BaseModel] = ToolArgsWithIdentifier
+    required_permission: Permission | None = None
+
+    def _run(self, identifier: str) -> int:
+        return self.kfinance_client.ticker(identifier).security_id
