60212c3eedf991cf590a9b853f89e3c8c11b3c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 16:05:48 +0000

    Add package maleo-token/0.0.1/maleo_token-0.0.1-py3-none-any.whl

---
+++ packages/maleo-token/maleo_token-0.0.1-py3-none-any.whl/maleo_token/models/transfers/parameters.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from maleo_metadata.enums.system_role import MaleoMetadataSystemRoleEnums
+from maleo_token.enums import MaleoTokenEnums
+
+class MaleoTokenParametersTransfers:
+    class Base(BaseModel):
+        system_role:MaleoMetadataSystemRoleEnums.SystemRole = Field(..., description="System role")
+        organization_key:Optional[str] = Field(..., description="Organization's Key")
+        identifier_type:MaleoTokenEnums.IdentifierType = Field(..., description="Identifier's type")
+        identifier_value:str = Field(..., description="Identifier's value")
+        password:str = Field(..., description="Password")
