afa6967788156ca95309bafbfe5176af2226862b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 03:22:04 +0000

    Add package prime-nos/0.2.5/prime_nos-0.2.5.tar.gz

---
+++ packages/prime-nos/prime_nos-0.2.5.tar.gz/prime_nos-0.2.5/README.md
@@ -0,0 +1,14 @@
+# prime_nos
+
+A tiny Python library to check if numbers are prime or coprime.
+
+## Installation
+
+```bash
+pip install prime_nos
+
+```
+has 3 functions:
+ - is_prime(n) - checks if n is prime or not
+ - are_coprime(a,b) - checks if a and b are coprime or not
+ - prime_range(a,b) - returns a list of all prime numbers between a and b
---
+++ packages/prime-nos/prime_nos-0.2.5.tar.gz/prime_nos-0.2.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prime_nos",
+    version="0.2.5",
+    author="Venkatesh",
+    description="A simple library to check for prime and coprime numbers",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.6",
+)
