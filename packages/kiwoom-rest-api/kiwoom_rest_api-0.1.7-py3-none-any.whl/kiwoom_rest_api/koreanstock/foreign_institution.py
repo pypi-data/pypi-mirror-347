33a828e9b1161c7c5d10a3cd5ea0b8f9555849a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:07:22 +0000

    Add package kiwoom-rest-api/0.1.7/kiwoom_rest_api-0.1.7-py3-none-any.whl

---
+++ packages/kiwoom-rest-api/kiwoom_rest_api-0.1.7-py3-none-any.whl/kiwoom_rest_api/koreanstock/foreign_institution.py
@@ -0,0 +1,28 @@
+from kiwoom_rest_api.core.base_api import KiwoomBaseAPI
+from typing import Union, Dict, Any, Awaitable
+
+class ForeignInstitution(KiwoomBaseAPI):
+    """한국 주식 외인 관련 API를 제공하는 클래스"""
+    
+    def __init__(
+        self, 
+        base_url: str = None, 
+        token_manager=None, 
+        use_async: bool = False,
+        resource_url: str = "/api/dostk/frgnistt"
+    ):
+        """
+        ForeignInstitution 클래스 초기화
+        
+        Args:
+            base_url (str, optional): API 기본 URL
+            token_manager: 토큰 관리자 객체
+            use_async (bool): 비동기 클라이언트 사용 여부 (기본값: False)
+        """
+        super().__init__(
+            base_url=base_url,
+            token_manager=token_manager,
+            use_async=use_async,
+            resource_url=resource_url
+        )
+        
