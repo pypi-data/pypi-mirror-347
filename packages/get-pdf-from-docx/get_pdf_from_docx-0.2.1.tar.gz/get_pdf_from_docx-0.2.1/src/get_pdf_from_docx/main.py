9c36e1e076db787fb5f66b19e23b79d9bdd8a9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 16:06:26 +0000

    Add package get-pdf-from-docx/0.2.1/get_pdf_from_docx-0.2.1.tar.gz

---
+++ packages/get-pdf-from-docx/get_pdf_from_docx-0.2.1.tar.gz/get_pdf_from_docx-0.2.1/src/get_pdf_from_docx/main.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+from pypdf import PdfReader, PdfWriter
+from get_pdf_from_docx.helpers.wordtools import generate_pdf
+
+
+def merge_pdf(original_filename, sign_file, no_sign_file):
+    try:
+        writer = PdfWriter()
+
+        reader1 = PdfReader(sign_file)
+        for page in reader1.pages:
+            writer.add_page(page)
+
+        reader2 = PdfReader(no_sign_file)
+        last_page = reader2.pages[-1]
+        writer.add_page(last_page)
+
+        with open(f"{original_filename}.pdf", "wb") as f_out:
+            writer.write(f_out)
+        print(f"COMPLETE: save to {original_filename}.pdf")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert DOCX to PDF")
+    parser.add_argument("-i", "--input_path", required=True, help="Path to DOCX file")
+    args = parser.parse_args()
+    original_filename, sign_file, no_sign_file = generate_pdf(args.input_path)
+    merge_pdf(original_filename, sign_file, no_sign_file)
+
+    os.remove(sign_file)
+    os.remove(no_sign_file)
+
+
+if __name__ == "__main__":
+    main()
