b73092da25fac700b9fd21b03288c629397b5778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 17:06:31 +0000

    Add package maleo-metadata/0.0.72/maleo_metadata-0.0.72-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.0.72-py3-none-any.whl/maleo_metadata/types/general/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from .blood_type import MaleoMetadataBloodTypeGeneralTypes
+from .gender import MaleoMetadataGenderGeneralTypes
+from .organization_role import MaleoMetadataOrganizationRoleGeneralTypes
+from .organization_type import MaleoMetadataOrganizationTypeGeneralTypes
+from .service import MaleoMetadataServiceGeneralTypes
+from .system_role import MaleoMetadataSystemRoleGeneralTypes
+from .user_type import MaleoMetadataUserTypeGeneralTypes
+
+class MaleoMetadataGeneralTypes:
+    BloodType = MaleoMetadataBloodTypeGeneralTypes
+    Gender = MaleoMetadataGenderGeneralTypes
+    OrganizationRole = MaleoMetadataOrganizationRoleGeneralTypes
+    OrganizationType = MaleoMetadataOrganizationTypeGeneralTypes
+    Service = MaleoMetadataServiceGeneralTypes
+    SystemRole = MaleoMetadataSystemRoleGeneralTypes
+    UserType = MaleoMetadataUserTypeGeneralTypes
