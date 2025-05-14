2b68378e2ba74cde66acbabe3017e2e15d1ef241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 07:07:47 +0000

    Add package euriai/0.3.19/euriai-0.3.19-py3-none-any.whl

---
+++ packages/euriai/euriai-0.3.19-py3-none-any.whl/euriai/__init__.py
@@ -0,0 +1,17 @@
+from .client import EuriaiClient
+from .langchain_llm import EuriaiLangChainLLM
+from .embedding import EuriaiEmbeddingClient
+from .langchain_embed import EuriaiEmbeddings
+from .euri_chat import EuriaiLlamaIndexLLM
+from .euri_embed import EuriaiLlamaIndexEmbedding
+
+__all__ = [
+    "EuriaiClient",
+    "EuriaiLangChainLLM",
+    "EuriaiEmbeddingClient",
+    "EuriaiEmbeddings",
+    "EuriaiChatClient",
+    "EuriaiEmbedClient",
+    "EuriaiLlamaIndexLLM",
+    "EuriaiLlamaIndexEmbedding"
+]
