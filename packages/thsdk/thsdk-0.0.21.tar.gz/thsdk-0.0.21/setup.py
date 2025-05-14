a92f8c1f61ef6d4d9a195ab468d4d59e4ed6a593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:06:18 +0000

    Add package thsdk/0.0.21/thsdk-0.0.21.tar.gz

---
+++ packages/thsdk/thsdk-0.0.21.tar.gz/thsdk-0.0.21/setup.py
@@ -0,0 +1,39 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.21'
+DESCRIPTION = 'a python package for thsdk'
+LONG_DESCRIPTION = 'a python package for thsdk'
+
+# Setting up
+setup(
+    name="thsdk",
+    version=VERSION,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(where='.', exclude=(), include=('*',)),
+    include_package_data=True,
+    install_requires=[],
+    keywords=['python', 'thsdk'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_data={
+        'thsdk': ['*.so', '*.h', '*'],
+    },
+)
