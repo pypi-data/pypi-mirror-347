0ff2cd69563283d928d13cea1617f8a72974c2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:06:20 +0000

    Add package econball/0.0.1/econball-0.0.1.tar.gz

---
+++ packages/econball/econball-0.0.1.tar.gz/econball-0.0.1/econball/fred.py
@@ -0,0 +1,37 @@
+"""A puller for FRED data."""
+
+# pylint: disable=global-statement
+import logging
+import os
+
+import pandas as pd
+from fredapi import Fred  # type: ignore
+
+_FRED_CLIENT = None
+
+
+def _get_fred_client() -> Fred:
+    global _FRED_CLIENT
+    if _FRED_CLIENT is None:
+        _FRED_CLIENT = Fred(api_key=os.environ["FRED_API_KEY"])
+    return _FRED_CLIENT
+
+
+def pull(series: str) -> pd.Series:
+    """Pull the FRED economic data."""
+    logging.info("Pulling FRED series %s", series)
+    series_name = "FRED_" + series
+    client = _get_fred_client()
+    try:
+        df = client.get_series_all_releases(series)
+    except ValueError:
+        df = client.get_series(series)
+        return df.rename(series_name)
+    df["date"] = pd.to_datetime(df["date"])
+    df["realtime_start"] = pd.to_datetime(df["realtime_start"])
+
+    def select_latest(group: pd.DataFrame) -> pd.DataFrame:
+        return group[group["realtime_start"] == group["realtime_start"].max()]
+
+    df = df.groupby("date").apply(select_latest)
+    return df.set_index("date")["value"].rename(series_name)
