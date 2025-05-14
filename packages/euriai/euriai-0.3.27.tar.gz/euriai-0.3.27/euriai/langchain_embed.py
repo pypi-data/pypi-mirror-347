c9d464945ca22042958f83048adb047c0901462f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 07:05:18 +0000

    Add package euriai/0.3.27/euriai-0.3.27.tar.gz

---
+++ packages/euriai/euriai-0.3.27.tar.gz/euriai-0.3.27/euriai/langchain_embed.py
@@ -0,0 +1,14 @@
+from langchain_core.embeddings import Embeddings
+from typing import List
+from euriai.embedding import EuriaiEmbeddingClient
+
+
+class EuriaiEmbeddings(Embeddings):
+    def __init__(self, api_key: str, model: str = "text-embedding-3-small"):
+        self.client = EuriaiEmbeddingClient(api_key=api_key, model=model)
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return [embedding.tolist() for embedding in self.client.embed_batch(texts)]
+
+    def embed_query(self, text: str) -> List[float]:
+        return self.client.embed(text).tolist()
