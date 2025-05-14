b6e7445031f2249ed45dc63302111057d73dda77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 06:10:50 +0000

    Add package hyperunison-public-api-sdk/2.3.5/hyperunison_public_api_sdk-2.3.5.tar.gz

---
+++ packages/hyperunison-public-api-sdk/hyperunison_public_api_sdk-2.3.5.tar.gz/hyperunison_public_api_sdk-2.3.5/README.md
@@ -0,0 +1,25 @@
+# Hyperunison Python SDK
+
+You can use this SDK to execute UQL queries via Public API.
+
+## Installation
+If you need to install this SDK, you should add the line
+```shell
+hyperunison-public-api-sdk===2.3.5
+```
+to the file **requirements.txt**.
+
+Or you can run the command
+```shell
+pip install hyperunison-public-api-sdk
+```
+in CLI.
+
+## API keys
+
+You will need to create API key to use the Public API. You can do it in Web interface of the site.
+
+## The example of using
+
+You can see all examples of using of this SDK here:
+<a href="https://github.com/Hyperunison/unison-sdk-python/tree/master/examples">https://github.com/Hyperunison/unison-sdk-python/tree/master/examples</a>.
---
+++ packages/hyperunison-public-api-sdk/hyperunison_public_api_sdk-2.3.5.tar.gz/hyperunison_public_api_sdk-2.3.5/src/hyperunison_public_api/hyperunison_public_api_sdk/models/__init__.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from hyperunison_public_api_sdk.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from hyperunison_public_api_sdk.model.biobank_omop_export_response import BiobankOMOPExportResponse
+from hyperunison_public_api_sdk.model.bulk_update_structure_request import BulkUpdateStructureRequest
+from hyperunison_public_api_sdk.model.data_item_dictionary_with_accuracy_list_response import DataItemDictionaryWithAccuracyListResponse
+from hyperunison_public_api_sdk.model.data_item_dictionary_with_accuracy_response import DataItemDictionaryWithAccuracyResponse
+from hyperunison_public_api_sdk.model.job import Job
+from hyperunison_public_api_sdk.model.multi_run_pipeline import MultiRunPipeline
+from hyperunison_public_api_sdk.model.nextflow_pipeline_output_formatter_result import NextflowPipelineOutputFormatterResult
+from hyperunison_public_api_sdk.model.nextflow_pipeline_output_formatter_result_item_status import NextflowPipelineOutputFormatterResultItemStatus
+from hyperunison_public_api_sdk.model.public_cohort_execute_query_request import PublicCohortExecuteQueryRequest
+from hyperunison_public_api_sdk.model.response_to_ucdm_result_with_sql import ResponseToUCDMResultWithSql
+from hyperunison_public_api_sdk.model.run_custom_workflow_request import RunCustomWorkflowRequest
+from hyperunison_public_api_sdk.model.run_pipeline import RunPipeline
+from hyperunison_public_api_sdk.model.runner_agent import RunnerAgent
