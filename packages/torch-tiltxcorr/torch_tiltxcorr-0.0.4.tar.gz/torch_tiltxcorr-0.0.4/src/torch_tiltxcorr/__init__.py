e8d4a352b5099efec9963cef63a9e7f92e2c2c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:37 +0000

    Add package torch-tiltxcorr/0.0.4/torch_tiltxcorr-0.0.4.tar.gz

---
+++ packages/torch-tiltxcorr/torch_tiltxcorr-0.0.4.tar.gz/torch_tiltxcorr-0.0.4/src/torch_tiltxcorr/__init__.py
@@ -0,0 +1,14 @@
+"""Cross correlation with cosine stretching for cryo-EM data in PyTorch"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("torch-tiltxcorr")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+__author__ = "Alister Burt"
+__email__ = "alisterburt@gmail.com"
+
+from torch_tiltxcorr.tiltxcorr import tiltxcorr
+
+__all__ = ['tiltxcorr']
