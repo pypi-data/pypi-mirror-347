b73891990d53a52dd821b1ec811784b4cd977d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:26 +0000

    Add package ros2em/0.1.1/ros2em-0.1.1-py3-none-any.whl

---
+++ packages/ros2em/ros2em-0.1.1-py3-none-any.whl/ros2em/utils/port_utils.py
@@ -0,0 +1,12 @@
+import socket
+
+def find_free_port(start_port = 6000, end_port = 7000):
+    for port in range(start_port, end_port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(("localhost", port))
+                return port
+            except OSError:
+                continue
+    
+    raise RuntimeError("No free ports in range.")
