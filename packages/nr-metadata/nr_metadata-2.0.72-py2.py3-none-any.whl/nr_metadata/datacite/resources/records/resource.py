08ffade14159dba53f045fa42b93b57f35f9ade8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 16:05:51 +0000

    Add package nr-metadata/2.0.72/nr_metadata-2.0.72-py2.py3-none-any.whl

---
+++ packages/nr-metadata/nr_metadata-2.0.72-py2.py3-none-any.whl/nr_metadata/datacite/resources/records/resource.py
@@ -0,0 +1,8 @@
+from invenio_drafts_resources.resources import RecordResource
+
+
+class DataciteResource(RecordResource):
+    """DataciteRecord resource."""
+
+    # here you can for example redefine
+    # create_url_rules function to add your own rules
