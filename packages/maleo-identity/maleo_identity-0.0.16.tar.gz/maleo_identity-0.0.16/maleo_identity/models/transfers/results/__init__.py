5c581da3707c1d09404cc61f2cfd3172bfe3cb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:05:40 +0000

    Add package maleo-identity/0.0.16/maleo_identity-0.0.16.tar.gz

---
+++ packages/maleo-identity/maleo_identity-0.0.16.tar.gz/maleo_identity-0.0.16/maleo_identity/models/transfers/results/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from .general import MaleoIdentityGeneralResultsTransfers
+from .query import MaleoIdentityQueryResultsTransfers
+
+class MaleoIdentityResultsTransfers:
+    General = MaleoIdentityGeneralResultsTransfers
+    Query = MaleoIdentityQueryResultsTransfers
