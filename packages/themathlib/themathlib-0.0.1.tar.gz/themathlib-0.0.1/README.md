c92b1d5b4d4c91f98480d7486ed18878cf7c26fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:06:11 +0000

    Add package themathlib/0.0.1/themathlib-0.0.1.tar.gz

---
+++ packages/themathlib/themathlib-0.0.1.tar.gz/themathlib-0.0.1/README.md
@@ -0,0 +1,3 @@
+ # The Math Lib
+
+- This is a basic package with simple geometric operations and basic math operations
---
+++ packages/themathlib/themathlib-0.0.1.tar.gz/themathlib-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="themathlib",
+    author="Mason Mapfundematsva",
+    author_email="mason.mapfundematsva-freelancer@digitalcareerinstitute.org",
+    version="0.0.1",
+    description="A simple package with basic math operations and example geometric operations",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
