3636a3aef57fb64ebe44cb7db0e8dc362487f2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 08:05:14 +0000

    Add package CellForge/1.1.5/cellforge-1.1.5.tar.gz

---
+++ packages/CellForge/cellforge-1.1.5.tar.gz/cellforge-1.1.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CellForge",
+    version="1.1.05",
+    packages=find_packages(),
+    install_requires=[
+        "pygame"
+    ]
+)
