d0209d1e488ddf7d93105ba222c39b8e1e01e084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 05:06:17 +0000

    Add package cohesity-management-sdk/1.12.4/cohesity_management_sdk-1.12.4.tar.gz

---
+++ packages/cohesity-management-sdk/cohesity_management_sdk-1.12.4.tar.gz/cohesity_management_sdk-1.12.4/cohesity_management_sdk/models_v2/azure_object_type_enum.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+class AzureObjectTypeEnum(object):
+
+    """Implementation of the 'azureObjectType' enum.
+
+    TODO: type enum description here.
+
+    Attributes:
+        KSUBSCRIPTION: TODO: type description here.
+        KRESOURCEGROUP: TODO: type description here.
+        KVIRTUALMACHINE: TODO: type description here.
+        KSTORAGEACCOUNT: TODO: type description here.
+        KSTORAGEKEY: TODO: type description here.
+        KSTORAGECONTAINER: TODO: type description here.
+        KSTORAGEBLOB: TODO: type description here.
+        KNETWORKSECURITYGROUP: TODO: type description here.
+        KVIRTUALNETWORK: TODO: type description here.
+        KSUBNET: TODO: type description here.
+        KCOMPUTEOPTIONS: TODO: type description here.
+        KSNAPSHOTMANAGERPERMIT: TODO: type description here.
+        KTAG: TODO: type description here.
+        KAVAILABILITYSET: TODO: type description here.
+
+    """
+
+    KSUBSCRIPTION = 'kSubscription'
+
+    KRESOURCEGROUP = 'kResourceGroup'
+
+    KVIRTUALMACHINE = 'kVirtualMachine'
+
+    KSTORAGEACCOUNT = 'kStorageAccount'
+
+    KSTORAGEKEY = 'kStorageKey'
+
+    KSTORAGECONTAINER = 'kStorageContainer'
+
+    KSTORAGEBLOB = 'kStorageBlob'
+
+    KNETWORKSECURITYGROUP = 'kNetworkSecurityGroup'
+
+    KVIRTUALNETWORK = 'kVirtualNetwork'
+
+    KSUBNET = 'kSubnet'
+
+    KCOMPUTEOPTIONS = 'kComputeOptions'
+
+    KSNAPSHOTMANAGERPERMIT = 'kSnapshotManagerPermit'
+
+    KTAG = 'kTag'
+
+    KAVAILABILITYSET = 'kAvailabilitySet'
