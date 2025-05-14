066c44c37948e39ca016f179715881cc8a75a1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:17 +0000

    Add package you-are-gay/0.1.0/you_are_gay-0.1.0-py3-none-any.whl

---
+++ packages/you-are-gay/you_are_gay-0.1.0-py3-none-any.whl/fancy_text/__init__.py
@@ -0,0 +1,8 @@
+"""
+Fancy Text - A fun Python package that displays text in a colorful, animated way
+"""
+
+from .main import display_text, animate_text
+
+__version__ = '0.1.0'
+__all__ = ['display_text', 'animate_text'] 
