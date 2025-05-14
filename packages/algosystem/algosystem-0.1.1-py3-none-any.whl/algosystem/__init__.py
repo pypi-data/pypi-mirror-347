c13eb8b34488bc3de92831eb13923b668bf6099c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:06:45 +0000

    Add package algosystem/0.1.1/algosystem-0.1.1-py3-none-any.whl

---
+++ packages/algosystem/algosystem-0.1.1-py3-none-any.whl/algosystem/__init__.py
@@ -0,0 +1,8 @@
+"""AlgoSystem: A batteries-included pythonic library for AlgoGators members."""
+
+__version__ = "0.1.0"
+
+from . import backtesting
+from . import data
+from . import analysis
+from . import utils
