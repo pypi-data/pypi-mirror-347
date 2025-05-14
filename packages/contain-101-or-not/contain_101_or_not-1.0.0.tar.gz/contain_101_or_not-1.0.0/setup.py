a295d6615afa6be6fc4389f92a565bdf2cd2756e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:05:10 +0000

    Add package contain-101-or-not/1.0.0/contain_101_or_not-1.0.0.tar.gz

---
+++ packages/contain-101-or-not/contain_101_or_not-1.0.0.tar.gz/contain_101_or_not-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='contain_101_or_not',
+    version='1.0.0',
+    author='Your Name',
+    description='A simple DFA string acceptance checker',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
