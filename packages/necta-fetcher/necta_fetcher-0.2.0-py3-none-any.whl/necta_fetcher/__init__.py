b5185c7884a730ce24564087a668da5820edc0a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 05:06:04 +0000

    Add package necta-fetcher/0.2.0/necta_fetcher-0.2.0-py3-none-any.whl

---
+++ packages/necta-fetcher/necta_fetcher-0.2.0-py3-none-any.whl/necta_fetcher/__init__.py
@@ -0,0 +1,23 @@
+# necta_fetcher/__init__.py
+from .client import NectaClient
+from .exceptions import (
+    NectaError,
+    NectaLoginError,
+    NectaTokenError,
+    NectaRequestError,
+    NectaResultError,
+    NectaStudentNotFoundError
+)
+
+__version__ = "0.2.0" # Initial version
+
+__all__ = [
+    "NectaClient",
+    "NectaError",
+    "NectaLoginError",
+    "NectaTokenError",
+    "NectaRequestError",
+    "NectaResultError",
+    "NectaStudentNotFoundError",
+   
+]
