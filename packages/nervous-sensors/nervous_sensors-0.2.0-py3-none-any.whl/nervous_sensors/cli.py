bc0aa95985e671002983b3010adffde0edbdf1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 04:05:51 +0000

    Add package nervous-sensors/0.2.0/nervous_sensors-0.2.0-py3-none-any.whl

---
+++ packages/nervous-sensors/nervous_sensors-0.2.0-py3-none-any.whl/nervous_sensors/cli.py
@@ -0,0 +1,62 @@
+import asyncio
+import os
+import sys
+
+import click
+
+from . import cli_utils
+from .cli_listener import CLIListener
+from .cli_utils import extract_sensors, print_bold_section, print_general_info, print_stop_info
+from .connection_manager import ConnectionManager
+
+
+@click.command()
+@click.option("-s", "--sensors", help=cli_utils.SENSORS_HELP)
+@click.option("-g", "--gui", is_flag=True, help=cli_utils.GUI_HELP)
+@click.option("-f", "--folder", type=click.Path(), help=cli_utils.FOLDER_HELP)
+@click.option("-l", "--lsl", is_flag=True, help=cli_utils.LSL_HELP)
+@click.option("-p", "--parallel", default=1, help=cli_utils.PARALLEL_HELP)
+def cli(sensors, gui, folder, lsl, parallel):
+    sys.stdout = CLIListener(sys.stdout)
+    sys.stderr = CLIListener(sys.stderr)
+
+    print_bold_section("Nervous CLI")
+
+    if sensors:
+        sensors = sensors.split(",")
+        true_sensors = extract_sensors(sensors)
+
+        if not true_sensors:
+            print_stop_info("No sensors follows -s/--sensors option, use synthax ECGxxx,EDAxxx...")
+            sys.exit(0)
+        else:
+            print_general_info(f"- Sensors used: {true_sensors}")
+    else:
+        print_stop_info("Option -s/--sensors is required.")
+        sys.exit(0)
+
+    if gui:
+        print_general_info("- GUI enabled")
+    if folder:
+        print_general_info(f"- Folder saving enabled in: '{folder}'")
+    if lsl:
+        print_general_info("- LSL enabled")
+    if parallel:
+        print_general_info(f"- Parallel connection authorized: {parallel}")
+
+    manager = ConnectionManager(true_sensors, gui, folder, lsl, parallel)
+
+    try:
+        asyncio.run(run_app(manager))
+    except (KeyboardInterrupt, OSError):
+        print_stop_info("Shutting down Nervous framework")
+        os._exit(os.EX_OK)
+
+
+async def run_app(manager: ConnectionManager):
+    try:
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(manager.start())
+    except Exception as Argument:
+        print("Connection manager :", str(Argument))
+        await manager.stop()
---
+++ packages/nervous-sensors/nervous_sensors-0.2.0-py3-none-any.whl/nervous_sensors/nervous_eda.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+from . import pb2
+from .codec import Codec
+from .data_manager import DataManager
+from .nervous_sensor import NervousSensor
+
+EDA_SAMPLING_RATE = 8
+
+
+class NervousEDA(NervousSensor):
+    def __init__(self, name, start_time, timeout, connection_manager):
+        super().__init__(name=name, start_time=start_time, timeout=timeout, connection_manager=connection_manager)
+        # override
+        self._data_manager = EDADataManager(sensor_name=name, sampling_rate=EDA_SAMPLING_RATE, start_time=start_time)
+        self._labels = ["GSR"]
+        self._units = ["uS"]
+
+    # override Nervous Sensor class properties
+
+    def get_type(self) -> str:
+        return "GSR"
+
+    def get_sampling_rate(self) -> int:
+        return EDA_SAMPLING_RATE
+
+
+class EDADataManager(DataManager):
+    def __init__(self, sensor_name, sampling_rate, start_time):
+        super().__init__(
+            sensor_name=sensor_name,
+            sampling_rate=sampling_rate,
+            header=["Time (s)", "EDA (uS)"],
+            start_time=start_time,
+            codec=EDACodec(),
+        )
+
+    # implements
+    def _process_decoded_data(self, timestamp, data):
+        data_to_add = [[timestamp, data]]
+        self._add_data(data_to_add)
+
+
+class EDACodec(Codec):
+    # override
+    async def protobuf_decode(self, data):
+        # parse the serialized message from a byte string
+        pb_buffer_msg = pb2.EdaBuffer()
+        pb_buffer_msg.ParseFromString(data)
+        # return self._reshape_data(type, pb_buffer_msg.data, pb_buffer_msg.timestamp)
+        # def _reshape_data(self, type, buffer_msg_data, buffer_msg_timestamp):
+        timestamp = pb_buffer_msg.timestamp.time + pb_buffer_msg.timestamp.us * 0.000001
+        real = pb_buffer_msg.data[0].real
+        imag = pb_buffer_msg.data[0].imag
+        buffer_data = np.sqrt(np.square(real) + np.square(imag))  # get impedance amgnitude of lowest frequency
+        buffer_data = 1000000 * 1 / buffer_data
+        return buffer_data, timestamp
