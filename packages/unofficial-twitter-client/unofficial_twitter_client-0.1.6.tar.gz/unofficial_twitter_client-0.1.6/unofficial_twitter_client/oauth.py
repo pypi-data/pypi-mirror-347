e41385b19678a2b8554e38be2c21e56a5dfd117a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 14:06:44 +0000

    Add package unofficial-twitter-client/0.1.6/unofficial_twitter_client-0.1.6.tar.gz

---
+++ packages/unofficial-twitter-client/unofficial_twitter_client-0.1.6.tar.gz/unofficial_twitter_client-0.1.6/unofficial_twitter_client/oauth.py
@@ -0,0 +1,33 @@
+from requests_oauthlib import OAuth1Session
+
+from unofficial_twitter_client.config import (
+    CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+)
+
+oauth1 = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+def tweet_by_oauth(text, reply_to_id=None):
+    """OAuthを使用してツイートを投稿"""
+    data = {
+        "text": text
+    }
+    if reply_to_id:
+        data["reply"] = {
+            "in_reply_to_tweet_id": reply_to_id
+        }
+    tweet_response = oauth1.post(
+        "https://api.twitter.com/2/tweets",
+        json=data,
+        headers={"Content-Type": "application/json"}
+    )
+    return tweet_response.json()
+
+def retweet(screen_name, id):
+    """リツイート"""
+    text = f"https://x.com/{screen_name}/status/{id}"
+    return tweet_by_oauth(text)
+
+def quote_tweet(text, screen_name, id):
+    """引用リツイート"""
+    text = f"{text} https://x.com/{screen_name}/status/{id}"
+    return tweet_by_oauth(text) 
