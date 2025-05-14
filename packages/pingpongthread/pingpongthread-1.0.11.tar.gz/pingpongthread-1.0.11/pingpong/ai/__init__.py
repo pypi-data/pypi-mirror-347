2e13163763057a6a05757b6de0e5e8f39bbc5f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 17:06:28 +0000

    Add package pingpongthread/1.0.11/pingpongthread-1.0.11.tar.gz

---
+++ packages/pingpongthread/pingpongthread-1.0.11.tar.gz/pingpongthread-1.0.11/pingpong/ai/__init__.py
@@ -0,0 +1,6 @@
+from pingpong.ai.aiderived import *
+from pingpong.ai.aiutils import *
+from pingpong.ai.classification import *
+from pingpong.ai.Cp949ToUniTable import *
+from pingpong.ai.getwebcam import *
+from pingpong.ai.voiceprocess import *
---
+++ packages/pingpongthread/pingpongthread-1.0.11.tar.gz/pingpongthread-1.0.11/pingpong/connection/__init__.py
@@ -0,0 +1,5 @@
+from pingpong.connection.connectionutils import *
+from pingpong.connection.processprotocol import *
+from pingpong.connection.rawprotocol import *
+from pingpong.connection.serialprotocol import *
+from pingpong.connection.utils import *
