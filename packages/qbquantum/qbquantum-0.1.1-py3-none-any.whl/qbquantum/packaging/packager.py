8cc29a803f2be6b4c23d1dba82169ecc761dd86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-14 15:05:58 +0000

    Add package qbquantum/0.1.1/qbquantum-0.1.1-py3-none-any.whl

---
+++ packages/qbquantum/qbquantum-0.1.1-py3-none-any.whl/qbquantum/packaging/packager.py
@@ -0,0 +1,38 @@
+import os
+import pathlib
+
+from ..building import get_build_directory
+from ..managing import pip
+
+
+def package(path: pathlib.Path):
+    build_directory = get_build_directory(path)
+    packaged_directory = build_directory.parent / "packaged"
+
+    args = [
+        "-m",
+        "build",
+        str(build_directory),
+        "--outdir",
+        str(packaged_directory),
+    ]
+
+    if not packaged_directory.exists():
+        packaged_directory.mkdir(parents=True, exist_ok=True)
+
+    os.system(f"python {' '.join(args)}")
+
+
+def install_self(path: pathlib.Path):
+    build_directory = get_build_directory(path)
+    packaged_directory = build_directory.parent / "packaged"
+
+    wheels = []
+
+    for file in packaged_directory.iterdir():
+        if file.suffix == ".whl":
+            wheels.append(str(file))
+
+    venv_path = path / ".venv"
+
+    pip(venv_path, wheels, no_deps=True, force_update=True)
