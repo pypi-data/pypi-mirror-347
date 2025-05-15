4536f72b3e566567223c1dca58665c5b9c99da8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 06:05:23 +0000

    Add package d3dtools/0.12.0/d3dtools-0.12.0-py3-none-any.whl

---
+++ packages/d3dtools/d3dtools-0.12.0-py3-none-any.whl/d3dtools/__init__.py
@@ -0,0 +1,19 @@
+"""
+D3DTOOLS - A collection of tools for working with shapefiles and converting them for Delft3D modeling.
+"""
+
+__version__ = '0.12.0'
+
+from . import ncrain
+from . import snorain
+from . import shpbc2pli
+from . import shpdike2pliz
+from . import shpblock2pol
+from . import shp2pli
+from . import shp2pliz
+from . import shp2pol
+from . import shp2ldb
+from . import shp2xyz
+from . import describe
+from . import evaluate
+from . import sensor
