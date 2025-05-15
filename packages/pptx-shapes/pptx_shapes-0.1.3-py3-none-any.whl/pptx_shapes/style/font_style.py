4653bb92306ba40046337fe3163000d689cb67bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 03:24:11 +0000

    Add package pptx-shapes/0.1.3/pptx_shapes-0.1.3-py3-none-any.whl

---
+++ packages/pptx-shapes/pptx_shapes-0.1.3-py3-none-any.whl/pptx_shapes/style/font_style.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from pptx_shapes.enums import Align, VerticalAlign
+
+
+@dataclass
+class FontStyle:
+    size: float = 14
+    color: str = "#000000"
+    align: Align = Align.CENTER
+    vertical_align: VerticalAlign = VerticalAlign.CENTER
