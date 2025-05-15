f2a7a7b09952e82bdf6f37ce84f41b1cc20134a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 15:06:25 +0000

    Add package indiafactorlibrary/0.0.10/indiafactorlibrary-0.0.10.tar.gz

---
+++ packages/indiafactorlibrary/indiafactorlibrary-0.0.10.tar.gz/indiafactorlibrary-0.0.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="indiafactorlibrary",
+    version="0.0.10",
+    description="A Python library to fetch data from Invespar Factor library for Indian equities.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "requests",
+        "lxml"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6",
+    license='Apache License 2.0',
+)
