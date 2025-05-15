3ba51f4803f8e9854d3521d77ee74d3af9356e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 14:07:01 +0000

    Add package passcode-link-mailer/0.1.5/passcode_link_mailer-0.1.5-py3-none-any.whl

---
+++ packages/passcode-link-mailer/passcode_link_mailer-0.1.5-py3-none-any.whl/simple_mailer/__init__.py
@@ -0,0 +1,16 @@
+"""
+Simple Passcode Mailer
+----------------------
+
+A simple Python library to send email confirmations via Gmail using App Passwords,
+featuring a unique passcode and a timed confirmation link.
+"""
+
+from .passcode_link_mailer import PasscodeLinkMailer, EmailSendingError, EmailSendingAuthError, EmailSendingConnectionError
+
+__version__ = "0.1.5"
+__author__ = "Jonatan Shaya"
+__email__ = "Jonatan.shaya99@gmail.com"
+
+
+__all__ = ['PasscodeLinkMailer', 'EmailSendingError', 'EmailSendingAuthError', 'EmailSendingConnectionError']
