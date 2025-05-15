4ff38f7573324f6b8e51e1ad1c35d362f3abbe3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 14:06:32 +0000

    Add package starri/0.21/starri-0.21.tar.gz

---
+++ packages/starri/starri-0.21.tar.gz/starri-0.21/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='starri',
+    version='0.21',
+    packages=find_packages(),
+    install_requires=[
+        'windows-curses; platform_system=="Windows"',
+    ],
+    entry_points={
+        'console_scripts': [
+            'starri = starri.menu:starri',
+        ],
+    },
+    description="A terminal-based menu system using curses.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Rowan Barker",
+    author_email="barker.rowan@sugarsalem.com",
+    url="https://github.com/lioen-dev/starri",
+)
