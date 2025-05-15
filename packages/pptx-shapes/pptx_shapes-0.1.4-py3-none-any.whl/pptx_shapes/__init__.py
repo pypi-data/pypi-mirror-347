b55335f53c461de6109ef9e1a3622b8234b42944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:07:22 +0000

    Add package pptx-shapes/0.1.4/pptx_shapes-0.1.4-py3-none-any.whl

---
+++ packages/pptx-shapes/pptx_shapes-0.1.4-py3-none-any.whl/pptx_shapes/__init__.py
@@ -0,0 +1,6 @@
+from . import enums
+from . import shapes
+from . import style
+from .presentation import Presentation
+
+__all__ = ["Presentation", "enums", "shapes", "style"]
---
+++ packages/pptx-shapes/pptx_shapes-0.1.4-py3-none-any.whl/pptx_shapes/shapes/__init__.py
@@ -0,0 +1,10 @@
+from .arc import Arc
+from .arrow import Arrow
+from .ellipse import Ellipse
+from .group import Group
+from .line import Line
+from .polygon import Polygon
+from .rectangle import Rectangle
+from .textbox import TextBox
+
+__all__ = ["Arc", "Arrow", "Ellipse", "Group", "Line", "Polygon", "Rectangle", "TextBox"]
