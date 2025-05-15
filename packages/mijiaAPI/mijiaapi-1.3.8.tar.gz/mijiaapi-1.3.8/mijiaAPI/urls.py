ed09419400b4f0e09af9bf9d0ca58a022b11f367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:07:16 +0000

    Add package mijiaAPI/1.3.8/mijiaapi-1.3.8.tar.gz

---
+++ packages/mijiaAPI/mijiaapi-1.3.8.tar.gz/mijiaapi-1.3.8/mijiaAPI/urls.py
@@ -0,0 +1,6 @@
+sid = 'xiaomiio'
+msgURL = f'https://account.xiaomi.com/pass/serviceLogin?sid={sid}&_json=true'
+loginURL = 'https://account.xiaomi.com/pass/serviceLoginAuth2'
+qrURL = 'https://account.xiaomi.com/longPolling/loginUrl'
+apiURL = 'https://api.io.mi.com/app'
+deviceURL = 'https://home.miot-spec.com/spec/'
