886afadde4311deb9edea7d714c713a08a95fda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 14:06:45 +0000

    Add package baldwin/0.0.9/baldwin-0.0.9.tar.gz

---
+++ packages/baldwin/baldwin-0.0.9.tar.gz/baldwin-0.0.9/baldwin/utils.py
@@ -0,0 +1,46 @@
+"""Utilities."""
+from __future__ import annotations
+
+import logging
+import logging.config
+
+log = logging.getLogger(__name__)
+
+
+def setup_logging(*,
+                  debug: bool = False,
+                  force_color: bool = False,
+                  no_color: bool = False) -> None:  # pragma: no cover
+    """Set up logging configuration."""
+    logging.config.dictConfig({
+        'disable_existing_loggers': True,
+        'root': {
+            'level': 'DEBUG' if debug else 'INFO',
+            'handlers': ['console'],
+        },
+        'formatters': {
+            'default': {
+                '()': 'colorlog.ColoredFormatter',
+                'force_color': force_color,
+                'format': (
+                    '%(light_cyan)s%(asctime)s%(reset)s | %(log_color)s%(levelname)-8s%(reset)s | '
+                    '%(light_green)s%(name)s%(reset)s:%(light_red)s%(funcName)s%(reset)s:'
+                    '%(blue)s%(lineno)d%(reset)s - %(message)s'),
+                'no_color': no_color,
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'colorlog.StreamHandler',
+                'formatter': 'default',
+            }
+        },
+        'loggers': {
+            'baldwin': {
+                'level': 'DEBUG' if debug else 'INFO',
+                'handlers': ['console'],
+                'propagate': False,
+            }
+        },
+        'version': 1
+    })
