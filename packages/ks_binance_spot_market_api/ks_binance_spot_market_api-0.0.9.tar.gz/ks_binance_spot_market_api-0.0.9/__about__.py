3f35bdbd5251f29961839355e8b6c4d4c01cc2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 03:23:25 +0000

    Add package ks_binance_spot_market_api/0.0.9/ks_binance_spot_market_api-0.0.9.tar.gz

---
+++ packages/ks_binance_spot_market_api/ks_binance_spot_market_api-0.0.9.tar.gz/ks_binance_spot_market_api-0.0.9/__about__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2024-present liuyanzhi08 <702368372@qq.com>
+#
+# SPDX-License-Identifier: MIT
+# __version__ = "0.0.8"
+__version__ = "0.0.9" # 增加query_hisotry_n的extend_time支持
