6c05025fcf21f24a3403787138f1d67870865c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 05:06:22 +0000

    Add package custom-base-django/0.1.4/custom_base_django-0.1.4-py3-none-any.whl

---
+++ packages/custom-base-django/custom_base_django-0.1.4-py3-none-any.whl/custom_base_django/models/__init__.py
@@ -0,0 +1,4 @@
+from .base import BaseModelFiscalDelete, BaseModelWitDateNotFiscalDelete, CustomRelatedNameManager, \
+    BaseModelNotFiscalDelete, Select2FormChoices, CustomManageFiscalDelete, CustomGenericRelation
+from .choices import  Choice as Choice, ChoiceForeignKey, Choices
+from .workflow import WorkflowState, Workflow, WorkflowHistory
