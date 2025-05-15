3c2d1e29b6a57becc00fceeac2608229252d6765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 05:06:28 +0000

    Add package open-minded/0.1.3/open_minded-0.1.3.tar.gz

---
+++ packages/open-minded/open_minded-0.1.3.tar.gz/open_minded-0.1.3/open_minded/__init__.py
@@ -0,0 +1,53 @@
+from contextlib import asynccontextmanager
+import logging
+from typing import Sequence
+
+from open_minded.models.llm_message import LlmMessage
+from open_minded.providers import get_provider_classes_shuffled
+from open_minded.utils.errors import FailedToFindSuitableProviderError, OpenMindedError
+from open_minded.utils.logging import setup_logging
+
+
+setup_logging()
+_logger = logging.getLogger("open_minded")
+
+
+async def fetch_llm_completion(message_history: Sequence[LlmMessage]):
+    shuffled_providers = get_provider_classes_shuffled()
+
+    for provider_class in shuffled_providers:
+        try:
+            async with provider_class() as provider:
+                return {
+                    "provider": provider,
+                    "result": await provider.fetch_llm_completion(message_history),
+                }
+        except OpenMindedError as error:
+            _logger.warning(
+                f"Failed to fetch completion from {provider_class.name}: {error}.\nTrying other providers..."
+            )
+
+    raise FailedToFindSuitableProviderError()
+
+
+@asynccontextmanager
+async def fetch_llm_completion_and_stream(message_history: Sequence[LlmMessage]):
+    shuffled_providers = get_provider_classes_shuffled()
+
+    for provider_class in shuffled_providers:
+        try:
+            async with (
+                provider_class() as provider,
+                provider.fetch_llm_completion_and_stream(message_history) as response,
+            ):
+                yield {
+                    "provider": provider,
+                    "result": response,
+                }
+                return
+        except Exception as error:
+            _logger.warning(
+                f"Failed to fetch completion from {provider_class}: {error}.\nTrying other providers..."
+            )
+
+    raise FailedToFindSuitableProviderError()
---
+++ packages/open-minded/open_minded-0.1.3.tar.gz/open_minded-0.1.3/open_minded/providers/__init__.py
@@ -0,0 +1,23 @@
+import random
+
+from open_minded.providers.pollinations import PollinationsProvider
+from open_minded.providers.deepai import DeepAiProvider
+
+
+PROVIDER_CLASSES = [DeepAiProvider, PollinationsProvider]
+
+
+def get_provider_classes_shuffled():
+    new_provider_classes = PROVIDER_CLASSES.copy()
+
+    for provider_index in range(len(new_provider_classes)):
+        provider_to_swap_index = random.randint(0, provider_index)
+        (
+            new_provider_classes[provider_index],
+            new_provider_classes[provider_to_swap_index],
+        ) = (
+            new_provider_classes[provider_to_swap_index],
+            new_provider_classes[provider_index],
+        )
+
+    return new_provider_classes
