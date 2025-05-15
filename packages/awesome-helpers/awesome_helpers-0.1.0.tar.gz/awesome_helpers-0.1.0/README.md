c098f262f9bc0ceec0e4eefb8082240a62bb89f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 15:06:02 +0000

    Add package awesome-helpers/0.1.0/awesome_helpers-0.1.0.tar.gz

---
+++ packages/awesome-helpers/awesome_helpers-0.1.0.tar.gz/awesome_helpers-0.1.0/README.md
@@ -0,0 +1,9 @@
+# mypackage
+
+A sample Python package.
+
+## Usage
+
+```python
+from mypackage import hello
+print(hello("Sheetala"))
---
+++ packages/awesome-helpers/awesome_helpers-0.1.0.tar.gz/awesome_helpers-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="awesome_helpers",
+    version="0.1.0",
+    description="A simple hello world package.",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[],
+)
