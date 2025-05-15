83ac77971014a09038148a2bf412a4c1275eca5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:06:57 +0000

    Add package mouse_ran_down/0.0.3/mouse_ran_down-0.0.3-py3-none-any.whl

---
+++ packages/mouse_ran_down/mouse_ran_down-0.0.3-py3-none-any.whl/mouse_ran_down/__init__.py
@@ -0,0 +1,16 @@
+"""Bring media from shared links directly into Telegram chats.
+
+Currently enabled:
+
+- X
+- Reddit
+- Instagram
+- TikTok
+- Bluesky
+- YouTube
+- Vimeo
+- SoundCloud
+- Bandcamp
+"""
+
+__version__ = '0.0.3'
