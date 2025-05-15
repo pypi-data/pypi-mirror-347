4c38c033171c925b012b7494af5f19c9251c7109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 03:23:08 +0000

    Add package div3dfa/0.1.1/div3dfa-0.1.1-py3-none-any.whl

---
+++ packages/div3dfa/div3dfa-0.1.1-py3-none-any.whl/div3dfa/core.py
@@ -0,0 +1,11 @@
+def accepts_binary_string(s: str) -> bool:
+
+    state = 0
+    for char in s:
+        if char not in ('0', '1'):
+            raise ValueError(f"Invalid character '{char}' in input string")
+        if char == '1':
+            state = (state + 1) % 3
+            
+    return state == 0
+ 
