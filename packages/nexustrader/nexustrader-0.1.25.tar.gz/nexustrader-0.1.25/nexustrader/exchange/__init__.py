a0db80ef6cb292dfe3d629119cb9c5d1b81a26a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:06:39 +0000

    Add package nexustrader/0.1.25/nexustrader-0.1.25.tar.gz

---
+++ packages/nexustrader/nexustrader-0.1.25.tar.gz/nexustrader-0.1.25/nexustrader/exchange/__init__.py
@@ -0,0 +1,7 @@
+# from tradebot.exchange.binance import BinanceExchangeManager, BinanceOrderManager, BinanceAccountManager, BinanceWebsocketManager, BinanceWSManager
+# from tradebot.exchange.okx import OkxExchangeManager, OkxOrderManager, OkxAccountManager, OkxWebsocketManager
+
+
+
+# __all__ = ['BinanceExchangeManager', 'BinanceOrderManager', 'BinanceAccountManager', 'BinanceWebsocketManager', 'BinanceWSManager', 
+#            'OkxExchangeManager', 'OkxOrderManager', 'OkxAccountManager', 'OkxWebsocketManager']
