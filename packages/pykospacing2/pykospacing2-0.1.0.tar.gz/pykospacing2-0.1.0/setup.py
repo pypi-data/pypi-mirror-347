86634c94155176b7632a3f7e5ade63e381591bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 15:06:15 +0000

    Add package pykospacing2/0.1.0/pykospacing2-0.1.0.tar.gz

---
+++ packages/pykospacing2/pykospacing2-0.1.0.tar.gz/pykospacing2-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pykospacing2",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A Korean spacing correction library",
+    long_description="A Korean spacing correction library",
+    long_description_content_type="text/plain",
+    url="https://github.com/yourusername/pykospacing2",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
