cf3f6ee146a5981d6d98b39ff66dab4e0bc193ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 03:23:25 +0000

    Add package yta-constants/0.0.18/yta_constants-0.0.18-py3-none-any.whl

---
+++ packages/yta-constants/yta_constants-0.0.18-py3-none-any.whl/yta_constants/text.py
@@ -0,0 +1,29 @@
+from yta_constants.enum import YTAEnum as Enum
+
+
+class TextFinderMode(Enum):
+    """
+    This is the mode in which we will look for the terms
+    in the given segment text to find any coincidences.
+    """
+
+    EXACT = 'exact'
+    """
+    The term found must be exactly matched on the text,
+    which means that accents and punctuation marks will
+    be considered.
+    """
+    IGNORE_CASE_AND_ACCENTS = 'ignore_case_and_accents'
+    """
+    The term found must match, in lower case and ignoring
+    the accents, the text.
+    """
+    IGNORE_CASE = 'ignore_case'
+    """
+    The term found must match, in lower case, the text.
+    """
+    IGNORE_ACCENTS = 'ignore_accents'
+    """
+    The term found must match, ignoring the accents, the
+    text.
+    """
