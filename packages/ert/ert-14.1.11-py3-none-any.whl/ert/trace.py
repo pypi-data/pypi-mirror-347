a0c6adb9efe38d7f01c878c5f4a33da780c5c4b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:07:45 +0000

    Add package ert/14.1.11/ert-14.1.11-py3-none-any.whl

---
+++ packages/ert/ert-14.1.11-py3-none-any.whl/ert/trace.py
@@ -0,0 +1,26 @@
+from opentelemetry import trace
+from opentelemetry.instrumentation.threading import ThreadingInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import SpanLimits, TracerProvider
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+
+ThreadingInstrumentor().instrument()
+
+resource = Resource(attributes={SERVICE_NAME: "ert"})
+tracer_provider = TracerProvider(
+    resource=resource, span_limits=SpanLimits(max_events=128 * 16)
+)
+trace.set_tracer_provider(tracer_provider)
+
+tracer = trace.get_tracer("ert.trace")
+
+
+def get_trace_id() -> str:
+    return trace.format_trace_id(trace.get_current_span().get_span_context().trace_id)
+
+
+def get_traceparent() -> str | None:
+    carrier: dict[str, str] = {}
+    # Write the current context into the carrier.
+    TraceContextTextMapPropagator().inject(carrier)
+    return carrier.get("traceparent")
