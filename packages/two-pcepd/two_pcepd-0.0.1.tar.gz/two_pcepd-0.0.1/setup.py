476216d9154dcf6e9a6944b02bce2ea38c10fbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 05:07:00 +0000

    Add package two-pcepd/0.0.1/two_pcepd-0.0.1.tar.gz

---
+++ packages/two-pcepd/two_pcepd-0.0.1.tar.gz/two_pcepd-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='two_pcepd',
+    version='0.0.1',    
+    description='An implementation of the 2pCePd-Net model',
+    author='Supratim Ghosh, Sourav Pramanik, Mahantapas Kundu, Mita Nasipuri',
+    author_email='supratimghosh2772@gmail.com',
+    license='GNU General Public License v3.0',
+    packages=['two_pcepd'],
+    install_requires=['numpy'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+)
---
+++ packages/two-pcepd/two_pcepd-0.0.1.tar.gz/two_pcepd-0.0.1/two_pcepd/model.py
@@ -0,0 +1,4 @@
+import numpy
+
+def add(a,b):
+    return a+b
