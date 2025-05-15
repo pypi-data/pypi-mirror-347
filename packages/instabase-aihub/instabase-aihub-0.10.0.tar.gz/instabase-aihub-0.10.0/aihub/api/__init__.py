6c72cc0653943968977f2d70a544a751280312b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 07:05:49 +0000

    Add package instabase-aihub/0.10.0/instabase-aihub-0.10.0.tar.gz

---
+++ packages/instabase-aihub/instabase-aihub-0.10.0.tar.gz/instabase-aihub-0.10.0/aihub/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from aihub.api.batches_api import BatchesApi
+from aihub.api.conversations_api import ConversationsApi
+from aihub.api.files_api import FilesApi
+from aihub.api.jobs_api import JobsApi
+from aihub.api.queries_api import QueriesApi
+from aihub.api.runs_api import RunsApi
+from aihub.api.secrets_api import SecretsApi
+
