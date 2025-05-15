61edb580eb74c2b1654a5dbf9e5b6ce442d11c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:06:42 +0000

    Add package dagster-teradata/0.0.3/dagster_teradata-0.0.3.tar.gz

---
+++ packages/dagster-teradata/dagster_teradata-0.0.3.tar.gz/dagster_teradata-0.0.3/dagster_teradata/__init__.py
@@ -0,0 +1,14 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from dagster_teradata.resources import (
+    TeradataDagsterConnection as TeradataDagsterConnection,
+    TeradataResource as TeradataResource,
+    fetch_last_updated_timestamps as fetch_last_updated_timestamps,
+    teradata_resource as teradata_resource,
+)
+
+__version__ = "0.0.3"
+
+DagsterLibraryRegistry.register(
+    "dagster-teradata", __version__, is_dagster_package=False
+)
