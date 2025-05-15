0060782b67b430681ec7bcca2b96dd5c4f757236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 03:23:28 +0000

    Add package geopandas-ai/0.0.17/geopandas_ai-0.0.17-py3-none-any.whl

---
+++ packages/geopandas-ai/geopandas_ai-0.0.17-py3-none-any.whl/geopandasai/cache/backend/file_system.py
@@ -0,0 +1,27 @@
+import os
+from typing import Optional
+
+from .base import CacheBackend
+
+
+class FileSystemCacheBackend(CacheBackend):
+    def __init__(self, cache_dir: str = "./.geopandasai_cache"):
+        self.cache_dir = cache_dir
+        os.makedirs(cache_dir, exist_ok=True)
+
+    def get_cache(self, key: str) -> Optional[bytes]:
+        try:
+            with open(os.path.join(self.cache_dir, key), "rb") as f:
+                return f.read()
+        except FileNotFoundError:
+            return None
+
+    def set_cache(self, key: str, value: bytes) -> None:
+        with open(os.path.join(self.cache_dir, key), "wb") as f:
+            f.write(value)
+
+    def clear_cache(self, key: str) -> None:
+        try:
+            os.remove(os.path.join(self.cache_dir, key))
+        except FileNotFoundError:
+            pass
