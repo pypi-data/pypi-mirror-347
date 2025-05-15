415b9eccf74abb7cacc10d7f60b252bb134ad1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:07:22 +0000

    Add package hsi-wizard/0.1.16/hsi_wizard-0.1.16.tar.gz

---
+++ packages/hsi-wizard/hsi_wizard-0.1.16.tar.gz/hsi_wizard-0.1.16/run_checks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Run pydocstyle
+if pydocstyle wizard; then
+# Run flake8
+  flake8 wizard
+else
+    echo "pydocstyle check failed, skipping flake8."
+    break
+fi
+
+pytest --cov=wizard --cov-report=term-missing
+
+# Deactivate the virtual environment
+deactivate
