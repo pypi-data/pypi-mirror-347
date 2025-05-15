2b0c8655fbf18d693ebf18b7833c8e9ac2d232b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 05:06:30 +0000

    Add package fastdevcommon/0.1.9/fastdevcommon-0.1.9-py3-none-any.whl

---
+++ packages/fastdevcommon/fastdevcommon-0.1.9-py3-none-any.whl/FastDevCommon/base/__init__.py
@@ -0,0 +1,5 @@
+from .base_dto import BaseDTO,PageDTO,Field
+from .base_dao import BaseDAO
+from .base_do import BaseDo,init_database,init_db,sessionmaker
+__all__ = ["BaseDTO","PageDTO","Field","BaseDo","init_database","init_db","sessionmaker"]
+
