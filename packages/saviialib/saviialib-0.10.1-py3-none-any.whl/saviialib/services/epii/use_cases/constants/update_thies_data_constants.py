077cdc8e567fd3531ed1f8fd2daa0ae1462b0e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 14:06:57 +0000

    Add package saviialib/0.10.1/saviialib-0.10.1-py3-none-any.whl

---
+++ packages/saviialib/saviialib-0.10.1-py3-none-any.whl/saviialib/services/epii/use_cases/constants/update_thies_data_constants.py
@@ -0,0 +1,5 @@
+SHAREPOINT_BASE_URL = "/sites/uc365_CentrosyEstacionesRegionalesUC/Shared%20Documents/General/Test_Raspberry/THIES"
+SHAREPOINT_THIES_FOLDERS = ["AVG", "EXT"]
+
+FTP_SERVER_PATH_AVG_FILES = "ftp/thies/BINFILES/ARCH_AV1"
+FTP_SERVER_PATH_EXT_FILES = "ftp/thies/BINFILES/ARCH_EX1"
