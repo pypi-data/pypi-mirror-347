5cffc2706f9dc7d595564ba7bafdb5818aaa8bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:08:21 +0000

    Add package markupy/2.4.0/markupy-2.4.0-py3-none-any.whl

---
+++ packages/markupy/markupy-2.4.0-py3-none-any.whl/markupy/_private/attributes/__init__.py
@@ -0,0 +1,5 @@
+from .attribute import Attribute, AttributeValue
+from .handlers import attribute_handlers
+from .store import AttributeStore
+
+__all__ = ["attribute_handlers", "Attribute", "AttributeValue", "AttributeStore"]
