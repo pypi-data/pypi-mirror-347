066be4f474e57ccdfa71a841fdee6dffe571e6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 06:05:51 +0000

    Add package picsl-greedy/0.0.9/picsl_greedy-0.0.9-cp38-cp38-win_amd64.whl

---
+++ packages/picsl-greedy/picsl_greedy-0.0.9-cp38-cp38-win_amd64.whl/lib/cmake/Greedy/GreedyTargets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "greedyapi" for configuration "Release"
+set_property(TARGET greedyapi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(greedyapi PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/greedyapi.lib"
+  )
+
+list(APPEND _cmake_import_check_targets greedyapi )
+list(APPEND _cmake_import_check_files_for_greedyapi "${_IMPORT_PREFIX}/lib/greedyapi.lib" )
+
+# Import target "multichunkgreedyapi" for configuration "Release"
+set_property(TARGET multichunkgreedyapi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(multichunkgreedyapi PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/multichunkgreedyapi.lib"
+  )
+
+list(APPEND _cmake_import_check_targets multichunkgreedyapi )
+list(APPEND _cmake_import_check_files_for_multichunkgreedyapi "${_IMPORT_PREFIX}/lib/multichunkgreedyapi.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
