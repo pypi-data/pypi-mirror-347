855e7e8226e3d02dc132ab251044c0855da3d1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:07:13 +0000

    Add package fastdevcommon/0.1.8/fastdevcommon-0.1.8-py3-none-any.whl

---
+++ packages/fastdevcommon/fastdevcommon-0.1.8-py3-none-any.whl/FastDevCommon/_base_dto/__init__.py
@@ -0,0 +1,4 @@
+from .base_dto import BaseDTO,PageDTO,Field
+
+__all__ = ["BaseDTO","PageDTO","Field"]
+
---
+++ packages/fastdevcommon/fastdevcommon-0.1.8-py3-none-any.whl/FastDevCommon/base/__init__.py
@@ -0,0 +1,4 @@
+from .base_dto import BaseDTO,PageDTO,Field
+from .base_dao import BaseDAO
+__all__ = ["BaseDTO","PageDTO","Field"]
+
