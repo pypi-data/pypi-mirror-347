26b91757bd2d886e83e8b5f88a6c46dcf3f6406c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 14:06:51 +0000

    Add package sparc-me/3.0.2/sparc_me-3.0.2.tar.gz

---
+++ packages/sparc-me/sparc_me-3.0.2.tar.gz/sparc_me-3.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# ingore pybids because conflicts with digitaltwins gen3 latest version. TODO. check again later
+
+setup(
+    name="sparc_me",
+
+    version="3.0.2",
+
+    description='A python tool to explore, enhance, and expand SPARC datasets and their descriptions in accordance with FAIR principles.',
+    author="Thiranja Prasad Babarenda Gamage, Chinchien Lin, Savindi Wijenayaka, Michael Hoffman, Linkun Gao, Haribalan Kumar",
+    email="psam012@aucklanduni.ac.nz, clin864@aucklanduni.ac.nz",
+    license="Apache-2.0",
+    packages=find_packages(exclude=["pybids"]),
+    include_package_data=True,
+    package_data={'': ['resources./*']},
+    install_requires=[
+        'pandas',
+        'styleframe',
+        'xlrd',
+        'openpyxl',
+        'jsonschema',
+        'requests'
+    ]
+)
