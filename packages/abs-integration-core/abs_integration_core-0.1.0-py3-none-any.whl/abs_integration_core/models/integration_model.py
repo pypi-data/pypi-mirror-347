97487114aee2ef05416e4b32f8335d68a89c77ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:06:10 +0000

    Add package abs-integration-core/0.1.0/abs_integration_core-0.1.0-py3-none-any.whl

---
+++ packages/abs-integration-core/abs_integration_core-0.1.0-py3-none-any.whl/abs_integration_core/models/integration_model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer, Text
+from sqlalchemy.orm import relationship
+
+from abs_repository_core.models import BaseModel
+
+
+class Integration(BaseModel):
+    """Integration model"""
+    __tablename__ = "gov_integrations"
+
+    provider_name = Column(String(255), nullable=False)
+    access_token = Column(Text, nullable=False)
+    refresh_token = Column(Text, nullable=False)
+    expires_in = Column(Integer, nullable=False)
