e4cff245e58cc3547c3d26608f3fe281a76fee2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 07:06:43 +0000

    Add package facetracker/0.2/facetracker-0.2.tar.gz

---
+++ packages/facetracker/facetracker-0.2.tar.gz/facetracker-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="facetracker",
+    version="0.2",
+    description="Real-time face recognition with live state export.",
+    author="Handicate Labs",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "face_recognition",
+        "opencv-python",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'facetracker=facetracker.tracker:start_recognition'
+        ]
+    }
+)
