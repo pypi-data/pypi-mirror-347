40e1beb704654e698f1e51e08dea2e53f4b12e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 05:06:55 +0000

    Add package tol-sdk/1.5.2/tol_sdk-1.5.2.tar.gz

---
+++ packages/tol-sdk/tol_sdk-1.5.2.tar.gz/tol_sdk-1.5.2/src/tol/sources/tolqc.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+import os
+
+from .defaults import Defaults
+from ..api_client import (
+    create_api_datasource
+)
+from ..core import (
+    core_data_object
+)
+
+
+def tolqc(retries: int = 5):
+    tolqc = create_api_datasource(
+        api_url=os.getenv('TOLQC_URL', Defaults.TOLQC_URL)
+        + os.getenv('TOLQC_API_PATH', Defaults.TOLQC_API_PATH),
+        token=os.getenv('TOLQC_API_KEY'),
+        data_prefix=os.getenv('TOLQC_API_DATA_PATH', Defaults.TOLQC_API_DATA_PATH),
+        retries=retries
+    )
+    core_data_object(tolqc)
+    return tolqc
