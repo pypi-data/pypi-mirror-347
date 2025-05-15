258921c6ea3710190125ee12eea13295050415f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:07:47 +0000

    Add package dendromatics/0.6.0b1/dendromatics-0.6.0b1.tar.gz

---
+++ packages/dendromatics/dendromatics-0.6.0b1.tar.gz/dendromatics-0.6.0b1/src/dendromatics/primitives/clustering.py
@@ -0,0 +1,13 @@
+from sklearn.cluster import DBSCAN
+
+
+def DBSCAN_clustering(xyz, eps, min_samples):
+    return DBSCAN(eps=eps, min_samples=min_samples, n_jobs=-1).fit(xyz).labels_
+
+
+# monkey patch clustering if possible
+try:
+    import dendroptimized
+    DBSCAN_clustering = dendroptimized.connected_components
+except ImportError:
+    pass
