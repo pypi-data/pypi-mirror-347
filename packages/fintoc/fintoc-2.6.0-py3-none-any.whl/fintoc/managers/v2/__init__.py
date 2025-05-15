868fa1609b8e8d7f8803e4aa8da82622081173db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 07:06:57 +0000

    Add package fintoc/2.6.0/fintoc-2.6.0-py3-none-any.whl

---
+++ packages/fintoc/fintoc-2.6.0-py3-none-any.whl/fintoc/managers/v2/__init__.py
@@ -0,0 +1,7 @@
+"""Init file for the v2 managers module of the SDK."""
+
+from .account_numbers_manager import AccountNumbersManager
+from .accounts_manager import AccountsManager
+from .entities_manager import EntitiesManager
+from .simulate_manager import SimulateManager
+from .transfers_manager import TransfersManager
