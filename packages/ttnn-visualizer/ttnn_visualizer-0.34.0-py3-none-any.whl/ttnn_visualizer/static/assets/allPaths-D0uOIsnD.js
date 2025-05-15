583bde01d3963c9f4607380b99404271d769ee51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 07:07:43 +0000

    Add package ttnn-visualizer/0.34.0/ttnn_visualizer-0.34.0-py3-none-any.whl

---
+++ packages/ttnn-visualizer/ttnn_visualizer-0.34.0-py3-none-any.whl/ttnn_visualizer/static/assets/allPaths-D0uOIsnD.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-BVMreIQm.js";import{I as e}from"./index-Do7YB6C4.js";import{p as r,I as s}from"./index-CHS7htir.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
---
+++ packages/ttnn-visualizer/ttnn_visualizer-0.34.0-py3-none-any.whl/ttnn_visualizer/static/assets/allPathsLoader-B1iveKGB.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-D0uOIsnD.js","assets/index-BVMreIQm.js","assets/index-Do7YB6C4.js","assets/index-CHS7htir.js","assets/index-CJJgzCye.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-CHS7htir.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-D0uOIsnD.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
