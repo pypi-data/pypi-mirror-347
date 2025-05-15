5da6bcb1ebfcc07acf051f471a933430b346cb41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 14:06:20 +0000

    Add package saviialib/0.10.2/saviialib-0.10.2.tar.gz

---
+++ packages/saviialib/saviialib-0.10.2.tar.gz/saviialib-0.10.2/src/saviialib/services/epii/use_cases/constants/upload_backup_to_sharepoint_constants.py
@@ -0,0 +1,6 @@
+import logging
+
+SHAREPOINT_BASE_URL = "/sites/uc365_CentrosyEstacionesRegionalesUC/Shared%20Documents/General/Test_Raspberry"
+
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger(__package__)
