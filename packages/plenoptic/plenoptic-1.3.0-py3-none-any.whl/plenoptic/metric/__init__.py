3dd7ca58669fc073c194513496028bc52b52c99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 06:05:00 +0000

    Add package plenoptic/1.3.0/plenoptic-1.3.0-py3-none-any.whl

---
+++ packages/plenoptic/plenoptic-1.3.0-py3-none-any.whl/plenoptic/metric/__init__.py
@@ -0,0 +1,12 @@
+"""
+Image quality metrics.
+
+These functions and classes address questions of the form "how different are these
+images?"
+"""
+# ruff: noqa: F401
+
+from .classes import NLP
+from .model_metric import model_metric
+from .naive import mse
+from .perceptual_distance import ms_ssim, nlpd, ssim, ssim_map
