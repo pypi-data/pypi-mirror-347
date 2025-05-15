57c5d9f3fbe9ea80e364c846e2947b01e250a543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:07:57 +0000

    Add package simple-io-kit/1.0.0/simple_io_kit-1.0.0.tar.gz

---
+++ packages/simple-io-kit/simple_io_kit-1.0.0.tar.gz/simple_io_kit-1.0.0/src/simpleio/__init__.py
@@ -0,0 +1,12 @@
+from .simpleio import (
+    read_from_file,
+    read_from_json_file,
+    list_dir,
+    write_to_file,
+    delete_file,
+    append_to_file,
+    append_to_json_list_file,
+    read_lines_from_file,
+    write_to_json_file,
+    make_parent_dirs,
+)
