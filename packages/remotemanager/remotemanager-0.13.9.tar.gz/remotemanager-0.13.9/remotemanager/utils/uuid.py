90d8699d55339acf11dab9d7f73d15a6b706a885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:06:52 +0000

    Add package remotemanager/0.13.9/remotemanager-0.13.9.tar.gz

---
+++ packages/remotemanager/remotemanager-0.13.9.tar.gz/remotemanager-0.13.9/remotemanager/utils/uuid.py
@@ -0,0 +1,21 @@
+import hashlib
+
+from remotemanager.logging_utils.utils import format_iterable
+
+
+def generate_uuid(string: str) -> str:
+    """
+    Generates a UUID string from an input
+
+    Args:
+        string:
+            input string
+    Returns:
+        (str) UUID
+    """
+    if not isinstance(string, str):
+        string = format_iterable(string)
+    h = hashlib.sha256()
+    h.update(bytes(string, "utf-8"))
+
+    return str(h.hexdigest())
