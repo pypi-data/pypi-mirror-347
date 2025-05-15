c2f1ae79de2010bf58c7c201bb157a03370bcf4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 05:06:42 +0000

    Add package petcmd/0.5.2/petcmd-0.5.2.tar.gz

---
+++ packages/petcmd/petcmd-0.5.2.tar.gz/petcmd-0.5.2/petcmd/autocompletion/zsh.py
@@ -0,0 +1,20 @@
+
+ZSH_AUTOCOMPLETE_TEMPLATE = """
+#compdef {alias}
+
+local -a completions
+completions=("${{(@f)$(app --shell-completion "$((CURRENT-2))" "${{words[@]:1}}")}}")
+completions=(${{completions:#}})
+
+if [[ "$completions[1]" == "__files__" ]]; then
+  _files && return 0
+  return 1
+fi
+
+if (( ${{#completions}} == 0 )); then
+  return 1
+fi
+
+compadd -V unsorted -a completions
+return 0
+""".lstrip()
