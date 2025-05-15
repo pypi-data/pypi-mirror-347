64e43cba9fe45c7798228f2e58d6db3ae4d0c8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 06:05:11 +0000

    Add package timeplus-neutrino/0.1.12/timeplus_neutrino-0.1.12.tar.gz

---
+++ packages/timeplus-neutrino/timeplus_neutrino-0.1.12.tar.gz/timeplus_neutrino-0.1.12/example/cdc_flat_generic.py
@@ -0,0 +1,34 @@
+from neutrino.pipeline.agent import DataExtractionAgent
+from neutrino.conf import TimeplusAgentConfig
+
+data = """{
+		"customer_id": 9000,
+		"name": "Johnathan Rodriguez",
+		"email": "thomasramirez@example.org",
+		"phone": "001-845-290-8721x77863",
+		"address": "743 Cervantes Causeway Apt. 762\nPort Lauren, NY 12698"
+	}"""
+
+source_stream_name = "kafka_cdc_postgres_customers"
+target_stream_name = "customers"
+
+#agent_config = TimeplusAgentConfig()
+#agent_config.config("default", "http://localhost:11434/v1", "ollama", "codellama:latest")
+
+agent = DataExtractionAgent()
+
+settings = {
+	"type" :'s3',
+    "access_key_id" : 'minioadmin',
+    "secret_access_key" : 'minioadmin',
+    "region" : 'us-east-1',
+    "bucket" : 'timeplus',
+   "data_format" : 'JSONEachRow',
+    "endpoint" : 'http://minio:9000',
+    "write_to" : 'lumi/data.json',
+    "use_environment_credentials" : False
+}
+agent1_output, agent2_output = agent.generic_pipeline(data, source_stream_name, target_stream_name, settings)
+
+print(f" target stream : {agent1_output}")
+print(f" mv extraction  : {agent2_output}")
