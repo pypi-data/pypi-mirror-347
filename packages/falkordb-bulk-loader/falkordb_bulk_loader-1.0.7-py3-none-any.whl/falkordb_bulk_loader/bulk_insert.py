4df35766dacf21ef8d2f201e3dde7a306d4c4db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:06:04 +0000

    Add package falkordb-bulk-loader/1.0.7/falkordb_bulk_loader-1.0.7-py3-none-any.whl

---
+++ packages/falkordb-bulk-loader/falkordb_bulk_loader-1.0.7-py3-none-any.whl/falkordb_bulk_loader/bulk_insert.py
@@ -0,0 +1,265 @@
+import sys
+from timeit import default_timer as timer
+
+import click
+import redis
+from falkordb import FalkorDB
+
+from .config import Config
+from .label import Label
+from .query_buffer import QueryBuffer
+from .relation_type import RelationType
+
+
+def parse_schemas(cls, query_buf, path_to_csv, csv_tuples, config):
+    schemas = [None] * (len(path_to_csv) + len(csv_tuples))
+    for idx, in_csv in enumerate(path_to_csv):
+        # Build entity descriptor from input CSV
+        schemas[idx] = cls(query_buf, in_csv, None, config)
+
+    offset = len(path_to_csv)
+    for idx, csv_tuple in enumerate(csv_tuples):
+        # Build entity descriptor from input CSV
+        schemas[idx + offset] = cls(query_buf, csv_tuple[1], csv_tuple[0], config)
+    return schemas
+
+
+# For each input file, validate contents and convert to binary format.
+# If any buffer limits have been reached, flush all enqueued inserts to Redis.
+def process_entities(entities):
+    for entity in entities:
+        entity.process_entities()
+        added_size = entity.binary_size
+        # Check to see if the addition of this data will exceed the buffer's capacity
+        if (
+            entity.query_buffer.buffer_size + added_size
+            >= entity.config.max_buffer_size
+            or entity.query_buffer.redis_token_count + len(entity.binary_entities)
+            >= entity.config.max_token_count
+        ):
+            # Send and flush the buffer if appropriate
+            entity.query_buffer.send_buffer()
+        # Add binary data to list and update all counts
+        entity.query_buffer.redis_token_count += len(entity.binary_entities)
+        entity.query_buffer.buffer_size += added_size
+
+
+################################################################################
+# Bulk loader
+################################################################################
+# Command-line arguments
+@click.command()
+@click.argument("graph")
+# Server connection settings
+@click.option(
+    "--server-url", "-u", default="redis://127.0.0.1:6379", help="Redis connection url"
+)
+@click.option("--nodes", "-n", multiple=True, help="Path to node csv file")
+@click.option(
+    "--nodes-with-label",
+    "-N",
+    nargs=2,
+    multiple=True,
+    help="Label string followed by path to node csv file",
+)
+@click.option("--relations", "-r", multiple=True, help="Path to relation csv file")
+@click.option(
+    "--relations-with-type",
+    "-R",
+    nargs=2,
+    multiple=True,
+    help="Relation type string followed by path to relation csv file",
+)
+@click.option(
+    "--separator", "-o", default=",", help="Field token separator in csv file"
+)
+# Schema options
+@click.option(
+    "--enforce-schema",
+    "-d",
+    default=False,
+    is_flag=True,
+    help="Enforce the schema described in CSV header rows",
+)
+@click.option(
+    "--id-type",
+    "-j",
+    default="STRING",
+    help="The data type of unique node ID properties (either STRING or INTEGER)",
+)
+@click.option(
+    "--skip-invalid-nodes",
+    "-s",
+    default=False,
+    is_flag=True,
+    help="ignore nodes that use previously defined IDs",
+)
+@click.option(
+    "--skip-invalid-edges",
+    "-e",
+    default=False,
+    is_flag=True,
+    help="ignore invalid edges, print an error message and continue loading (True), or stop loading after an edge loading failure (False)",
+)
+@click.option(
+    "--quote",
+    "-q",
+    default=0,
+    help="the quoting format used in the CSV file. QUOTE_MINIMAL=0,QUOTE_ALL=1,QUOTE_NONNUMERIC=2,QUOTE_NONE=3",
+)
+@click.option(
+    "--escapechar",
+    "-x",
+    default="\\",
+    help='the escape char used for the CSV reader (default \\). Use "none" for None.',
+)
+# Buffer size restrictions
+@click.option(
+    "--max-token-count",
+    "-c",
+    default=1024,
+    help="max number of processed CSVs to send per query (default 1024)",
+)
+@click.option(
+    "--max-buffer-size",
+    "-b",
+    default=64,
+    help="max buffer size in megabytes (default 64, max 1024)",
+)
+@click.option(
+    "--max-token-size",
+    "-t",
+    default=64,
+    help="max size of each token in megabytes (default 64, max 512)",
+)
+@click.option(
+    "--index", "-i", multiple=True, help="Label:Propery on which to create an index"
+)
+@click.option(
+    "--full-text-index",
+    "-f",
+    multiple=True,
+    help="Label:Propery on which to create an full text search index",
+)
+def bulk_insert(
+    graph,
+    server_url,
+    nodes,
+    nodes_with_label,
+    relations,
+    relations_with_type,
+    separator,
+    enforce_schema,
+    id_type,
+    skip_invalid_nodes,
+    skip_invalid_edges,
+    escapechar,
+    quote,
+    max_token_count,
+    max_buffer_size,
+    max_token_size,
+    index,
+    full_text_index,
+):
+    if sys.version_info.major < 3 or sys.version_info.minor < 6:
+        raise Exception("Python >= 3.6 is required for the falkordb bulk loader.")
+
+    if not (any(nodes) or any(nodes_with_label)):
+        raise Exception("At least one node file must be specified.")
+
+    start_time = timer()
+
+    # If relations are being built, we must store unique node identifiers to later resolve endpoints.
+    store_node_identifiers = any(relations) or any(relations_with_type)
+
+    # Initialize configurations with command-line arguments
+    config = Config(
+        max_token_count,
+        max_buffer_size,
+        max_token_size,
+        enforce_schema,
+        id_type,
+        skip_invalid_nodes,
+        skip_invalid_edges,
+        separator,
+        int(quote),
+        store_node_identifiers,
+        escapechar,
+    )
+
+    redis_con = redis.from_url(server_url)
+    client = FalkorDB.from_url(server_url)
+
+    # Attempt to connect to the server
+    try:
+        redis_con.ping()
+    except redis.exceptions.ConnectionError as e:
+        print("Could not connect to FalkorDB server.")
+        raise e
+
+    # Attempt to verify that falkordb module is loaded
+    try:
+        module_list = [m[b"name"] for m in redis_con.module_list()]
+        if b"graph" not in module_list:
+            print("falkordb module not loaded on connected server.")
+            sys.exit(1)
+    except redis.exceptions.ResponseError:
+        # Ignore check if the connected server does not support the "MODULE LIST" command
+        pass
+
+    # Verify that the graph name is not already used in the Redis database
+    key_exists = redis_con.exists(graph)
+    if key_exists:
+        print(
+            f"Graph with name '{graph}', could not be created, as '{graph}' already exists."
+        )
+        sys.exit(1)
+
+    query_buf = QueryBuffer(graph, client, config)
+
+    # Read the header rows of each input CSV and save its schema.
+    labels = parse_schemas(Label, query_buf, nodes, nodes_with_label, config)
+    reltypes = parse_schemas(
+        RelationType, query_buf, relations, relations_with_type, config
+    )
+
+    process_entities(labels)
+    process_entities(reltypes)
+
+    # Send all remaining tokens to Redis
+    query_buf.send_buffer()
+    query_buf.wait_pool()
+
+    end_time = timer()
+    query_buf.report_completion(end_time - start_time)
+
+    # Add in Graph Indices after graph creation
+    graph = client.select_graph(graph)
+    for i in index:
+        l, p = i.split(":")
+        print(f"Creating Index on Label: {l}, Property: {p}")
+        try:
+            graph.create_node_range_index(l, p)
+        except redis.exceptions.ResponseError as e:
+            print(f"Unable to create Index on Label: {l}, Property {p}")
+            print(e)
+
+    # Add in Full Text Search Indices after graph creation
+    for i in full_text_index:
+        l, p = i.split(":")
+        print(f"Creating Full Text Search Index on Label: {l}, Property: {p}")
+        try:
+            graph.create_node_fulltext_index(l, p)
+        except redis.exceptions.ResponseError as e:
+            print(
+                f"Unable to create Full Text Search Index on Label: {l}, Property {p}"
+            )
+            print(e)
+        except Exception:
+            print(
+                f"Unknown Error: Unable to create Full Text Search Index on Label: {l}, Property {p}"
+            )
+
+
+if __name__ == "__main__":
+    bulk_insert()
---
+++ packages/falkordb-bulk-loader/falkordb_bulk_loader-1.0.7-py3-none-any.whl/falkordb_bulk_loader/relation_type.py
@@ -0,0 +1,119 @@
+import re
+import struct
+
+import click
+
+from .entity_file import EntityFile, Type
+from .exceptions import CSVError, SchemaError
+
+
+# Handler class for processing relation csv files.
+class RelationType(EntityFile):
+    def __init__(self, query_buffer, infile, type_str, config):
+        super(RelationType, self).__init__(infile, type_str, config)
+        self.query_buffer = query_buffer
+
+    def process_schemaless_header(self, header):
+        if self.column_count < 2:
+            raise CSVError(
+                "Relation file '%s' should have at least 2 elements in header line."
+                % (self.infile.name)
+            )
+        # The first column is the source ID and the second is the destination ID.
+        self.start_id = 0
+        self.end_id = 1
+        self.start_namespace = None
+        self.end_namespace = None
+
+        for idx, field in enumerate(header[2:]):
+            self.column_names[idx + 2] = field.strip()
+
+    def post_process_header_with_schema(self, header):
+        # Can interleave these tasks if preferred.
+        if self.types.count(Type.START_ID) != 1:
+            raise SchemaError(
+                "Relation file '%s' should have exactly one START_ID column."
+                % (self.infile.name)
+            )
+        if self.types.count(Type.END_ID) != 1:
+            raise SchemaError(
+                "Relation file '%s' should have exactly one END_ID column."
+                % (self.infile.name)
+            )
+
+        self.start_id = self.types.index(Type.START_ID)
+        self.end_id = self.types.index(Type.END_ID)
+        self.start_namespace = None
+        self.end_namespace = None
+        # Capture namespaces of start and end IDs if provided
+        start_match = re.search(r"\((\w+)\)", header[self.start_id])
+        if start_match:
+            self.start_namespace = start_match.group(1)
+        end_match = re.search(r"\((\w+)\)", header[self.end_id])
+        if end_match:
+            self.end_namespace = end_match.group(1)
+
+    def process_entities(self):
+        entities_created = 0
+        with click.progressbar(
+            self.reader,
+            length=self.entities_count,
+            label=self.entity_str,
+            update_min_steps=100,
+        ) as reader:
+            for row in reader:
+                self.validate_row(row)
+                try:
+                    start_id = row[self.start_id]
+                    if self.start_namespace:
+                        start_id = self.start_namespace + "." + str(start_id)
+                    end_id = row[self.end_id]
+                    if self.end_namespace:
+                        end_id = self.end_namespace + "." + str(end_id)
+
+                    src = self.query_buffer.nodes[start_id]
+                    dest = self.query_buffer.nodes[end_id]
+                except KeyError as e:
+                    print(
+                        "%s:%d Relationship specified a non-existent identifier. src: %s; dest: %s"
+                        % (
+                            self.infile.name,
+                            self.reader.line_num - 1,
+                            row[self.start_id],
+                            row[self.end_id],
+                        )
+                    )
+                    if self.config.skip_invalid_edges is False:
+                        raise e
+                    continue
+                fmt = "=QQ"  # 8-byte unsigned ints for src and dest
+                try:
+                    row_binary = struct.pack(fmt, src, dest) + self.pack_props(row)
+                except SchemaError as e:
+                    raise SchemaError(
+                        "%s:%d %s" % (self.infile.name, self.reader.line_num, str(e))
+                    )
+                row_binary_len = len(row_binary)
+                # If the addition of this entity will make the binary token grow too large,
+                # send the buffer now.
+                added_size = self.binary_size + row_binary_len
+                if (
+                    added_size >= self.config.max_token_size
+                    or self.query_buffer.buffer_size + added_size
+                    >= self.config.max_buffer_size
+                ):
+                    self.query_buffer.reltypes.append(self.to_binary())
+                    self.query_buffer.send_buffer()
+                    self.reset_partial_binary()
+                    # Push the reltype onto the query buffer again, as there are more entities to process.
+                    self.query_buffer.reltypes.append(self.to_binary())
+
+                self.query_buffer.relation_count += 1
+                entities_created += 1
+                self.binary_size += row_binary_len
+                self.binary_entities.append(row_binary)
+            self.query_buffer.reltypes.append(self.to_binary())
+        self.infile.close()
+        print(
+            "%d relations created for type '%s'" % (entities_created, self.entity_str)
+        )
