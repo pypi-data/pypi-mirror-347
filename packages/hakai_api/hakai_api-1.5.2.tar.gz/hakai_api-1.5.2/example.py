2909346f4b7ae18777d562be29e8a718a674a7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 06:05:09 +0000

    Add package hakai_api/1.5.2/hakai_api-1.5.2.tar.gz

---
+++ packages/hakai_api/hakai_api-1.5.2.tar.gz/hakai_api-1.5.2/example.py
@@ -0,0 +1,14 @@
+"""An example showing how to use the Hakai Api Python Client."""
+
+from hakai_api import Client
+
+if __name__ == "__main__":
+    # Get the api request client
+    client = Client()
+
+    # Make a data request for sampling stations
+    url = "%s/%s" % (client.api_root, "/aco/views/projects/phases")
+    response = client.get(url)
+
+    print(url)
+    print(response.json())
