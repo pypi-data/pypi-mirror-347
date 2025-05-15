59318bd375f59b4ba93f8dfdd8a75618cafc1456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 15:06:08 +0000

    Add package pdf-json-extractor/3.4.0/pdf_json_extractor-3.4.0.tar.gz

---
+++ packages/pdf-json-extractor/pdf_json_extractor-3.4.0.tar.gz/pdf_json_extractor-3.4.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pdf_json_extractor",         
+    version="3.4.0",
+    description="PDF to JSON extractor",
+    author="RameshLanke",
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        "google-generativeai",
+        "requests",
+        "beautifulsoup4",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
