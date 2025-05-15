6ac550a90861b23c1a6fb467a3054f86f7c36da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 03:23:08 +0000

    Add package api-bricks-sec-api-rest/1.0.1/api_bricks_sec_api_rest-1.0.1.tar.gz

---
+++ packages/api-bricks-sec-api-rest/api_bricks_sec_api_rest-1.0.1.tar.gz/api_bricks_sec_api_rest-1.0.1/api_bricks_sec_api_rest/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from api_bricks_sec_api_rest.api.content_extraction_api import ContentExtractionApi
+from api_bricks_sec_api_rest.api.filing_metadata_api import FilingMetadataApi
+from api_bricks_sec_api_rest.api.full_text_search_api import FullTextSearchApi
+
