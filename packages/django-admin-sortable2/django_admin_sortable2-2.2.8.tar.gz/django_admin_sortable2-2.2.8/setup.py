cf805bcbf96e6f079a91f78cd28374eb8de1dc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 16:06:04 +0000

    Add package django-admin-sortable2/2.2.8/django_admin_sortable2-2.2.8.tar.gz

---
+++ packages/django-admin-sortable2/django_admin_sortable2-2.2.8.tar.gz/django_admin_sortable2-2.2.8/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import io
+from setuptools import setup, find_namespace_packages
+from adminsortable2 import __version__
+
+
+def readfile(filename):
+    with io.open(filename, encoding='utf-8') as fd:
+        return fd.read()
+
+
+DESCRIPTION = 'Generic drag-and-drop sorting for the List, the Stacked- and the Tabular-Inlines Views in the Django Admin'
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
+    'Framework :: Django',
+    'Framework :: Django :: 4.2',
+    'Framework :: Django :: 5.0',
+    'Framework :: Django :: 5.1',
+    'Framework :: Django :: 5.2',
+]
+
+
+setup(
+    name='django-admin-sortable2',
+    version=__version__,
+    author='Jacob Rief',
+    author_email='jacob.rief@gmail.com',
+    description=DESCRIPTION,
+    long_description=readfile('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jrief/django-admin-sortable2',
+    license='MIT',
+    keywords=['django'],
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=4.2',
+    ],
+    packages=find_namespace_packages(include=['adminsortable2']),
+    include_package_data=True,
+    zip_safe=False,
+)
