b599b808a062beb695c1a4bc90a9aee020d18c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 06:05:41 +0000

    Add package fwdi/0.1.8/fwdi-0.1.8-py3-none-any.whl

---
+++ packages/fwdi/fwdi-0.1.8-py3-none-any.whl/fwdi/Application/Pages/web_main_page.py
@@ -0,0 +1,20 @@
+from ...Application.Abstractions.base_web_tabs_panel import BaseWebTabsPanel
+from ...Application.Abstractions.base_web_main_page import BaseWebMainPage
+from ...Infrastructure.Web.web_tabs_panel import WebTabsPanel, BaseWebTabItem
+
+class WebMainPage(BaseWebMainPage):
+    def __init__(self):
+        self.__panel:BaseWebTabsPanel = WebTabsPanel()
+        self.__lst_page:list[BaseWebTabItem] = []
+
+    def __create_tabs(self):
+        for item in self.__lst_page:
+            item.create_gui_tab()
+            self.__panel.add_tab(item)
+
+    def add_page(self, item:BaseWebTabItem):
+        self.__lst_page.append(item)
+
+    def create(self):
+        self.__create_tabs()
+        return self.__panel.create_panel()
