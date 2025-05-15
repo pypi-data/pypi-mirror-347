8d7fb7f726ee3d7c6fa70718714de2cfad1f5db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 06:05:31 +0000

    Add package torch-mice/0.3.0/torch_mice-0.3.0.tar.gz

---
+++ packages/torch-mice/torch_mice-0.3.0.tar.gz/torch_mice-0.3.0/torch_mice/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.0"
+
+from .positive_linear import PositiveLinearHK, PositiveLinear3DHK
+from .convex_gate     import ConvexGate
+from .batched_icnn    import BatchedICNN
+from .vector_hull     import VectorHull
