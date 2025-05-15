11c91d641e9787235227655415d612a6d2925d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 04:07:13 +0000

    Add package elkar/0.1.20/elkar-0.1.20.tar.gz

---
+++ packages/elkar/elkar-0.1.20.tar.gz/elkar-0.1.20/elkar/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.20"
+
+from . import a2a_types, server, store, task_manager, task_modifier, task_queue
+from .server import A2AServer
+
+__all__ = ["A2AServer", "a2a_types", "task_manager", "task_modifier", "server", "store", "task_queue"]
