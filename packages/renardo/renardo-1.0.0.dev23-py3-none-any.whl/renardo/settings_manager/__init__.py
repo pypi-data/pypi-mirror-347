86c5ab1efb31f912628f4452950ae28073378854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 07:06:59 +0000

    Add package renardo/1.0.0.dev23/renardo-1.0.0.dev23-py3-none-any.whl

---
+++ packages/renardo/renardo-1.0.0.dev23-py3-none-any.whl/renardo/settings_manager/__init__.py
@@ -0,0 +1,10 @@
+
+from .settings_manager import SettingsManager, settings
+
+from .renardo_core_settings import get_tutorial_files
+from .foxdot_editor_settings import Colours, conf
+
+from .sc_backend_settings import settings
+from .samples_pack_settings import settings
+from .webserver_settings import settings
+
