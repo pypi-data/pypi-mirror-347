b225e592fe69456b7c5a65209acede86641ef6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-15 05:07:31 +0000

    Add package saviialib/0.9.0/saviialib-0.9.0-py3-none-any.whl

---
+++ packages/saviialib/saviialib-0.9.0-py3-none-any.whl/saviialib/services/epii/controllers/types/update_thies_data_types.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from typing import Dict
+from saviialib.general_types.api.epii_api_types import (
+    EpiiUpdateThiesConfig,
+)
+
+
+@dataclass
+class UpdateThiesDataControllerInput:
+    config: EpiiUpdateThiesConfig
+
+
+@dataclass
+class UpdateThiesDataControllerOutput:
+    message: str
+    status: int
+    metadata: Dict[str, str] = field(default_factory=dict)
